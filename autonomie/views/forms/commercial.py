a79dff0522ff6bb26ae5e536cf1ca7d1159320fe
G.Tjebbes <g.t@majerti.fr>
2010-09-14 18:08:44 +0200

    Use the main module in commercial forms

--- autonomie/views/forms/commercial.py
+++ autonomie/views/forms/commercial.py
@@ -21,24 +21,22 @@
 #    You should have received a copy of the GNU General Public License
 #    along with Autonomie.  If not, see <http://www.gnu.org/licenses/>.
 #
+"""
+    Form schemas for commercial handling
+"""
 
 import colander
-from datetime import date
 from deform import widget
 
-from autonomie.views.forms.widgets import deferred_year_select_widget
+from autonomie.views.forms import main
 from .custom_types import AmountType
 
-@colander.deferred
-def default_year(node, kw):
-    return date.today().year
-
-
 class CommercialFormSchema(colander.MappingSchema):
-    year = colander.SchemaNode(colander.Integer(),
-            widget=deferred_year_select_widget,
-            default=default_year,
-            missing=default_year,
+    year = colander.SchemaNode(
+            colander.Integer(),
+            widget=main.deferred_year_select_widget,
+            default=main.default_year,
+            missing=main.default_year,
             title=u"")
 
 
@@ -48,7 +46,6 @@ class CommercialSetFormSchema(colander.MappingSchema):
                                 title=u'',
                                 validator=colander.Range(1,12))
     value = colander.SchemaNode(AmountType(), title=u"CA prévisionnel")
-    comment = colander.SchemaNode(colander.String(),
-                                 widget=widget.TextAreaWidget(cols=25, rows=1),
-                                    title=u"Commentaire",
-                                    missing=u"")
+    comment = main.textarea_node(
+                title=u"Commentaire",
+                missing=u"")

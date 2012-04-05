ee51858ca9c4e8a02fe5c6af029f14a774665c55
tonthon <tonthon21@gmail.com>
2012-04-05 23:08:39 +0200

    Company edition page

--- autonomie/views/company.py
+++ autonomie/views/company.py
@@ -17,14 +17,19 @@
     Entry point for the main users
 """
 import logging
+from deform import ValidationFailure
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPForbidden
+
+from deform import Form
+from autonomie.views.forms import CompanySchema
 
 log = logging.getLogger(__name__)
 @view_config(route_name='company', renderer='company_index.mako')
 def company_index(request):
     """
         index page for the company shows latest news :
+            #TODO
             - last validated estimation/invoice
             - To be relaunched bill
     """
@@ -36,3 +41,34 @@ def company_index(request):
     except KeyError:
         ret_val = HTTPForbidden()
     return ret_val
+
+@view_config(route_name='company', renderer='company_edit.mako', request_param='edit')
+def company_edit(request):
+    """
+        Company edition page
+    """
+    cid = request.matchdict.get('cid')
+    avatar = request.session['user']
+    try:
+        company = avatar.get_company(cid)
+    except KeyError:
+        return HTTPForbidden()
+    schema = CompanySchema().bind(edit=True)
+    form = Form(schema, buttons=('submit', ))
+    if 'submit' in request.params:
+        datas = request.params.items()
+        print datas
+        try:
+            app_datas = form.validate(datas)
+        except ValidationFailure, errform:
+            html_form = errform.render()
+        else:
+            company = merge_session_with_post(client, app_datas)
+            dbsession.merge(client)
+            request.session.flash(message, queue='main')
+            html_form = form.render(company.appstruct())
+    else:
+        html_form = form.render(company.appstruct())
+    return dict(title=company.name,
+                company=company,
+                html_form=html_form)

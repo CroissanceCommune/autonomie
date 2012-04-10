b3744b1d746cab0e0733154337343f1194bc250a
tonthon <tonthon21@gmail.com>
2012-04-10 09:39:25 +0200

    Import at module level

---
+++ autonomie/views/forms/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# * File Name : __init__.py
+#
+# * Copyright (C) 2010 Gaston TJEBBES <g.t@majerti.fr>
+# * Company : Majerti ( http://www.majerti.fr )
+#
+#   This software is distributed under GPLV3
+#   License: http://www.gnu.org/licenses/gpl-3.0.txt
+#
+# * Creation Date : 10-04-2012
+# * Last Modified :
+#
+# * Project : Autonomie
+#
+"""
+    Form Schemas for autonomie
+"""
+from .user import authSchema
+from .user import pwdSchema
+from .client import ClientSchema
+from .company import CompanySchema
+from .project import ProjectSchema

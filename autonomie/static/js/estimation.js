a0ddf4386c6a5b6c9c2ffd1813b4653b8e26a4c3
tonthon <tonthon21@gmail.com>
2012-04-11 13:04:21 +0200

    Estimation amounts computation

--- autonomie/static/js/estimation.js
+++ autonomie/static/js/estimation.js
@@ -12,27 +12,177 @@ function computeLineTotal(id){
   /*
    * Compute Estimation line total
    */
-  console.log("Computing line total");
-  var totaldiv = $("#total_" + id);
-  var price = $("#price_" + id)[0].value;
-  var quantity = $("#quantity_" + id)[0].value;
-  var total = parseInt(price) * parseInt(quantity);
-  /* TODO : test Nan value for total and alert user
-  */
-  totaldiv.html("<div>" + total + "</div>");
+  var totalinput = $("#total_" + id + " input");
+  var price = $("#price_" + id + " input")[0].value;
+  var quantity = $("#quantity_" + id + " input")[0].value;
+  var total = transformToCents(price) * transformToCents(quantity);
+  console.log("total : %s", total);
+  console.log(totalinput);
+  totalinput.val( formatAmount(total) );
+}
+function computeLinesTotal(){
+  /*
+   * Compute the estimation Total
+   * and subtotals
+   */
+  var linestotal = 0;
+  $("div.linetotal input").each(function(){
+    linestotal += transformToCents($(this).html());
+  });
+  return linestotal;
+}
+function getDiscount(){
+  /*
+   * Returns the current discount
+   */
+  return transformToCents($("#discount input").val());
+}
+function getTVA(){
+  /*
+   *  Returns the current tva
+   */
+  var tva = $("#tva select").val();
+  if (tva <0){
+    tva = 0;
+  }
+  return tva;
+}
+function getTvaPart(total, tva){
+  /*
+   * returns the value of the tva
+   */
+  return total * (tva / 10000);
+}
+function getExpenses(){
+  /*
+   *  Return the current expense configured
+   */
+  return transformToCents( $('#expenses input').val() );
+}
+function formatPrice(price) {
+  /*
+   * Return a formatted price
+   * @price : compute-formatted price
+   */
+  var cents = $.trim("" + price);
+  var idx = cents.indexOf(".");
+  var centimes;
+  if(idx >= 0) {
+    if(Math.round("0." + cents.substring(idx + 1)) > 0)
+      price ++;
+    cents = $.trim("" + price);
+    idx = cents.indexOf(".");
+    cents = cents.substring(0, idx);
+  }
+
+  centimes = cents.substring(cents.length - 2);
+  if(centimes.length === 0)
+    centimes = "00";
+  else if(centimes.length < 2)
+    centimes = "0" + centimes;
+  return Math.floor(price / 100) + "," + centimes;
+}
+function formatAmount( amount ){
+  /*
+   * return a formatted user-friendly amount
+   */
+  return formatPrice( amount ) + "&nbsp;&euro;";
 }
 function computeTotal(){
+  /*
+   * Compute the main totals
+   */
   console.log("Computing the total");
+  var linestotal = computeLinesTotal();
+  var discount = getDiscount();
+  var HTTotal = linestotal - discount;
+  var tva = getTVA();
+  var tvaPart = getTvaPart( HTTotal, tva );
+  var expenses = getExpenses();
+  var total = HTTotal + tvaPart + expenses;
+  console.log("Linestotal : %s", linestotal);
+  console.log("discount: %s", discount);
+  console.log("HTTotal: %s", HTTotal);
+  console.log("tva : %s", tva);
+  console.log("tvaPart : %s", tvaPart);
+  console.log("expenses : %s", expenses);
+  console.log("total : %s", total);
+  $('#linestotal input').empty().html(formatAmount(linestotal));
+  $('#httotal input').empty().html(formatAmount(HTTotal));
+  $('#tvapart input').empty().html(formatAmount(tvaPart));
+  $('#total input').empty().html(formatAmount(total));
 }
-function addLine(id){
-  /* TODO : add id detection
-   * TODO : add computeTotal
+function getNextId(){
+  /*
+   * Returns the next available id
+   */
+  var newid = 1;
+  $('div.estimation_line').each(function(){
+    var lineid = parseInt(this.id.substring("estimation_line_".length), 10);
+    if (lineid >= newid){
+      newid = lineid + 1;
+    }
+  });
+  return newid;
+}
+function addLine(){
+  /*
+   * Add an estimation line
    */
   var template = $( '#prestationTmpl' ).template();
-  args = {id:id};
+  var id = getNextId();
+  args = { id : id };
   html = $.tmpl(  template, args );
   console.log(html);
-  $( '#estimationcontainer' ).html(html);
-   $("#quantity_" + id).blur(function(){computeLineTotal(id);});
-   $("#price_" + id).blur(function(){computeLineTotal(id);});
+  $( '#estimationcontainer' ).append(html);
+  $("#quantity_" + id + " input").blur(function(){
+      computeLineTotal(id);
+      computeTotal();
+      });
+  $("#price_" + id + " input").blur(function(){
+      computeLineTotal(id);
+      computeTotal();
+      });
+}
+function transformToCents(value) {
+  /*
+   * Transform the value to cents
+   */
+  var cents;
+  var centimes;
+  var sval = "" + value;
+  var i;
+
+  i = sval.indexOf('.');
+  if(i < 0)
+    i = sval.indexOf(',');
+
+  if(i >= 0) {
+    centimes = sval.substr(i + 1, 2);
+    if(centimes.length < 1)
+      centimes = "00";
+    else if(centimes.length < 2)
+      centimes = centimes + "0";
+    cents = sval.substr(0, i) + centimes;
+  }
+  else {
+    cents = value * 100;
+  }
+
+  return cents;
+}
+function setDefault(){
+  /*
+   * initialize the estimation creation form
+   */
+  addLine();
+  $('#expenses input').blur(function(){
+    computeTotal();
+  });
+  $('#discount input').blur(function(){
+    computeTotal();
+  });
+  $('#tva select').change(function(){
+    computeTotal();
+  });
 }

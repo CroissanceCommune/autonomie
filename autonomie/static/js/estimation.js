fbd824af9e9dfd0b028488ce299da5762afa992d
tonthon <tonthon21@gmail.com>
2012-04-13 23:13:24 +0200

    javascript : deform-compatible form handling

--- autonomie/static/js/estimation.js
+++ autonomie/static/js/estimation.js
@@ -8,14 +8,57 @@
  * License: http://www.gnu.org/licenses/gpl-3.0.txt
  *
  */
-function computeEstimationRows(id){
+var Facade = new Object();
+/*
+ *
+ * Utitilities
+ *
+ *
+ */
+
+function transformToCents(value) {
+  /*
+   * Transform the value to cents
+   */
+  var cents;
+  var centimes;
+  var sval = "" + value;
+  var i;
+
+  if ((value === undefined)||(value === null)){
+    value = "0.00";
+  }
+  value = value.replace(",", ".");
+  result = parseFloat(value);
+  if (isNaN(result)){
+    return 0.0;
+  }else{
+    return result;
+  }
+}
+
+function delRow(id){
+  /*
+   * Remove the estimation line of id 'id'
+   */
+  $('#' + id).remove();
+  $(Facade).trigger("linedelete");
+}
+/*
+ *
+ * Estimation Lines handling (computing ...)
+ *
+ */
+
+function computeEstimationRow(tag){
   /*
    * Compute Estimation line total
    */
-  var totalinput = $("#total_" + id + " .input");
-  var price = $("#price_" + id + " input")[0].value;
-  var quantity = $("#quantity_" + id + " input")[0].value;
-  var total = transformToCents(price) * transformToCents(quantity);
+  var row = $(tag);
+  var totalinput = row.find(".linetotal .input");
+  var cost = row.find("input[name=cost]").val();
+  var quantity = row.find("input[name=quantity]").val();
+  var total = transformToCents(cost) * transformToCents(quantity);
   totalinput.empty().html( formatAmount(total) );
 }
 function computeRowsTotal(){
@@ -23,23 +66,25 @@ function computeRowsTotal(){
    * Compute the estimation Total
    * and subtotals
    */
+  console.log('## Computing rows total');
   var sum = 0;
   $("div.linetotal .input").each(function(){
     sum += transformToCents($(this).text());
   });
+  console.log("  -> %s", sum);
   return sum;
 }
 function getDiscount(){
   /*
    * Returns the current discount
    */
-  return transformToCents($("#discount input").val());
+  return transformToCents($("input[name=discount]").val());
 }
 function getTVA(){
   /*
    *  Returns the current tva
    */
-  var tva = $("#tva select").val();
+  var tva = $("select[name=tva]").val();
   if (tva <0){
     tva = 0;
   }
@@ -55,11 +100,11 @@ function getExpenses(){
   /*
    *  Return the current expense configured
    */
-  return transformToCents( $('#expenses input').val() );
+  return transformToCents( $('input[name=expenses]').val() );
 }
 function formatPrice(price) {
   /*
-   * Return a formatted price
+   * Return a formatted price for display
    * @price : compute-formatted price
    */
   price = Math.round(price*100) / 100;
@@ -87,7 +132,9 @@ function computeTotal(){
    */
   console.log("Computing the total");
   var linestotal = computeRowsTotal();
+  console.log(" -Lines total : %s", linestotal);
   var discount = getDiscount();
+  console.log(" -Discount : %s", discount);
   var HTTotal = linestotal - discount;
   var tva = getTVA();
   var tvaPart = getTvaPart( HTTotal, tva );
@@ -97,54 +144,60 @@ function computeTotal(){
   $('#httotal .input').empty().html(formatAmount(HTTotal));
   $('#tvapart .input').empty().html(formatAmount(tvaPart));
   $('#total .input').empty().html(formatAmount(total));
-  updatePayments(total);
-  return total;
+  $(Facade).trigger('totalchanged');
 }
+/*
+ *
+ *
+ * Payment informations
+ *
+ *
+ */
 function computePaymentRows(){
   /*
    * Return the sum of manually configured values in the payment lines
    */
   console.log("** computePaymentRows");
   var sum = 0;
-  $(".paymentamount input").each(function(){
+  $(".paymentline input[name=amount]").each(function(){
     sum += transformToCents($(this).val());
   });
   console.log("  -> %s", sum);
   console.log("-------------");
   return sum;
 }
-function updatePayments(total){
-  /*
-   * Update the payments lines
-   * Needed to allow manual configuration
-   */
-  if (total === undefined){
-    total = transformToCents($('#total .input').text());
-  }
-  var nbpayments = getNbPayments();
-  if (nbpayments === -1){
-    // Manual payment
-    var topay = setAccount(total);
-    var linesum = computePaymentRows();
-    var rest = topay - linesum;
-    $('.paymentamount .input').html(formatAmount(rest));
-  }else{
-    // Computed payments
-    setPaymentValues(total, nbpayments);
-  }
-}
-function setAccount(total){
-  /*
-   * Compute and set the account line (if needed)
-   */
-  console.log("** setAccount");
-  var account_percent = getAccountPercent();
-  var account_amount = setAccountAmount(total, account_percent);
-  var topay = total - account_amount;
-  console.log("Rest to be paid : %s", topay);
-  return topay;
-}
-function setPaymentValues(total, nbpayments){
+//function updatePayments(total){
+//  /*
+//   * Update the payments lines
+//   * Needed to allow manual configuration
+//   */
+//  if (total === undefined){
+//    total = transformToCents($('#total .input').text());
+//  }
+//  var nbpayments = getNbPayments();
+//  if (nbpayments === -1){
+//    // Manual payment
+//    var topay = setDeposit(total);
+//    var linesum = computePaymentRows();
+//    var rest = topay - linesum;
+//    $('.paymentamount .input').html(formatAmount(rest));
+//  }else{
+//    // Computed payments
+//    setPaymentRows(total, nbpayments);
+//  }
+//}
+//function setDeposit(total){
+//  /*
+//   * Compute and set the account line (if needed)
+//   */
+//  console.log("** setDeposit");
+//  var account_percent = getDepositPercent();
+//  var account_amount = setDepositAmount(total, account_percent);
+//  var topay = total - account_amount;
+//  console.log("Rest to be paid : %s", topay);
+//  return topay;
+//}
+function setPaymentRows(total, nbpayments){
   /*
    * Compute and set the payment amounts
    */
@@ -155,7 +208,7 @@ function setPaymentValues(total, nbpayments){
   if (nbpayments === undefined){
     nbpayments = getNbPayments();
   }
-  var topay = setAccount(total);
+  var topay = getToPayAfterDeposit();
   if (nbpayments === -1){
     // we ask for manual payment configuration
     // we want two lines (one manually configured,
@@ -182,9 +235,6 @@ function setDividedPayments(total, nbpayments, readonly){
     var p = total * 100 / nbpayments;
     part = Math.round( p / 100 );
   }
-  // Rest is equal to total if nbpayments <=1
-  var rest = total - ( (nbpayments-1) * part );
-
   for (i=1; i< nbpayments; i++){
     addPaymentRow({id:i,
                     amount:formatAmount(part),
@@ -192,21 +242,43 @@ function setDividedPayments(total, nbpayments, readonly){
                     description:description});
     description = "Paiement " + (i+1);
   }
-  addPaymentRow({id:nbpayments,
-                  amount:formatAmount(rest),
-                  readonly:true,
-                  description:description});
+  // Rest is equal to total if nbpayments <=1
+  var rest = total - ( (nbpayments-1) * part );
+  setSoldAmount(rest);
+}
+
+function computeSoldAmount(){
+  /*
+   * Return the sold's amount value
+   */
+  var topayAfterDeposit = getToPayAfterDeposit();
+  var sold = topayAfterDeposit - computePaymentRowSum();
+}
+function getSoldAmountTag(){
+  /*
+   * Returns the sold row
+   */
+  return $('#paymentline_1000 .paymentamount .input');
 }
 function addPaymentRow(args, after){
   /*
    * Add a payment line
    */
+  console.log("Adding a payment row");
+  console.log(args);
   if (args === undefined){
     args = new Object();
   }
   if (args['id'] === undefined){
     var id = getNextId("div.paymentline", "paymentline_");
     args['id'] = id;
+  }
+  if (args['readonly'] === undefined){
+    if (getNbPayments()>0){
+      args['readonly'] = true;
+    }else{
+      args['readonly'] = false;
+    }
   }
   var line = getPaymentRow(args);
   if (after === undefined){
@@ -216,17 +288,16 @@ function addPaymentRow(args, after){
   }
   if (!(args['readonly'])){
     $('#amount_' + args['id']).blur(function(){
-      updatePayments();
+      $(Facade).trigger('paymentlinechange');
     });
   }
-  console.log("Setting datepicker %s", args['id']);
   $("#paymentDate_" + args['id']).datepicker();
 }
 function getNbPayments(){
   /*
    * Return the number of configured payments
    */
-  return parseInt($("#nbpayment select").val(), 10);
+  return parseInt($("select[name=payment_times]").val(), 10);
 }
 function getIdFromTagId(parseStr, tagid){
   /*
@@ -258,31 +329,6 @@ function getRow(args){
   var template = $( '#prestationTmpl' ).template();
   return $.tmpl(  template, args );
 }
-function addEstimationRow(args){
-  /*
-   * Add an estimation line
-   * @param: args : initialization datas
-   */
-  if (args === undefined){
-    args = new Object();
-  }
-  var id = getNextId('div.estimationline', "estimationline_");
-  args["id"] = id;
-  var html = getRow(args);
-  $( '#estimationcontainer' ).append(html);
-  $("#quantity_" + id + " input").blur(function(){
-      computeEstimationRows(id);
-      computeTotal();
-      });
-  $("#price_" + id + " input").blur(function(){
-      computeEstimationRows(id);
-      computeTotal();
-      });
-  computeEstimationRows(id);
-  computeTotal();
-  console.log("Setting focus");
-  $("#estimationline_" + id + " textarea").focus();
-}
 function getPaymentRow(args){
   /*
    *  return a payment line in html format
@@ -292,87 +338,160 @@ function getPaymentRow(args){
   html = $.tmpl(  template, args );
   return html;
 }
-function transformToCents(value) {
-  /*
-   * Transform the value to cents
-   */
-  var cents;
-  var centimes;
-  var sval = "" + value;
-  var i;
-
-  if ((value === undefined)||(value === null)){
-    value = "0.00";
-  }
-  value = value.replace(",", ".");
-  result = parseFloat(value);
-  if (isNaN(result)){
-    return 0.0;
-  }else{
-    return result;
-  }
-}
-function setDefault(){
-  /*
-   * initialize the estimation creation form
-   */
-  addEstimationRow();
-  $('#expenses input').blur(function(){
-    computeTotal();
-  });
-  $('#discount input').blur(function(){
-    computeTotal();
-  });
-  $('#tva select').change(function(){
-    computeTotal();
-  });
-  $("#taskDate").datepicker();
-  $('#account_percent select').change(function(){
-    updatePayments();
-  });
-  $('#nbpayment select').change(function(){
-    setPaymentValues();
-  });
-  computeTotal();
-  setPaymentValues();
-}
-function getAccountPercent(){
-  /*
-   * return the amount of account
-   */
-  console.log("** getAccountPercent");
-  var percent = transformToCents($('#account_percent select').val());
-  console.log("    -> Percent : %s", percent);
-  return percent;
-}
-function computeAccount(total, percent){
+function computeDeposit(total, percent){
   /*
    *  Compute the expected account
    */
-  console.log("** Computing Account total : %s percent : %s", total, percent);
+  console.log("** Computing Deposit total : %s percent : %s", total, percent);
   var result = total * percent / 100;
   console.log("   -> Result : %s", result);
   return result;
 }
-function setAccountAmount(total, percent){
+function setDepositAmount(deposit){
   /*
    *  set the amount of the account
    *  @total : TTC total
    *  @percent : percent of the account
    */
-  var value = computeAccount( total, percent );
-  if (percent > 0){
-    $("#account_amount .input").html(formatAmount(value));
+  if (deposit > 0){
+    $("#account_amount .input").html(formatAmount(deposit));
     $("#account_container").css("display", "table-row");
   }else{
     $("#account_container").hide();
   }
-  return value;
 }
-function delRow(id){
+function getToPayAfterDeposit(){
   /*
-   * Remove the estimation line of id 'id'
+   *  return the topay value after deposit
    */
-  $('#' + id).remove();
-  computeTotal();
+  return getTotal() - getDeposit();
 }
+function getDeposit(){
+  /*
+   * Return the computed deposit
+   */
+  return getTotal() * getDepositPercent() / 100;
+}
+function getDepositPercent(tag){
+  /*
+   * Return the current configured deposit
+   * @tag : the select tag where the deposit is configured
+   */
+    if (tag === undefined){
+      tag = 'select[name=deposit]';
+    }
+    var deposit =  $(tag).val();
+    return transformToCents(deposit);
+}
+function getTotal(){
+  /*
+   * Return the current total
+   */
+  return transformToCents($('#total .input').text());
+}
+function setDeposit(){
+  /*
+   *  Define the deposit
+   *  @percent : deposit percent
+   */
+  var deposit = getDeposit();
+  setDepositAmount(deposit);
+}
+function setSoldAmount(value){
+  /*
+   * Set the sold amount
+   */
+  getSoldAmountTag().html(formatAmount(value));
+}
+function updatePaymentRows(){
+  /*
+   * Update payment rows
+   */
+  var nbpayments = getNbPayments();
+  if (nbpayments === -1){
+    updateSoldAmount();
+  }else{
+    setPaymentRows();
+  }
+}
+function updateSoldAmount(){
+  /*
+   * update the sold amount
+   */
+  var topay = getToPayAfterDeposit();
+  var sold = topay - computePaymentRows();
+  console.log("The sold is now : %s", sold);
+  setSoldAmount(sold);
+}
+function getAmountInput(textval, id){
+  /*
+   * Return an amount input
+   */
+  var args = {id:id, amount:textval};
+  var tmpl = $('#paymentAmountTmpl').template();
+  return $.tmpl( tmpl, args );
+}
+function setPaymentRowsToEditable(){
+  /*
+   * Update payment rows to change the amount to a html input
+   * On load, we don't know if the payment lines we get are editable
+   * by default we assume they are readonly, so we change them
+   * at startup if needed
+   */
+  if ($('#paymentcontainer').is('.paymentline')){
+    $('#paymentcontainer .paymentline .paymentamount').each(function(){
+      var textval = $(this).find('.input').text();
+      // Parent node is the paymentline, we get the id there
+      var myid = getIdFromTagId( 'paymentline_', $(this).parent().attr('id') );
+      $(this).empty().html(getAmountInput(textval, myid));
+    });
+  }else{
+    setPaymentRows();
+  }
+}
+function initialize(){
+  /*
+   *  Initialize the estimation UI
+   */
+  if (getNbPayments() < 1 ){
+     /*
+      * Add a row if needed and update rows to fit manual configuration
+      * (set them editable for example)
+      */
+     console.log("Manual payments");
+     setPaymentRowsToEditable();
+  }
+  $(Facade).bind('linechange', function(event, element){
+    computeEstimationRow(element);
+  });
+  $(Facade).bind('totalchange', function(event){
+    computeTotal();
+  });
+  $(Facade).bind('linedelete', function(event, element){
+    computeTotal();
+  });
+  $(Facade).bind('depositchange', function(event){
+    setDeposit();
+    updatePaymentRows();
+  });
+  $(Facade).bind('payment_timeschange', function(event){
+    setPaymentRows();
+  });
+  $(Facade).bind('paymentlinechange', function(event){
+    updateSoldAmount();
+  });
+  $(Facade).bind('totalchange', function(event){
+    setDeposit();
+    updatePaymentRows();
+  });
+
+  $('.estimationline').each(function(){
+    $(Facade).trigger('linechange', this);
+  });
+  $(Facade).trigger('totalchange');
+  $('select[name=tva]').change(function(){
+    $(Facade).trigger('totalchange');
+  });
+  $('#deform textarea').first().focus();
+}
+

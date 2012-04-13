dbf78dca4bc2166af5b7cd616678e19698da33b2
tonthon <tonthon21@gmail.com>
2012-04-13 23:14:19 +0200

    Estimation forms models

--- autonomie/views/forms/estimation.py
+++ autonomie/views/forms/estimation.py
@@ -36,6 +36,7 @@
 """
 import logging
 import colander
+import datetime
 
 from deform import ValidationFailure
 
@@ -47,148 +48,378 @@ from autonomie.models.model import EstimationLine
 
 log = logging.getLogger(__name__)
 
-class EstimationFormSchema():
+def get_percents():
     """
-        colander Schema for validating the estimation form
+        Return percents for select widget
     """
-    pass
+    percent_options = [('0','0'), ('5','5%')]
+    for i in range(10,110,10):
+        percent_options.append(("%d" % i, "%d %%" % i))
+    return percent_options
 
-def collect_indexes(keys, identifier):
+def get_payment_times():
     """
-        return estimation line indexes
+        Return options for payment times select
     """
-    return [int(key[len(identifier):]) for key in keys if key.startswith(identifier)]
+    payment_times = [('-1', u'Manuel')]
+    for i in range(1,12):
+        payment_times.append(('%d' % i, '%d fois' % i))
+    return payment_times
 
-def format_lines(datas):
-    """
-        get all the estimation lines configured in datas and
-        return a colander friendly data structure
-    """
-    keys = datas.keys()
-    # keys :
-    # prestation_id
-    # price_id
-    # quantity_id
-    # unity_id
-    indexes = collect_indexes(keys, "prestation_")
-    lines = []
-    for index in indexes:
-        prestation = datas['prestation_%d' % index]
-        quantity = datas['quantity_%d' % index]
-        price = datas['price_%d' % index]
-        unity = datas['unity_%d' % index]
-        lines.append(dict(description=prestation,
-                          cost=price,
-                          quantity=quantity,
-                          unity=unity))
-    return lines
 
-def format_task(datas):
-    """
-        return all the task related fields
-    """
-    keys = ['id_phase','description','taskDate',]
-    return dict((key, datas.get(key)) for key in keys)
+import colander
+from deform import widget
 
-def format_estimation(datas):
-    """
-        return the estimation related fields
-    """
-    keys = ["tva",
-            "deposit",
-            "paymentConditions",
-            "exclusions",
-            "course",
-            "displayedUnits",
-            "discountHT",
-            "expenses",
-            "paymentDisplay",]
-    return dict((key, datas.get(key)) for key in keys)
+days = (
+        ('NONE', '-'),
+        ('HOUR', u'Heure(s)'),
+        ('DAY', u'Jour(s)'),
+        ('WEEK', u'Semaine(s)'),
+        ('MONTH', u'Mois'),
+        ('FEUIL', u'Feuillet(s)'),
+        ('PACK', u'Forfait'),
+        )
 
-def format_payment_conditions(datas):
+class EstimationLine(colander.MappingSchema):
     """
-        format all payment conditions related datas
-        to fit colander schema's expected structure
+        A single estimation line
     """
-    keys = datas.keys()
-    payments = []
-    indexes = collect_indexes(keys, "description_")
-    indexes.sort()
-    for index in indexes:
-        description = datas["description_%d" % index]
-        amount = datas["amount_%d" % index]
-        paymentDate = datas["paymentDate_%s" % index]
-        payments.append(dict(description=description,
-                             amount=amount,
-                             paymentDate=paymentDate,
-                             rowIndex=index))
-    return payments
+    prestation = colander.SchemaNode(colander.String(),
+         widget=widget.TextAreaWidget(cols=80, rows=4,
+             template='autonomie:deform_templates/lineblock/prestation.mako',
+             css_class='span5'
+             ),
+         missing=u'',
+         css_class='span5')
+    cost = colander.SchemaNode(colander.String(),
+            widget=widget.TextInputWidget(
+                template='autonomie:deform_templates/lineblock/lineinput.mako',
+                ),
+            css_class='span1'
+            )
+    quantity = colander.SchemaNode(colander.String(),
+            widget=widget.TextInputWidget(
+                template='autonomie:deform_templates/lineblock/lineinput.mako',
+                ),
+            css_class='span1'
+            )
+    unity = colander.SchemaNode(
+                colander.String(),
+                widget=widget.SelectWidget(
+                    values=days,
+                    template='autonomie:deform_templates/lineblock/unity.mako',
+                    ),
+                css_class='span2'
+                )
 
-def format_datas(datas):
+class EstimationLines(colander.SequenceSchema):
     """
-        format all estimation form datas to fit the colander schema
+        Sequence of estimation lines
     """
-    log.debug("Formatting datas : %s" % (datas,))
-    tovalid = dict(estimation=format_estimation(datas),
-                   task=format_task(datas),
-                   estimation_lines=format_lines(datas),
-                   payment_conditions=format_payment_conditions(datas),
-                   )
-    return tovalid
+    estimationline = EstimationLine(
+            widget=widget.MappingWidget(
+                    template='autonomie:deform_templates/\
+lineblock/estimationline_mapping.mako',
+               item_template='autonomie:deform_templates/\
+lineblock/estimationline_mapping_item.mako'
+                     )
+            )
 
-class EstimationForm():
-    """
-        Estimation Form makes the glue beetween
-        colander (form validation),
-        sqla (database orm)
-        user-input
-        user inputted datas are formatted;
-        sent to colander;
-        once validated, returns the appropriate elements
-        to insert into the database
-    """
-    def __init__(self, schema):
-        self.schema = schema
-
-    def validate(self, datas):
+def get_deferred_choices_widget(keyword, **options):
+    @colander.deferred
+    def deferred_choices_widget(node, kw):
         """
-            Launch datas validation
+            Returns a dynamically generated Select widget
+            with choices passed at bind
         """
-        cstruct = format_datas(datas)
-        try:
-            validated = self.schema.deserialize(cstruct)
-        except colander.Invalid, e:
-            errors = e.asdict()
-            log.warn(errors)
-            raise ValidationFailure(errors)
-        return validated
+        choices = kw.get(keyword)
+        if choices:
+            wid = widget.SelectWidget(values=choices, **options)
+        else:
+            wid = widget.TextInputWidget(**options)
+        return wid
+    return deferred_choices_widget
 
-def get_estimation_line(lines, _id):
+class EstimationLinesBlock(colander.MappingSchema):
     """
-        Return an estimation_line object matching the _id
+        Fieldset containing the "Détail de la prestation" block
+        with estimation lines and all the stuff
     """
-    for line in lines:
-        if line['id'] == _id:
-            return line
-    return EstimationLine()
+    lines = EstimationLines(
+            widget=widget.SequenceWidget(
+                template='autonomie:deform_templates/\
+lineblock/estimationlines_sequence.mako',
+                item_template='autonomie:deform_templates/\
+lineblock/estimationlines_sequence_item.mako',
+                min_len=1
+                ),
+            title=u''
+            )
+    discount = colander.SchemaNode(colander.String(),
+            widget=widget.TextInputWidget(
+         template='autonomie:deform_templates/lineblock/wrappable_input.mako',
+         before='autonomie:deform_templates/lineblock/staticinput.mako',
+         before_options={'label':u'Total HT avant Remise', 'id':'linestotal'},
+         after='autonomie:deform_templates/lineblock/staticinput.mako',
+         after_options={'label':u'Total HT', 'id':'httotal'}
+                ),
+            title=u"Remise",
+            missing=u'0')
+    tva = colander.SchemaNode(colander.String(),
+            widget=get_deferred_choices_widget("tvas", css_class='span1'),
+            title=u'TVA')
 
-def merge_line(line, estimation_lines):
-    """
-        Merge a line with an estimation_line sqla object if needed
-    """
-    pass
+    expenses = colander.SchemaNode(colander.String(),
+            widget=widget.TextInputWidget(
+         template='autonomie:deform_templates/lineblock/wrappable_input.mako',
+         before='autonomie:deform_templates/lineblock/staticinput.mako',
+         before_options={'label':u'Montant TVA', 'id':'tvapart'},
+         after='autonomie:deform_templates/lineblock/staticinput.mako',
+         after_options={'label':u'Total TTC', 'id':'total'}
+                ),
+            title=u'Frais TTC',
+            missing=u'0')
 
-def merge_appstruct_todatabase(datas, task=None,
-                                      estimation=None,
-                                      estimation_lines=[],
-                                      payment_conditions=None):
+class EstimationConfiguration(colander.MappingSchema):
     """
-        Build/update sqla elements from validated datas
-        Input : datas and sqla objects
-        Output : updated sqla objects
-        Note: find a way to match estimation_lines with workline ids
+        Main fields to be configured
     """
-    estimation.name = datas['estimation']['name']
-    for line in datas['estimation_line']:
-        if line.has_key('idworkline'):
-            merge_line(line, estimation_lines)
+    phase = colander.SchemaNode(
+                colander.String(),
+                widget=get_deferred_choices_widget('phases')
+                )
+
+class EstimationNotes(colander.MappingSchema):
+    """
+        Notes
+    """
+    exclusions = colander.SchemaNode(
+                colander.String(),
+                title=u'Notes',
+                widget=widget.TextAreaWidget(css_class='span10'),
+                missing=u"")
+
+class EstimationPaymentLine(colander.MappingSchema):
+    """
+        Payment line
+    """
+    description = colander.SchemaNode(
+                            colander.String(),
+                            title=u'',
+                            widget=widget.TextInputWidget(
+            template='autonomie:deform_templates/lineblock/prestation.mako',
+            css_class='span5'),
+                            css_class='span5',)
+    paymentDate = colander.SchemaNode(
+                colander.Date(),
+                title=u'',
+                widget=widget.DateInputWidget(css_class='span2'),
+                default=datetime.date.today(),
+                css_class='span2',)
+    amount = colander.SchemaNode(
+            colander.String(),
+            title=u'',
+            widget=widget.TextInputWidget(
+                template='autonomie:deform_templates/lineblock/amount.mako',
+                css_class='span5'
+                ),
+            css_class='span2 offset2',
+            )
+
+class EstimationPaymentLines(colander.SequenceSchema):
+    """
+        Sequence of payment lines
+    """
+    line = EstimationPaymentLine(
+            widget=widget.MappingWidget(
+                    template='autonomie:deform_templates/\
+lineblock/paymentline_mapping.mako',
+               item_template='autonomie:deform_templates/\
+lineblock/paymentline_mapping_item.mako'
+)
+            )
+
+
+class EstimationPayments(colander.MappingSchema):
+    """
+        Gestion des accomptes
+    """
+    deposit = colander.SchemaNode(
+                        colander.String(),
+                        title=u"Accompte à la commande",
+                        default="0",
+                        widget=widget.SelectWidget(values=get_percents(),
+                             css_class='span1'))
+    payment_times = colander.SchemaNode(
+                        colander.String(),
+                        title=u"Paiement en ",
+                        default='1',
+                        widget = widget.SelectWidget(values=get_payment_times(),
+                                     css_class='span2'))
+    payment_lines = EstimationPaymentLines(
+               widget=widget.SequenceWidget(
+                   template='autonomie:deform_templates/\
+lineblock/paymentlines_sequence.mako',
+                   item_template='autonomie:deform_templates/\
+lineblock/paymentlines_sequence_item.mako',
+                   min_len=1
+                   ),
+               title=u''
+               )
+
+class EstimationSchema(colander.MappingSchema):
+    """
+        colander Estimation form schema
+    """
+    common = EstimationConfiguration()
+    lines = EstimationLinesBlock(title=u"Détail des prestations",
+            widget=widget.MappingWidget(
+           item_template='autonomie:deform_templates/lineblock/\
+estimationdetails_item.mako'
+            )
+            )
+    notes = EstimationNotes(title=u"Notes")
+    accounts = EstimationPayments(title=u'Conditions de paiement',
+            widget=widget.MappingWidget(
+                item_template='autonomie:deform_templates/lineblock/\
+paymentdetails_item.mako'))
+
+#def collect_indexes(keys, identifier):
+#    """
+#        return estimation line indexes
+#    """
+#    return [int(key[len(identifier):]) for key in keys if key.startswith(identifier)]
+#
+#def format_lines(datas):
+#    """
+#        get all the estimation lines configured in datas and
+#        return a colander friendly data structure
+#    """
+#    keys = datas.keys()
+#    # keys :
+#    # prestation_id
+#    # price_id
+#    # quantity_id
+#    # unity_id
+#    indexes = collect_indexes(keys, "prestation_")
+#    lines = []
+#    for index in indexes:
+#        prestation = datas['prestation_%d' % index]
+#        quantity = datas['quantity_%d' % index]
+#        price = datas['price_%d' % index]
+#        unity = datas['unity_%d' % index]
+#        lines.append(dict(description=prestation,
+#                          cost=price,
+#                          quantity=quantity,
+#                          unity=unity))
+#    return lines
+#
+#def format_task(datas):
+#    """
+#        return all the task related fields
+#    """
+#    keys = ['id_phase','description','taskDate',]
+#    return dict((key, datas.get(key)) for key in keys)
+#
+#def format_estimation(datas):
+#    """
+#        return the estimation related fields
+#    """
+#    keys = ["tva",
+#            "deposit",
+#            "paymentConditions",
+#            "exclusions",
+#            "course",
+#            "displayedUnits",
+#            "discountHT",
+#            "expenses",
+#            "paymentDisplay",]
+#    return dict((key, datas.get(key)) for key in keys)
+#
+#def format_payment_conditions(datas):
+#    """
+#        format all payment conditions related datas
+#        to fit colander schema's expected structure
+#    """
+#    keys = datas.keys()
+#    payments = []
+#    indexes = collect_indexes(keys, "description_")
+#    indexes.sort()
+#    for index in indexes:
+#        description = datas["description_%d" % index]
+#        amount = datas["amount_%d" % index]
+#        paymentDate = datas["paymentDate_%s" % index]
+#        payments.append(dict(description=description,
+#                             amount=amount,
+#                             paymentDate=paymentDate,
+#                             rowIndex=index))
+#    return payments
+#
+#def format_datas(datas):
+#    """
+#        format all estimation form datas to fit the colander schema
+#    """
+#    log.debug("Formatting datas : %s" % (datas,))
+#    tovalid = dict(estimation=format_estimation(datas),
+#                   task=format_task(datas),
+#                   estimation_lines=format_lines(datas),
+#                   payment_conditions=format_payment_conditions(datas),
+#                   )
+#    return tovalid
+#
+#class EstimationForm():
+#    """
+#        Estimation Form makes the glue beetween
+#        colander (form validation),
+#        sqla (database orm)
+#        user-input
+#        user inputted datas are formatted;
+#        sent to colander;
+#        once validated, returns the appropriate elements
+#        to insert into the database
+#    """
+#    def __init__(self, schema):
+#        self.schema = schema
+#
+#    def validate(self, datas):
+#        """
+#            Launch datas validation
+#        """
+#        cstruct = format_datas(datas)
+#        try:
+#            validated = self.schema.deserialize(cstruct)
+#        except colander.Invalid, e:
+#            errors = e.asdict()
+#            log.warn(errors)
+#            raise ValidationFailure(errors)
+#        return validated
+#
+#def get_estimation_line(lines, _id):
+#    """
+#        Return an estimation_line object matching the _id
+#    """
+#    for line in lines:
+#        if line['id'] == _id:
+#            return line
+#    return EstimationLine()
+#
+#def merge_line(line, estimation_lines):
+#    """
+#        Merge a line with an estimation_line sqla object if needed
+#    """
+#    pass
+#
+#def merge_appstruct_todatabase(datas, task=None,
+#                                      estimation=None,
+#                                      estimation_lines=[],
+#                                      payment_conditions=None):
+#    """
+#        Build/update sqla elements from validated datas
+#        Input : datas and sqla objects
+#        Output : updated sqla objects
+#        Note: find a way to match estimation_lines with workline ids
+#    """
+#    estimation.name = datas['estimation']['name']
+#    for line in datas['estimation_line']:
+#        if line.has_key('idworkline'):
+#            merge_line(line, estimation_lines)

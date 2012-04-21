5fa7366d295c7b6e322f733f5fd1833259621596
tonthon <tonthon21@gmail.com>
2012-04-21 12:34:55 +0200

    Some tests for estimation handling

--- autonomie/tests/test_estimation.py
+++ autonomie/tests/test_estimation.py
@@ -29,11 +29,13 @@ dbdatas = dict(estimation=dict(course="0",
                      {'description':'text2',
                      'cost':'20',
                      'unity':'month',
-                     'quantity':'12'},
+                     'quantity':'12',
+                     'rowIndex':'2'},
                      {'description':'text1',
                      'cost':'10',
                      'unity':'days',
-                     'quantity':'12'},
+                     'quantity':'12',
+                     'rowIndex':'1'},
                      ],
                 payment_conditions=[
                     {'description':"Début", "paymentDate":"12-12-2012",
@@ -46,7 +48,16 @@ dbdatas = dict(estimation=dict(course="0",
                 )
 datas = {'common': dict(id_phase="485",
                         taskDate="10-12-2012",
-                        description="Devis pour le client test"),
+                        description="Devis pour le client test",
+                        course="0",
+                        displayedUnits="1",),
+        'lines':dict(discountHT='20',
+                     tva="1960",
+                     expenses="150",),
+        'notes':dict(exclusions="Ne sera pas fait selon la règle"),
+        'payments':dict(paymentDisplay='1',
+                        deposit='20',),
+        'comments':dict(paymentConditions="Payer à l'heure"),
                         }
 
 #class Test(BaseTestCase):
@@ -76,5 +87,37 @@ class Test(BaseTestCase):
     def test_merge_task_in_session(self):
         from autonomie.views.forms.estimation import merge_task_in_datas
         result = merge_task_in_datas({}, dbdatas['task'])
-        expected = {'common':datas['common']}
-        self.assertEqual(result, expected)
+        for field, value in dbdatas['task'].items():
+            self.assertEqual(result['common'][field], value)
+
+    def test_merge_estimation_in_session(self):
+        from autonomie.views.forms.estimation import merge_estimation_in_datas
+        result = merge_estimation_in_datas({}, dbdatas['estimation'])
+        for field, group in (('course', 'common',), ('displayedUnits', 'common'),
+                    ('discountHT', 'lines'), ('tva', 'lines'),
+                    ('expenses', 'lines'), ('deposit', 'payments'),
+                    ('exclusions', 'notes'), ('paymentDisplay', 'payments'),
+                    ('paymentConditions', 'comments'),):
+            self.assertEqual(dbdatas['estimation'][field], result[group][field])
+
+    def test_merge_estimationlines_in_session(self):
+        from autonomie.views.forms.estimation import merge_estimationlines_in_datas
+        result = merge_estimationlines_in_datas({}, dbdatas['estimation_lines'])
+        from copy import deepcopy
+        lines = deepcopy(dbdatas['estimation_lines'])
+        lines = sorted(lines, key=lambda row:int(row['rowIndex']))
+        for line in lines:
+            del(line['rowIndex'])
+        for i, line in enumerate(lines):
+            self.assertEqual(result['lines']['lines'][i], line)
+
+    def test_merge_paymentlines_in_session(self):
+        from autonomie.views.forms.estimation import merge_paymentlines_in_datas
+        result = merge_paymentlines_in_datas({}, dbdatas['payment_conditions'])
+        from copy import deepcopy
+        lines = deepcopy(dbdatas['payment_conditions'])
+        lines = sorted(lines, key=lambda row:int(row['rowIndex']))
+        for line in lines:
+            del(line['rowIndex'])
+        for i,line in enumerate(lines):
+            self.assertEqual(result['payments']['payment_lines'][i], line)

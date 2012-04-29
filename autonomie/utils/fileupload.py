e6e7d409ffe0fd6caa157b94ca48145743ffa4f0
tonthon <tonthon21@gmail.com>
2012-04-29 22:16:36 +0200

    Adding a preview url to an existing file

--- autonomie/utils/fileupload.py
+++ autonomie/utils/fileupload.py
@@ -30,41 +30,48 @@ class FileTempStore(dict):
         implements deform.interfaces.FileUploadTempStore
     """
     session_key = 'deform_uploads'
-    def __init__(self, session, path, default_filename=None):
+    def __init__(self, session, path, url, default_filename=None):
         self.session = session
         self.store_directory = path
+        self.store_url = url
         self.default_filename = default_filename
         if not os.path.isdir(self.store_directory):
             os.system("mkdir -p %s" % self.store_directory)
 
-    def preview_url(self, name):
+    def preview_url(self, uid, filename=None):
         """
             Returns the url for the preview
         """
-        #TODO : overwrite deform's templates to add a preview link
+        print "Asking for a preview URl"
+        print "uid : %s" % uid
 
-        #filepath = os.path.join(self.store_directory, filename)
+        if not filename:
+            filename = self.get(uid, {}).get('filename')
+        filepath = os.path.join(self.store_url, filename)
+
+        print "The filepath where to find our file is : %s" % filepath
+
         #if os.path.isfile(filepath):
         #    filedata = file(filepath, 'r').read()
         #    value['fp'] = filedata
-        return None
+        return filepath
 
-    def get(self, name, default=None):
+    def get(self, uid, default=None):
         """
             Tries to get a element given by name,
             or default when it doesn't exists
         """
         try:
-            return self.__getitem__(name)
+            return self.__getitem__(uid)
         except AttributeError:
             return default
 
-    def __getitem__(self, name):
+    def __getitem__(self, uid):
         """
             Retrieves the file contents of a given file
         """
         log.debug("In __getitem__")
-        log.debug(" + name %s" % name)
+        log.debug(" + uid %s" % uid)
 
         # We erase elements in the for loop
         items = self.session[self.session_key].items()
@@ -74,24 +81,24 @@ class FileTempStore(dict):
                 log.debug("  + Expiring %s" % key)
                 del self.session[self.session_key][key]
 
-        if not name in self.session[self.session_key]:
-            raise AttributeError, "Name '{0}' does not exists".format(name)
+        if not uid in self.session[self.session_key]:
+            raise AttributeError, "Name '{0}' does not exists".format(uid)
 
         # reset last timestamp
-        self.session[self.session_key][name]['time'] = datetime.now()
+        self.session[self.session_key][uid]['time'] = datetime.now()
         self.session.persist()
 
-        value = self.session[self.session_key][name]['value']
+        value = self.session[self.session_key][uid]['value']
         log.debug(" + Value : %s" % value)
-        value['uid'] = name
+        value['uid'] = uid
         return value
 
-    def __setitem__(self, name, value):
+    def __setitem__(self, uid, value):
         """
             Saves the file contents to a file
         """
         log.debug("In __setitem__")
-        log.debug(name)
+        log.debug(uid)
         log.debug(value)
         filedata = value.get('fp')
         if filedata:
@@ -104,15 +111,16 @@ class FileTempStore(dict):
                 else:
                     fbuf.write(filedata)
             del value['fp']
+        if value.get('filename'):
+            value['preview_url'] = self.preview_url(uid, value['filename'])
 
-        self.session.setdefault(self.session_key, {})[name] = {
+        self.session.setdefault(self.session_key, {})[uid] = {
             'time': datetime.now(),
             'value': {
                 'filename': value['filename'],
                 'mimetype': value.get('mimetype'),
-                'size': value.get('size')
+                'size': value.get('size'),
+                'preview_url':value.get('preview_url')
                 }
             }
-        log.debug("The session")
-        log.debug(self.session)
         self.session.persist()

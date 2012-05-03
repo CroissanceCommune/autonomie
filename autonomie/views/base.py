dc15ac0d94aae27a08f63757786aaed7d7e2a983
tonthon <tonthon21@gmail.com>
2012-05-03 17:51:19 +0200

    task base view

--- autonomie/views/base.py
+++ autonomie/views/base.py
@@ -15,13 +15,21 @@
 """
     Base views with commonly used utilities
 """
+import logging
 from functools import partial
 
 from webhelpers import paginate
+from deform import Button
 from pyramid.httpexceptions import HTTPForbidden
+from pyramid.httpexceptions import HTTPFound
+from pyramid.url import route_path
 
 from autonomie.models import DBSESSION
+from autonomie.models.model import Phase
+from autonomie.models.model import Tva
 from autonomie.utils.views import get_page_url
+
+log = logging.getLogger(__file__)
 
 class BaseView(object):
     """
@@ -87,3 +95,141 @@ class ListView(BaseView):
                              current_page,
                              url=page_url,
                              items_per_page=items_per_page)
+
+class TaskView(BaseView):
+    """
+        BaseTask related view
+        Base object for estimation and invoice views
+    """
+    schema = None
+    add_title = u""
+    edit_title = u""
+    taskname_tmpl = u""
+    tasknumber_tmpl = u""
+    route = u""
+
+    def __init__(self, request):
+        BaseView.__init__(self, request)
+        self.company = self.get_current_company()
+        self.project = self.get_current_project(self.company)
+        self.taskid = self.request.matchdict.get('taskid')
+        self.task = self.get_task()
+        self.set_lines()
+
+    def get_task(self):
+        """
+            should return the current task
+        """
+        raise Exception("Not implemented yet")
+
+    def set_lines(self):
+        """
+            add task lines to self
+        """
+        raise Exception("Not implemented yet")
+
+    def redirect_to_view_only(self):
+        """
+            redirect the user to the view only url
+        """
+        return HTTPFound(route_path(
+                            self.route,
+                            self.request,
+                            cid=self.company.id,
+                            id=self.project.id,
+                            taskid=self.taskid,
+                            _query=dict(view='html')
+                            ))
+
+    def get_current_project(self, company):
+        """
+            Returns the current project
+        """
+        project_id = self.request.matchdict.get('id')
+        return company.get_project(project_id)
+
+    def add_default_phase(self):
+        """
+            Adds a default phase to an existing project
+        """
+        default_phase = Phase(name=u"Phase par défaut")
+        default_phase.id_project = self.project.id
+        default_phase = self.dbsession.merge(default_phase)
+        self.dbsession.flush()
+        return default_phase
+
+    def get_phases_choice(self):
+        """
+            returns the options for phase select
+        """
+        phase_choices = ((phase.id, phase.name) \
+                        for phase in self.project.phases)
+        if not self.project.phases: # On a pas de phase dans le projet
+            default_phase = self.add_default_phase()
+            phase_choices = ((default_phase.id, default_phase.name),)
+        return phase_choices
+
+    def get_sequencenumber(self):
+        """
+            set the sequence number
+            don't know really if this column matters
+        """
+        return len(self.project.estimations) + 1
+
+    def get_taskname(self):
+        """
+            set the current taskname
+        """
+        return self.taskname_tmpl.format(self.get_sequencenumber())
+
+    def get_tasknumber(self, taskDate):
+        """
+            return the task number
+        """
+        date = "{0}{1}".format(
+                               taskDate.month,
+                               str(taskDate.year)[2:])
+        pcode = self.project.code
+        ccode = self.project.client.id
+        return self.tasknumber_tmpl.format(
+                                  pcode,
+                                  ccode,
+                                  self.get_sequencenumber(),
+                                  date)
+    def get_taskstatus(self):
+        """
+            Set all status related attributes
+        """
+        caestatus = self.request.params['submit']
+        if caestatus in ('draft', 'wait'):
+            return caestatus
+        else:
+            return 'draft'
+
+
+    def get_tvas(self):
+        """
+            return all configured tva amounts
+        """
+        tvas = self.dbsession.query(Tva).all()
+        return [(tva.value, tva.name)for tva in tvas]
+
+    def get_buttons(self):
+        """
+            returns submit buttons for estimation/invoice form
+        """
+        draft = Button(name='submit',
+                    title=u"Enregistrer en tant que brouillon",
+                    type='submit',
+                    value="draft")
+        askvalidation = Button(name='submit',
+                            title=u"Demander à la CAE de valider ce document",
+                            type='submit',
+                            value="wait")
+        cancel = Button(name='cancel',
+                        title=u"Annuler",
+                        type='reset',
+                        value=u"Annuler")
+        return (draft, askvalidation, cancel,)
+
+

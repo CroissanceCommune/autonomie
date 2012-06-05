57be3a421162c47bf500af48419c302aad20bd87
tonthon <tonthon21@gmail.com>
2012-06-05 23:50:43 +0200

    Adding factories to dynamically generates acls

--- autonomie/utils/security.py
+++ autonomie/utils/security.py
@@ -6,28 +6,140 @@
 #   License: http://www.gnu.org/licenses/gpl-3.0.txt
 #
 # * Creation Date : 07-02-2012
-# * Last Modified : ven. 04 mai 2012 16:47:26 CEST
+# * Last Modified : mar. 05 juin 2012 23:48:10 CEST
 #
 # * Project : autonomie
 #
 """
     Root factory <=> Acl handling
 """
+import logging
 from pyramid.security import Allow
 from pyramid.security import Authenticated
 
-class RootFactory(object):
+from autonomie.models.model import Project
+from autonomie.models.model import Company
+from autonomie.models.model import Client
+
+log = logging.getLogger(__file__)
+
+class BaseDBFactory(object):
+    __acl__ = [(Allow, Authenticated, 'view'),]
+    dbsession = None
+
+class RootFactory(dict):
     """
        Ressource factory, returns the appropriate resource regarding
        the request object
     """
-    __acl__ = [(Allow, Authenticated, 'view'),
+    __acl__ = [
+                (Allow, Authenticated, 'view'),
                 ]
     def __init__(self, request):
         self.request = request
+        self['companies'] = CompanyFactory(self, "companies")
+        self['projects'] = ProjectFactory(self, 'projects')
+        self['clients'] = ClientFactory(self, 'clients')
 
-class Forbidden(Exception):
+class CompanyWrapper(Company):
     """
-        Forbidden exception, used to raise a forbidden action error
+        Wrap The project model to handle acls
     """
-    pass
+    @property
+    def __acl__(self):
+        acl = [(Allow, u"admin", ("view", 'edit',)),
+               (Allow, Authenticated, ('visit',)),]
+        acl.extend([(Allow, u"%s" % user.login, ("view", "edit",))
+                            for user in self.employees])
+        return acl
+
+class CompanyFactory(BaseDBFactory):
+    """
+        Handle access to a project
+    """
+    def __init__(self, parent, name):
+        self.__parent__ = parent
+        self.__name__ = name
+
+    def __getitem__(self, key):
+        """
+            Returns the traversed object
+        """
+        if self.dbsession == None:
+            raise Exception("Missing dbsession")
+        dbsession = self.dbsession()
+        obj = dbsession.query(CompanyWrapper).filter(
+                               CompanyWrapper.id==key).scalar()
+        if obj is None:
+            raise KeyError
+        obj.__parent__ = self
+        obj.__name__ = "company"
+        return obj
+
+class ProjectWrapper(Project):
+    """
+        Wrap The project model to handle acls
+    """
+    @property
+    def __acl__(self):
+        acl = [(Allow, u"admin", ("view", 'edit',)),]
+        acl.extend([(Allow, u"%s" % user.login, ("view", "edit",))
+                            for user in self.company.employees])
+        return acl
+
+class ProjectFactory(BaseDBFactory):
+    """
+        Handle access to a project
+    """
+    def __init__(self, parent, name):
+        self.__parent__ = parent
+        self.__name__ = name
+
+    def __getitem__(self, key):
+        """
+            Returns the traversed object
+        """
+        if self.dbsession == None:
+            raise Exception("Missing dbsession")
+        dbsession = self.dbsession()
+        obj = dbsession.query(ProjectWrapper).filter(
+                                               ProjectWrapper.id==key).scalar()
+        if obj is None:
+            raise KeyError
+        obj.__parent__ = self
+        obj.__name__ = "project"
+        return obj
+
+class ClientWrapper(Client):
+    """
+        Wrap The client model to handle acls
+    """
+    @property
+    def __acl__(self):
+        acl = [(Allow, u"admin", ("view", 'edit',)),]
+        acl.extend([(Allow, u"%s" % user.login, ("view", "edit",))
+                            for user in self.company.employees])
+        return acl
+
+class ClientFactory(BaseDBFactory):
+    """
+        Handle access to a client
+    """
+    def __init__(self, parent, name):
+        self.__parent__ = parent
+        self.__name__ = name
+
+    def __getitem__(self, key):
+        """
+            Returns the traversed object
+        """
+        if self.dbsession == None:
+            raise Exception("Missing dbsession")
+        dbsession = self.dbsession()
+        obj = dbsession.query(ClientWrapper).filter(
+                                             ClientWrapper.id==key).scalar()
+        if obj is None:
+            raise KeyError
+        obj.__parent__ = self
+        obj.__name__ = "client"
+        return obj

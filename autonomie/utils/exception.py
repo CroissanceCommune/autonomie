9c3f8f87b10966fa8b3fa0dcbb7c8577e38cb978
tonthon <tonthon21@gmail.com>
2012-06-05 23:42:30 +0200

    Specific exceptions

---
+++ autonomie/utils/exception.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# * File Name : exception.py
+#
+# * Copyright (C) 2010 Gaston TJEBBES <g.t@majerti.fr>
+# * Company : Majerti ( http://www.majerti.fr )
+#
+#   This software is distributed under GPLV3
+#   License: http://www.gnu.org/licenses/gpl-3.0.txt
+#
+# * Creation Date : 04-06-2012
+# * Last Modified :
+#
+# * Project :
+#
+"""
+    Specific exception
+"""
+
+class Forbidden(Exception):
+    """
+        Forbidden exception, used to raise a forbidden action error
+    """
+    pass

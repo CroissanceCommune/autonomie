d9dba9e7ec2640245462dc2c8fc916e8b8c8189c
tonthon <tonthon21@gmail.com>
2012-06-22 11:56:12 +0200

    Building menus regarding the current url with permission handling

--- autonomie/views/subscribers.py
+++ autonomie/views/subscribers.py
@@ -18,6 +18,8 @@
                if the company id is set
 """
 
+from webhelpers.html import tags
+
 from pyramid.url import route_path
 from pyramid.events import subscriber
 from pyramid.events import BeforeRender
@@ -25,6 +27,97 @@ from pyramid.events import NewRequest
 from pyramid.threadlocal import get_current_request
 
 from autonomie.i18n import translate
+from autonomie.models.model import Company
+from autonomie.utils.widgets import Menu
+from autonomie.utils.widgets import MainMenuItem
+from autonomie.utils.widgets import MenuDropDown
+from autonomie.utils.widgets import StaticWidget
+
+def get_cid(request):
+    """
+        Return the current cid from the request
+    """
+    cid = None
+    if hasattr(request, "user") and request.user and \
+                            request.user.companies == 1:
+        cid = request.user.companies[0].id
+    elif hasattr(request, "context") and hasattr(request.context,
+                                                "get_company_id"):
+        cid = request.context.get_company_id()
+    return cid
+
+def get_companies(request):
+    """
+        Return available companies from the request object
+    """
+    companies = []
+    if hasattr(request, "user") and request.user:
+        if request.user.is_admin() or request.user.is_manager():
+            dbsession = request.dbsession()
+            companies = dbsession.query(Company).all()
+        else:
+            companies = request.user.companies
+    return companies
+
+def get_company(request, cid):
+    """
+        Return the current company object
+    """
+    dbsession = request.dbsession()
+    company = dbsession.query(Company).filter(Company.id==cid).first()
+    return company
+
+def get_user_menu(request, css=None):
+    """
+        Return the menu for a common user
+    """
+    cid = get_cid(request)
+    menu = None
+    if cid:
+        menu = Menu("base/mainmenu.mako", css=css)
+        menu.add(MainMenuItem(u"Clients", "view",
+                                path="company_clients", id=cid))
+        menu.add(MainMenuItem(u"Projets", "view",
+                                path="company_projects", id=cid))
+        gestion = MenuDropDown(u"Gestion", "view")
+        gestion.add(MainMenuItem(u"Factures", "edit",
+                                path="company_invoices", id=cid))
+        gestion.add(MainMenuItem(u"Trésorerie", "edit",
+                                path="company_treasury", id=cid))
+        menu.add(gestion)
+    return menu
+
+def get_admin_menus(request):
+    """
+        Return the menu for admin or managers
+    """
+    menu = Menu("base/mainmenu.mako")
+    menu.add(MainMenuItem(u"Factures", "manage", path="invoices"))
+    menu.add(MainMenuItem(u"Configuration", "admin", path="admin_index"))
+    submenu = get_user_menu(request, "nav-pills")
+    return menu, submenu
+
+def user_menu(request):
+    """
+        Return the user config menu
+    """
+    # TODO
+    pass
+
+def company_menu(request, companies, cid):
+    """
+        Add the company choose menu
+    """
+    menu = get_company( request, cid ).name
+    if len(companies) > 1:
+        options = ((request.route_path("company", id=company.id),
+                    company.name) for company in companies)
+        default = request.route_path("company", id=cid)
+        html_attrs = {'class':'floatted company-search',
+                      'id':"company-select-menu"}
+        menu = tags.select("companies", default, options, **html_attrs)
+    menu = StaticWidget(menu, "view")
+    return menu
 
 @subscriber(BeforeRender)
 def add_menu(event):
@@ -33,45 +126,27 @@ def add_menu(event):
         if cid is not None
     """
     request = event['req']
-    cid = None
-    # We test matchdict is present : it's not when inner render call is made
-    if hasattr(request, "context") and hasattr(request.context,
-                                                "get_company_id"):
-        cid = request.context.get_company_id()
-    elif hasattr(request, "user") and request.user:
-        if len(request.user.companies):
-            cid = request.user.companies[0].id
-    menu = {}
-    if cid:
-        menu = [dict(label=u'Clients',
-                     url=route_path('company_clients',
-                                                request,
-                                                id=cid)),
-                dict(label=u"Projets",
-                     url=route_path('company_projects',
-                                                request,
-                                                id=cid)),
-                dict(label="Gestion",
-                    dropdown=[
-                    dict(label=u"Factures",
-                         url=route_path('company_invoices',
-                                    request,
-                                    id=cid)),
-                    dict(label=u"Trésorerie",
-                        url=route_path('company_treasury',
-                                    request,
-                                    id=cid)),
-                    ]),
-                dict(label=u"Paramètres",
-                     url=route_path('company',
-                                                request,
-                                                id=cid,
-                                                _query={'action':'edit'})),
-                dict(label=u"Annuaire",
-                    url=route_path('users',
-                                    request,)),
-               ]
+    cid = get_cid(request)
+    companies = get_companies(request)
+    menu = None
+    submenu = None
+    if request:
+        if request.user.is_admin() or request.user.is_manager():
+            menu, submenu = get_admin_menus(request)
+            submenu.insert(company_menu(request, companies, cid))
+        else:
+            menu = get_user_menu(request)
+            menu.add(company_menu(request, companies, cid))
+
+        menu.add(MainMenuItem(u"Annuaire", "view", path="users"))
+
+
+
         event.update({'menu':menu})
+        if submenu:
+            if len(companies) > 1:
+                event.update({'companies':companies, 'cid':cid})
+            event.update({'submenu':submenu})
 
 @subscriber(BeforeRender)
 def add_renderer_globals(event):

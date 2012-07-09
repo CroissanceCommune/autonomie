5ed593745a14e993680c4822942d39ce9479f9f4
tonthon <tonthon21@gmail.com>
2012-07-10 00:09:19 +0200

    Adding masked input for project code

--- autonomie/views/forms/project.py
+++ autonomie/views/forms/project.py
@@ -20,11 +20,13 @@ import logging
 
 from deform import widget
 
-from autonomie.views.forms.widgets import deferred_edit_widget
+from autonomie.views.forms.widgets import get_deferred_edit_widget
 from autonomie.views.forms.widgets import deferred_autocomplete_widget
 from autonomie.views.forms.widgets import get_date_input
 
 log = logging.getLogger(__name__)
+
+
 
 class ProjectSchema(colander.MappingSchema):
     """
@@ -35,7 +37,7 @@ class ProjectSchema(colander.MappingSchema):
             validator=colander.Length(max=150), css_class='floatted')
     code = colander.SchemaNode(colander.String(),
             title=u"Code du projet",
-            widget=deferred_edit_widget,
+            widget=get_deferred_edit_widget(mask='* * * *'),
             validator=colander.Length(4))
     type = colander.SchemaNode(colander.String(),
             title="Type de projet",

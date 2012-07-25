6c3141851a0fc715e15cc4a0c982b79474a7036d
tonthon <tonthon21@gmail.com>
2012-07-25 20:51:56 +0200

    Adding header file in test datas

--- sample_db/testdatas.sql
+++ sample_db/testdatas.sql
@@ -1,5 +1,5 @@
 INSERT into egw_accounts  (`account_lid`, `account_pwd`, `account_firstname` ,`account_lastname`, `account_email`) VALUES ('user1_login', '24c9e15e52afc47c225b757e7bee1f9d', 'user1_firstname', 'user1_lastname', 'user1@test.fr');
-INSERT into coop_company (`name`, `object`, `phone`, `creationDate`, `updateDate`, `IDGroup`, `IDEGWUser`, `logo`) VALUES ('company1', 'Company of user1', '0457858585', '1286804741', '1286804741', '0', '0', 'logo.jpg');
+INSERT into coop_company (`name`, `object`, `phone`, `creationDate`, `updateDate`, `IDGroup`, `IDEGWUser`, `logo`, `header`) VALUES ('company1', 'Company of user1', '0457858585', '1286804741', '1286804741', '0', '0', 'logo.png', 'header.png');
 INSERT into coop_company_employee (`IDCompany`, `IDEmployee`) VALUES ('1', '1');
 INSERT into coop_customer (`code`, `name`, `creationDate`, `updateDate`, `IDCompany`) VALUES ('C001', 'Client1', '1286804741', '1286804741', '1');
 INSERT into coop_project (`name`, `customerCode`, `code`, `definition`, `creationDate`, `updateDate`, `status`, `IDCompany`) VALUES ('Projet de test', 'C001', 'P001', 'Projet de test', '1286804741', '1286804741', '', '1');

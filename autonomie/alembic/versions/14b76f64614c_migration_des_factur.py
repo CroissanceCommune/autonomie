0d4a6d95f81cf054aeeaa58bc075e910b29783f9
tonthon <tonthon21@gmail.com>
2012-09-03 20:07:00 +0200

    Cancel invoice status migration

--- autonomie/alembic/versions/14b76f64614c_migration_des_factur.py
+++ autonomie/alembic/versions/14b76f64614c_migration_des_factur.py
@@ -19,15 +19,20 @@ from autonomie.models.task import Payment
 
 def upgrade():
     sess = DBSESSION()
-    for fact in (model.Invoice, model.CancelInvoice,):
-        for i in sess.query(fact).filter(fact.CAEStatus=='paid').all():
-            amount = i.total()
-            mode = i.paymentMode
-            date = i.statusDate
-            payment = Payment(mode=mode, amount=amount, date=date)
-            i.payments.append(payment)
-            i.CAEStatus = 'resulted'
-            sess.merge(i)
+    for i in sess.query(model.Invoice)\
+            .filter(model.Invoice.CAEStatus=='paid').all():
+        amount = i.total()
+        mode = i.paymentMode
+        date = i.statusDate
+        payment = Payment(mode=mode, amount=amount, date=date)
+        i.payments.append(payment)
+        i.CAEStatus = 'resulted'
+        sess.merge(i)
+    # We ensure all cancel invoice have a correct status
+    for i in sess.query(model.CancelInvoice)\
+        .filter(model.CancelInvoice.CAEStatus.in_(('paid', 'sent', 'recinv'))):
+        i.CAEStatus = 'valid'
+        sess.merge(i)
     sess.flush()
 
 

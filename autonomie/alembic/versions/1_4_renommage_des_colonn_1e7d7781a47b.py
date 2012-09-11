551e015461f44478d6a9ae590af36b04bfa42a7a
tonthon <tonthon21@gmail.com>
2012-09-11 17:57:42 +0200

    Ref #545: problème de migration

--- autonomie/alembic/versions/1_4_renommage_des_colonn_1e7d7781a47b.py
+++ autonomie/alembic/versions/1_4_renommage_des_colonn_1e7d7781a47b.py
@@ -12,44 +12,8 @@ down_revision = '209c0f6d7620'
 
 from alembic import op
 import sqlalchemy as sa
-
-def force_rename_table(old, new):
-    from autonomie.models import DBSESSION
-    conn = DBSESSION.connection()
-    if table_exists(old):
-        if table_exists(new):
-            op.drop_table(new)
-        op.rename_table(old, new)
-
-def table_exists(tbl):
-    from autonomie.models import DBSESSION
-    conn = DBSESSION.connection()
-    ret = False
-    try:
-        conn.execute("select * from `%s`" % tbl)
-        ret = True
-    except:
-        pass
-    return ret
-
-def rename_column(tbl, column_name, name, type_=sa.Integer, nullable=False, autoincrement=False, **kw):
-    if column_exists(tbl, column_name):
-        if autoincrement:
-            op.execute("Alter table `%s` change `%s` `%s` int(11) NOT NULL AUTO_INCREMENT;" % (tbl, column_name, name))
-        else:
-            op.alter_column(tbl, column_name, name=name, type_=type_, nullable=nullable, **kw)
-
-
-def column_exists(tbl, column_name):
-    from autonomie.models import DBSESSION
-    conn = DBSESSION.connection()
-    ret = False
-    try:
-        conn.execute("select %s from %s" % (column_name, tbl))
-        ret = True
-    except:
-        pass
-    return ret
+from autonomie.alembic.utils import force_rename_table
+from autonomie.alembic.utils import rename_column
 
 def upgrade():
     force_rename_table("egw_accounts", "accounts")
--- autonomie/alembic/versions/1_4_taux_de_tva_par_1987a6d83e5f.py
+++ autonomie/alembic/versions/1_4_taux_de_tva_par_1987a6d83e5f.py
@@ -12,43 +12,46 @@ down_revision = '1e7d7781a47b'
 
 from alembic import op
 import sqlalchemy as sa
-from autonomie.models import DBSESSION
-from autonomie.models.task.estimation import Estimation, EstimationLine
-from autonomie.models.task.invoice import Invoice, InvoiceLine
+from autonomie.models.task.estimation import Estimation
+from autonomie.models.task.invoice import Invoice
+from autonomie.alembic.utils import add_column
+from autonomie.alembic.utils import column_exists
 
 
 def upgrade():
     # Devis
-    op.add_column("estimation_line", "tva", sa.Integer, server_default=0)
+    op.add_column("estimation_line",
+                    sa.Column("tva", sa.Integer))
     # Adding tva to the lines
     op.execute("""
     update estimation_line as l join estimation as e on e.id=l.task_id set l.tva=e.tva;
     """)
     # Moving dicounts to a discount table
     for est in Estimation.query().all():
-        discount = est.discountHT
-        tva = est.tva
         id_ = est.id
         op.execute("""
-insert discount (description, amount, tva, task_id) values ("Remise", '%s', '%s', '%s')
-    """% (discount, tva, id_))
+insert discount (description, amount, tva, task_id) select "Remise", discountHT, tva, id from estimation where estimation.id='%s';
+    """% (id_))
     # Factures
-    op.add_column("invoice_line", "tva", sa.Integer, server_default=0)
+    op.add_column("invoice_line",
+                    sa.Column("tva", sa.Integer))
     # Adding tva to the lines
     op.execute("""
-    update invoice_line as l join invoice as i on i.id=l.task_id set l.tva=e.tva;
+    update invoice_line as l join invoice as i on i.id=l.task_id set l.tva=i.tva;
     """)
     # Moving discounts to discount table
     for inv in Invoice.query().all():
-        discount = inv.discountHT
-        tva = inv.tva
         id_ = inv.id
         op.execute("""
-insert discount (description, amount, tva, task_id) values ("Remise", '%s', '%s', '%s')
-    """% (discount, tva, id_))
+    insert discount (description, amount, tva, task_id) select "Remise", discountHT, tva, id from invoice where invoice.id='%s';
+    """% (id_))
     # Avoirs
-    op.execute("""
-    update cancelinvoice_line as l join cancelinvoice as i on i.id=l.task_id set l.tva=e.tva;
+    add_column("cancelinvoice_line",
+                    sa.Column("tva", sa.Integer))
+    if column_exists("cancelinvoice", "tva"):
+        # If the column exists, there may be some cancelinvoices
+        op.execute("""
+    update cancelinvoice_line as l join cancelinvoice as i on i.id=l.task_id set l.tva=i.tva;
     """)
 
 def downgrade():

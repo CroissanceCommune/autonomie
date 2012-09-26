75e59d349426e365aee1604ae067c7b5b90ca318
Julien Miotte <mike.perdide@gmail.com>
2012-09-26 12:33:32 +0200

    [REFACTOR] Refactored the conftest.py file.

--- conftest.py
+++ conftest.py
@@ -13,99 +13,127 @@
 # * Project :
 #
 import sys
-import os
+from os import system
+from os.path import dirname, join
 
-here = os.path.dirname(__file__)
+from paste.deploy.loadwsgi import appconfig
+from pyramid.config import Configurator
+from sqlalchemy import engine_from_config
 
-def coerce_datas(settings, prefix, here=None):
-    """
-        get the test db settings as a dict and format them
-    """
-    options = dict((key[len(prefix):], settings[key])
-                for key in settings if key.startswith(prefix))
-    if here:
-        options['sampledb'] = os.path.join(here, options['sampledb'])
-        options['sampledatas'] = os.path.join(here, options['sampledatas'])
-        options['updatedir'] = os.path.join(here, options['updatedir'])
-    return options
+from autonomie.models.initialize import initialize_sql
+from autonomie.tests.base import TMPDIR
+
+
+HERE = dirname(__file__)
+SETTINGS = appconfig('config:' + join(HERE, 'test.ini'), "autonomie")
+PREFIX = "testdb."
+
+OPTIONS = dict((key[len(PREFIX):], SETTINGS[key])
+            for key in SETTINGS if key.startswith(PREFIX))
+
+if HERE:
+    OPTIONS['sampledb'] = join(HERE, OPTIONS['sampledb'])
+    OPTIONS['sampledatas'] = join(HERE, OPTIONS['sampledatas'])
+    OPTIONS['updatedir'] = join(HERE, OPTIONS['updatedir'])
+
 
 def launch_cmd(cmd):
     """
         Main entry to launch os commands
     """
-    print "Launching : %s" % cmd
-    return os.system(cmd)
+    command_line = cmd.format(**OPTIONS)
+    print "Launching : %s" % command_line
+    return system(command_line)
 
-def create_sql_user(options):
+
+def launch_sql_cmd(cmd):
+    """
+        Main entry to launch sql commands
+    """
+    return launch_cmd("echo \"%s\"|{mysql_cmd}" % cmd)
+
+
+def create_sql_user():
     """
         Create the sql test user
     """
-    launch_cmd("echo 'CREATE USER {user}@localhost;' | {mysql_cmd}".format(
-                                                                **options))
+    launch_sql_cmd('CREATE USER {user}@localhost;')
 
-def create_test_db(options):
+
+def create_test_db():
     """
         Create the test database and grant rights
     """
-    launch_cmd("echo 'CREATE DATABASE IF NOT EXISTS `{db}` character \
-set utf8;' | {mysql_cmd}".format(**options))
+    launch_sql_cmd('CREATE DATABASE IF NOT EXISTS "{db}" character set utf8;')
 
-def grant_user(options):
+
+def grant_user():
     """
         grant privileges on the test db to the test user
     """
-    launch_cmd("echo 'GRANT ALL PRIVILEGES on {db}.* to {user}@localhost \
-identified by \"{password}\"; \
-FLUSH PRIVILEGES;' | {mysql_cmd}".format(**options))
+    launch_sql_cmd("GRANT ALL PRIVILEGES on {db}.* to {user}@localhost "
+                   "identified by '{password}'; FLUSH PRIVILEGES;")
 
-def dump_sample(options):
+
+def dump_sample():
     """
         dump sample datas in our test database
     """
-    launch_cmd("{mysql_cmd} {db} < {sampledatas}".format(**options))
+    launch_cmd("{mysql_cmd} {db} < {sampledatas}")
 
-def drop_db(options):
+
+def drop_db():
     """
         drop the test database
     """
-    cmd = "echo \"echo 'drop database {db};' | {mysql_cmd}\" | at now".format(**options)
+    cmd = "echo \"echo 'drop database {db};' | {mysql_cmd}\" | at now"
     launch_cmd(cmd)
 
-def drop_sql_datas(settings, prefix):
+
+def drop_sql_datas():
     """
         Drop all the test db created stuff
     """
     print "Dropping test datas : keeping it clean"
-    options = coerce_datas(settings, prefix)
-    drop_db(options)
+    drop_db()
 
-def test_connect(options):
+
+def test_connect():
     """
         test the db connection
     """
-    cmd = "echo 'quit' | {mysql_cmd}".format(**options)
+    cmd = "echo 'quit' | {mysql_cmd}"
     ret_code = launch_cmd(cmd)
+
     if ret_code != 0:
         err_str = """
-    actual err_code = %s \n
-    You need to configure the test.ini file to allow the test
-setup script to build the mysql test database like the following : \n
-    testdb.mysql_cmd=mysql --defaults-file=/etc/mysql/debian.cnf\n
-    or \n
-    testdb.mysql_cmd=mysql -uroot -p<password>\n
-""" % ret_code
+    actual err_code = %s\n
+    You need to configure the test.ini file so that this script can connect\n
+    to the Mysql database:
+
+        ...\n
+        testdb.mysql_cmd=mysql --defaults-file=/etc/mysql/debian.cnf\n
+        ...\n
+    or\n
+        ...\n
+        testdb.mysql_cmd=mysql -uroot -p<password>\n
+        ...\n
+        """ % ret_code
+
         print err_str
         sys.exit(err_str)
 
-def initialize_test_database(settings):
+
+def initialize_test_database():
     """
         dump sample datas as a test database
     """
     print "Initializing test database"
-    test_connect(settings)
-    create_sql_user(settings)
-    create_test_db(settings)
-    grant_user(settings)
+    test_connect()
+    create_sql_user()
+    create_test_db()
+    grant_user()
+
 
 def migrate_database(settings, key):
     """
@@ -113,6 +141,7 @@ def migrate_database(settings, key):
     """
     from autonomie.scripts.migrate import upgrade
     upgrade(settings[key])
+
 
 def pytest_sessionstart():
     """
@@ -122,34 +151,25 @@ def pytest_sessionstart():
 
     # Only run database setup on master (in case of xdist/multiproc mode)
     if not hasattr(config, 'slaveinput'):
-        from autonomie.models.initialize import initialize_sql
-        from pyramid.config import Configurator
-        from paste.deploy.loadwsgi import appconfig
-        from sqlalchemy import engine_from_config
+        initialize_test_database()
 
-        settings = appconfig('config:' + os.path.join(here,
-                                                      'test.ini'),
-                             "autonomie")
-        test_settings = coerce_datas(settings, "testdb.", here)
-        initialize_test_database(test_settings)
-        engine = engine_from_config(settings, prefix='sqlalchemy.')
+        engine = engine_from_config(SETTINGS, prefix='sqlalchemy.')
         print 'Creating the tables on the test database %s' % engine
-        config = Configurator(settings=settings)
+
+        config = Configurator(settings=SETTINGS)
         config.begin()
         config.commit()
+
         initialize_sql(engine)
-        dump_sample(test_settings)
+        dump_sample()
+
 
 def pytest_sessionfinish():
     """
         py.test teardown
     """
     from py.test import config
+
     if not hasattr(config, 'slaveinput'):
-        from paste.deploy.loadwsgi import appconfig
-        settings = appconfig('config:' + os.path.join(here,
-                                                      'test.ini'),
-                             "autonomie")
-        drop_sql_datas(settings, 'testdb.')
-        from autonomie.tests.base import TMPDIR
+        drop_sql_datas()
         launch_cmd('rm -rf "%s"' % TMPDIR)

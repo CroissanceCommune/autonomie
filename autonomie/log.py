f049f47c90e1dbfc0bf771dcacd81cbeb2af6c05
tonthon <tonthon21@gmail.com>
2012-10-03 19:16:41 +0200

    Adding custom logging handlers with client ip and user login

---
+++ autonomie/log.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# * File Name :
+#
+# * Copyright (C) 2012 Gaston TJEBBES <g.t@majerti.fr>
+# * Company : Majerti ( http://www.majerti.fr )
+#
+#   This software is distributed under GPLV3
+#   License: http://www.gnu.org/licenses/gpl-3.0.txt
+#
+# * Creation Date : 03-10-2012
+# * Last Modified :
+#
+# * Project :
+#
+"""
+    Logging utility to allow logging ips into custom logs
+"""
+import logging
+from pyramid.threadlocal import get_current_request
+
+def get_user(request):
+    """
+        Return the current user or anonymous
+    """
+    if request and request.user:
+        return request.user.login
+    else:
+        return "Anonymous"
+
+def get_ip(request):
+    """
+        Return the client's ip or None
+    """
+    if request:
+        return request.client_addr
+    else:
+        return "None"
+
+class CustomFileHandler(logging.FileHandler, object):
+    """
+        CustomeFile Handler allowing to add ip and username in logs
+    """
+    def emit(self, record):
+        request = get_current_request()
+        record.ip = get_ip(request)
+        record.user = get_user(request)
+        super(CustomFileHandler, self).emit(record)
+
+class CustomStreamHandler(logging.StreamHandler, object):
+    """
+        CustomeStream Handler allowing to add ip and username in logs
+    """
+    def emit(self, record):
+        request = get_current_request()
+        record.ip = get_ip(request)
+        record.user = get_user(request)
+        super(CustomStreamHandler, self).emit(record)

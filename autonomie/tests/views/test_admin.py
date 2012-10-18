f4d320c167f0cd9b6539913088a04e8d118ac848
tonthon <tonthon21@gmail.com>
2012-10-18 22:14:08 +0200

    Testing main admin view

--- autonomie/tests/views/test_admin.py
+++ autonomie/tests/views/test_admin.py
@@ -13,7 +13,8 @@
 # * Project :
 #
 from autonomie.models import tva
-from autonomie.views.admin import AdminTva
+from autonomie.models.config import get_config
+from autonomie.views.admin import AdminTva, AdminMain
 from autonomie.tests.base import BaseFunctionnalTest
 
 class TestTvaView(BaseFunctionnalTest):
@@ -27,3 +28,14 @@ class TestTvaView(BaseFunctionnalTest):
         appstruct = {'tvas':[{'name':"19,6%", 'value':1960, "default":1}]}
         view.submit_success(appstruct)
         self.assertEqual(len(self.session.query(tva.Tva).all()), 1)
+
+class TestMainView(BaseFunctionnalTest):
+    def test_success(self):
+        self.config.add_route('admin_main', '/')
+        appstruct = {"site":{'welcome':'testvalue'},
+                     "document":{'footertitle':'testvalue2'}}
+        view = AdminMain(self.get_csrf_request())
+        view.submit_success(appstruct)
+        print get_config()
+        self.assertTrue(get_config()['welcome'] == u'testvalue')
+        self.assertTrue(get_config()['coop_pdffootertitle'] == u'testvalue2')

c63ae21d1627abaef84196fc10dc33e869762370
G.Tjebbes <g.t@majerti.fr>
2012-11-14 14:29:22 +0100

    Setting the request cookie for items_per_page value

--- autonomie/views/forms/lists.py
+++ autonomie/views/forms/lists.py
@@ -22,6 +22,7 @@ ITEMS_PER_PAGE_OPTIONS = (('10 par page', u'10'),
                           ('50 par page', u'50'),
                           ('Tous', u'10000'))
 
+
 @colander.deferred
 def deferred_default_sort(node, kw):
     return kw['default_sort']
@@ -37,6 +38,32 @@ def deferred_default_direction(node, kw):
     return kw['default_direction']
 
 
+@colander.deferred
+def deferred_items_per_page(node, kw):
+    """
+        get the default items_per_page value from the request cookies
+    """
+    req = kw['request']
+    item_per_page = req.cookies.get('items_per_page', 10)
+    try:
+        item_per_page = int(item_per_page)
+    except:
+        item_per_page = 10
+    return item_per_page
+
+
+@colander.deferred
+def deferred_items_per_page_validator(node, kw):
+    """
+        Return a fake validator that only set a cookie in the session
+    """
+    req = kw['request']
+    def set_cookie(node, value):
+        req.response.set_cookie("items_per_page", str(value))
+        req.cookies['items_per_page'] = str(value)
+    return set_cookie
+
+
 class BaseListsSchema(colander.MappingSchema):
     """
         Base List schema used to validate the common list view options
@@ -45,7 +72,9 @@ class BaseListsSchema(colander.MappingSchema):
         sort parameters
     """
     search = colander.SchemaNode(colander.String(), missing=u'')
-    items_per_page = colander.SchemaNode(colander.Integer(), missing=10)
+    items_per_page = colander.SchemaNode(colander.Integer(),
+                               missing=deferred_items_per_page,
+                               validator=deferred_items_per_page_validator)
     page = colander.SchemaNode(colander.Integer(), missing=0)
     sort = colander.SchemaNode(colander.String(),
                                missing=deferred_default_sort,

5aee8a9b50808ac57b7722c68e678c729122aa0a
G.Tjebbes <g.t@majerti.fr>
2012-12-21 01:14:45 +0100

    Adding foreign keys with constraints to the documents

--- autonomie/alembic/versions/1_5_multiple_client__1cc9ff114346.py
+++ autonomie/alembic/versions/1_5_multiple_client__1cc9ff114346.py
@@ -12,12 +12,19 @@ down_revision = '70853b55768c'
 
 from alembic import op
 import sqlalchemy as sa
+from autonomie.models import DBSESSION
 
+TABLENAMES = ('estimation', 'invoice', 'cancelinvoice')
+
+def purge_line_type(factory):
+    for line in factory.query():
+        if line.task is None:
+            DBSESSION().delete(line)
 
 def upgrade():
-    from autonomie.models import DBSESSION
     from autonomie.models.project import Project
     from autonomie.models.client import Client
+
     for proj in DBSESSION().query(Project):
         try:
             client = Client.get(proj.client_id)
@@ -27,6 +34,26 @@ def upgrade():
         except:
             continue
 
+    # Adding foreign keys constraints to existing tables
+    # First removing wrong references
+    from autonomie.models.task import EstimationLine
+    from autonomie.models.task import PaymentLine
+    from autonomie.models.task import InvoiceLine
+    from autonomie.models.task import CancelInvoiceLine
+
+    for i in (EstimationLine, PaymentLine, InvoiceLine, CancelInvoiceLine):
+        purge_line_type(i)
+
+    # Adding the keys
+    for i in TABLENAMES:
+        op.create_foreign_key("fk_%s" % i, "%s_line" % i, i,
+            ['task_id'], ['id'], ondelete="CASCADE")
+    op.create_foreign_key("fk_estimation_payment", "estimation_payment",
+            "estimation", ['task_id'], ['id'], ondelete="CASCADE")
+
 
 def downgrade():
     op.execute("DELETE from project_client;")
+    for i in TABLENAMES:
+        op.execute("alter table %s_line drop foreign key fk_%s;" % (i,i))
+    op.execute("alter table estimation_payment drop foreign key fk_estimation_payment")

a48f75a702641243653eea0723c00c0e84ef7818
G.Tjebbes <g.t@majerti.fr>
2013-01-03 11:09:02 +0100

    Tests

--- autonomie/tests/tasks/test_compute.py
+++ autonomie/tests/tasks/test_compute.py
@@ -12,17 +12,15 @@
 #
 # * Project :
 #
-from mock import MagicMock
 from autonomie.tests.base import BaseTestCase
 from autonomie.models.task.compute import (LineCompute, TaskCompute,
-        EstimationCompute)
+        EstimationCompute, InvoiceCompute)
 
-TASK = {"expenses":1500, "manualDeliverables":1, "deposit":20}
+TASK = {"expenses":1500,}
 LINES = [{'cost':10025, 'tva':1960, 'quantity':1.25},
          {'cost':7500,  'tva':1960, 'quantity':3},
          {'cost':-5200, 'tva':1960, 'quantity':1}]
 DISCOUNTS = [{'amount':2000, 'tva':1960}]
-PAYMENT_LINES = [{"amount":1000}, {"amount":1000}, {"amount":150}]
 
 # Values:
 #         the money values are represented *100
@@ -34,11 +32,11 @@ PAYMENT_LINES = [{"amount":1000}, {"amount":1000}, {"amount":150}]
 # so it fits the limit case
 #
 # Line totals should be floats (here they are *100)
-EST_LINES_TOTAL_HT = (12531.25, 22500, -5200)
-EST_LINES_TVAS = (2456.125, 4410, -1019.2)
+TASK_LINES_TOTAL_HT = (12531.25, 22500, -5200)
+TASK_LINES_TVAS = (2456.125, 4410, -1019.2)
 
-LINES_TOTAL_HT = sum(EST_LINES_TOTAL_HT)
-LINES_TOTAL_TVAS = sum(EST_LINES_TVAS)
+LINES_TOTAL_HT = sum(TASK_LINES_TOTAL_HT)
+LINES_TOTAL_TVAS = sum(TASK_LINES_TVAS)
 
 DISCOUNT_TOTAL_HT = sum([d['amount']for d in DISCOUNTS])
 DISCOUNT_TVAS = (392,)
@@ -47,36 +45,38 @@ DISCOUNT_TOTAL_TVAS = sum(DISCOUNT_TVAS)
 HT_TOTAL =  int(LINES_TOTAL_HT - DISCOUNT_TOTAL_HT)
 TVA = int(LINES_TOTAL_TVAS - DISCOUNT_TOTAL_TVAS)
 
-# EST_TOTAL = lines + tva + expenses rounded
-EST_TOTAL = HT_TOTAL + TVA + TASK['expenses']
-EST_DEPOSIT_HT = int(HT_TOTAL * TASK['deposit'] / 100.0)
-EST_DEPOSIT = int(EST_TOTAL * TASK['deposit'] / 100.0)
-PAYMENTSSUM = sum([p['amount'] for p in PAYMENT_LINES[:-1]])
+# TASK_TOTAL = lines + tva + expenses rounded
+TASK_TOTAL = HT_TOTAL + TVA + TASK['expenses']
 
-EST_SOLD = EST_TOTAL - EST_DEPOSIT - PAYMENTSSUM
+print "TASK_TOTAL : %s" % TASK_TOTAL
 
-print "EST_DEPOSIT_HT : %s" % EST_DEPOSIT_HT
-print "PAYMENTSSUM : %s" % PAYMENTSSUM
-print "EST_DEPOSIT : %s" % EST_DEPOSIT
-print "EST_TOTAL : %s" % EST_TOTAL
 
-print "EST_SOLD %s" % EST_SOLD
+class Dummy(object):
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
-class DummyLine(LineCompute):
+
+class DummyLine(Dummy, LineCompute):
     """
         Dummy line model
     """
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    pass
 
-class DummyTask(TaskCompute):
+
+class DummyTask(Dummy, TaskCompute):
     """
         Dummy task model
     """
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    pass
+
+
+class DummyInvoice(Dummy, InvoiceCompute):
+    pass
+
+
+class DummyEstimation(Dummy, EstimationCompute):
+    pass
 
 
 def get_lines(datas=LINES):
@@ -85,29 +85,16 @@ def get_lines(datas=LINES):
         lines.append(DummyLine(**line))
     return lines
 
-def get_task():
-    t = DummyTask(**TASK)
+def get_task(factory=DummyTask):
+    t = factory(**TASK)
     t.lines = get_lines()
     t.discounts = get_lines(DISCOUNTS)
     return t
 
-
-class TestComputing(BaseTestCase):
-    def test_line_compute(self):
-        for index, line_obj in enumerate(get_lines()):
-            self.assertEqual(line_obj.total_ht(), EST_LINES_TOTAL_HT[index])
-            self.assertEqual(line_obj.total(), EST_LINES_TOTAL_HT[index] +\
-                    EST_LINES_TVAS[index])
-
+class TestTaskCompute(BaseTestCase):
     def test_lines_total_ht(self):
         task = get_task()
         self.assertEqual(task.lines_total_ht(), LINES_TOTAL_HT)
-
-    def test_discount_compute(self):
-        for index, line_obj in enumerate(get_lines(DISCOUNTS)):
-            self.assertEqual(line_obj.total_ht(), DISCOUNTS[index]['amount'])
-            self.assertEqual(line_obj.total(), DISCOUNTS[index]['amount'] \
-                    + DISCOUNT_TVAS[index])
 
     def test_discounts_total_ht(self):
         task = get_task()
@@ -117,13 +104,6 @@ class TestComputing(BaseTestCase):
         est = get_task()
         self.assertEqual(est.total_ht(),
                     HT_TOTAL)
-
-    def test_tva_amount(self):
-        # cf #501
-        line = DummyLine(cost=5010, quantity=1, tva=1960)
-        self.assertEqual(line.tva_amount(), 981.96)
-        task = get_task()
-        self.assertEqual(task.tva_amount(), TVA)
 
     def test_get_tvas(self):
         task = TaskCompute()
@@ -135,6 +115,13 @@ class TestComputing(BaseTestCase):
         self.assertEqual(tvas.keys(), [1960, 550])
         self.assertEqual(tvas[1960], 3920)
         self.assertEqual(tvas[550], 2134)
+
+    def test_tva_amount(self):
+        # cf #501
+        line = DummyLine(cost=5010, quantity=1, tva=1960)
+        self.assertEqual(line.tva_amount(), 981.96)
+        task = get_task()
+        self.assertEqual(task.tva_amount(), TVA)
 
     def test_total_ttc(self):
         task = TaskCompute()
@@ -148,4 +135,84 @@ class TestComputing(BaseTestCase):
 
     def test_total(self):
         est = get_task()
-        self.assertEqual(est.total(), EST_TOTAL)
+        self.assertEqual(est.total(), TASK_TOTAL)
+
+    def test_no_tva(self):
+        line = DummyLine(cost=3500, tva=-100)
+        task = DummyTask(lines=[line])
+        self.assertTrue(task.no_tva())
+
+        line = DummyLine(cost=3500, tva=0)
+        task = DummyTask(lines=[line])
+        self.assertFalse(task.no_tva())
+
+        line = DummyLine(cost=3500, tva=100)
+        task.lines.append(line)
+        self.assertFalse(task.no_tva())
+
+
+class TestInvoiceCompute(BaseTestCase):
+    def getOne(self):
+        task = DummyInvoice()
+        task.expenses = 0
+        task.payments = [Dummy(amount=1500), Dummy(amount=1000)]
+        task.lines =  [DummyLine(cost=6000, quantity=1, tva=0)]
+        task.discounts = []
+        return task
+
+    def test_paid(self):
+        task = self.getOne()
+        self.assertEqual(task.paid(), 2500)
+
+    def test_topay(self):
+        task = self.getOne()
+        self.assertEqual(task.topay(), 3500)
+
+
+class TestEstimationCompute(BaseTestCase):
+    def getOne(self):
+        task = DummyEstimation()
+        task.expenses = 0
+        task.deposit = 20
+        task.manualDeliverables = 0
+        task.lines = [DummyLine(cost=10000, quantity=1, tva=0)]
+        task.discounts = []
+        task.payment_lines = [Dummy(amount=1000), Dummy(amount=1000),
+                              Dummy(amount=150)]
+        return task
+
+    def test_deposit_amount(self):
+        task = self.getOne()
+        self.assertEqual(task.deposit_amount(), 2000)
+
+    def test_get_nb_payment_lines(self):
+        task = self.getOne()
+        self.assertEqual(task.get_nb_payment_lines(), 3)
+
+    def test_paymentline_amount(self):
+        task = self.getOne()
+        self.assertEqual(task.paymentline_amount(), 2666)
+
+    def test_deposit_amount_ttc(self):
+        task = self.getOne()
+        self.assertEqual(task.deposit_amount_ttc(), 2000)
+
+    def test_sold(self):
+        task = self.getOne()
+        self.assertEqual(task.sold(), 2668)
+
+
+class TestLineCompute(BaseTestCase):
+    def test_line_compute(self):
+        for index, line_obj in enumerate(get_lines()):
+            self.assertEqual(line_obj.total_ht(), TASK_LINES_TOTAL_HT[index])
+            self.assertEqual(line_obj.total(), TASK_LINES_TOTAL_HT[index] +\
+                    TASK_LINES_TVAS[index])
+
+    def test_discount_compute(self):
+        for index, line_obj in enumerate(get_lines(DISCOUNTS)):
+            self.assertEqual(line_obj.total_ht(), DISCOUNTS[index]['amount'])
+            self.assertEqual(line_obj.total(), DISCOUNTS[index]['amount'] \
+                    + DISCOUNT_TVAS[index])
+
+

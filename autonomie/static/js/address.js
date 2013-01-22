393be985e3871ba895b8499832fddafa9ce4d17a
G.Tjebbes <g.t@majerti.fr>
2013-01-22 16:47:24 +0100

    Refactor address module to avoid Js conflicts
    duplicate.js and address.js were providing the same function, causing
    conflicts. Now we store all the address handling into an object
    "address_handler".

--- autonomie/static/js/address.js
+++ autonomie/static/js/address.js
@@ -9,57 +9,55 @@
  *
  */
 var ALREADY_LOADED = new Object();
-
-function fetch_client(client_id){
-  return $.ajax({
+var address_handler = {
+  already_loaded: new Object(),
+  fetch_client: function(client_id) {
+    var this_ = this;
+    return $.ajax({
          type: 'GET',
          url:"/clients/" + client_id,
          dataType: 'json',
          success: function(data) {
-           ALREADY_LOADED[client_id] = data;
+           this_.already_loaded[client_id] = data;
          },
          async: false
-  });
-}
-function get_select_client(){
-  return $("select[name=client_id]");
-}
-function get_client(client_id){
-  /*
-   * Fetch the client object through the json api
-   */
-  if (client_id in ALREADY_LOADED){
-    return ALREADY_LOADED[client_id];
-  }else{
-    fetch_client(client_id);
-    return ALREADY_LOADED[client_id];
+    });
+  },
+  getEl: function() {
+    return $("select[name=client_id]");
+  },
+  get: function(client_id) {
+    if (! (client_id in this.already_loaded)){
+      this.fetch_client(client_id);
+    }
+    return this.already_loaded[client_id];
+  },
+  selected: function() {
+    var client_id = this.getEl().children('option:selected').val();
+    if (client_id !== '0'){
+      return this.get(client_id);
+    }else{
+      return null;
+    }
+  },
+  address: function(client) {
+    return client.full_address;
+  },
+  set: function(client) {
+    var address_obj = $('textarea[name=address]');
+    address_obj.val(this.address(client));
+  },
+  change: function(){
+    var client_obj = this.selected();
+    if (client_obj !== null){
+      this.set(client_obj);
+    }
   }
-}
-function getCurrentClient(){
-  var client_id = get_select_client().children('option:selected').val();
-  if (client_id !== '0'){
-    return get_client(client_id);
-  }else{
-    return "0";
-  }
-}
-function getClientAddress(client){
-  return client.full_address;
-}
-function set_client_address(client){
-  /*
-   * Set the selected client's address
-   */
-  var address_obj = $('textarea[name=address]');
-  address_obj.val(getClientAddress(client));
-}
+};
 $(function(){
-  get_select_client().change(
+  address_handler.getEl().change(
     function(){
-      var client_obj = getCurrentClient();
-      if (client_obj !== "0"){
-        set_client_address(client_obj);
-      }
+      address_handler.change();
     }
   );
 });

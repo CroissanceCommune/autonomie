444cbe3ceea1993805024156e13efe77b02b7330
G.Tjebbes <g.t@majerti.fr>
2013-02-01 13:48:24 +0100

    Javascript resources definition reviewed

--- autonomie/resources.py
+++ autonomie/resources.py
@@ -26,7 +26,6 @@ from js.jqueryui import ui_sortable
 from js.jqueryui import ui_autocomplete
 from js.jqueryui import ui_datepicker_fr
 from js.jquery_maskedinput import jquery_maskedinput
-#from js.jqueryui import bootstrap as jqueryui_bootstrap_theme
 from js.jquery_form import jquery_form
 from js.deform_bootstrap import deform_bootstrap_js
 from js.chosen import chosen_jquery
@@ -36,27 +35,42 @@ lib_autonomie = Library("fanstatic", "static")
 
 main = Resource(lib_autonomie, "js/main.js", depends=[ui_dialog, ui_sortable])
 
-underscore = Resource(lib_autonomie, "js/underscore.js",
+_hogan = Resource(lib_autonomie, "js/hogan-2.0.0.js")
+_underscore = Resource(lib_autonomie, "js/underscore.js",
         minified="js/underscore-min.js")
-backbone = Resource(lib_autonomie, "js/backbone.js",
-        minified="js/backbone-min.js",
-        depends=[underscore])
-backbone_marionnette = Resource(lib_autonomie, "js/backbone.marionette.js",
-        minified="js/backbone.marionette.min.js",
-        depends=[backbone])
+_backbone = Resource(lib_autonomie, "js/backbone.js",
+        minified="js/backbone-min.js", depends=[_underscore])
+_backbone_marionnette = Resource(lib_autonomie, "js/backbone.marionette.js",
+        minified="js/backbone.marionette.min.js", depends=[_backbone])
+_backbone_validation = Resource(lib_autonomie, "js/backbone-validation.js",
+        minified="js/backbone-validation-min.js", depends=[_backbone])
 
-dom = Resource(lib_autonomie, "js/dom.js", depends=[jquery])
-duplicate = Resource(lib_autonomie, "js/duplicate.js", depends=[jquery])
-math = Resource(lib_autonomie, "js/math.js")
+_backbone_tuning = Resource(lib_autonomie, "js/backbone-tuning.js",
+        depends=[_backbone_marionnette, _hogan, main])
+_backbone_validation_bootstrap = Resource(lib_autonomie,
+        "js/backbone-validation-bootstrap.js", depends=[_backbone_validation])
 
+backbone = Group([_backbone_validation_bootstrap,
+                  _backbone_tuning])
+
+templates = Resource(lib_autonomie,
+        "js/template.js", depends=[_hogan])
 jquery_tmpl = Resource(lib_autonomie, "js/jquery.tmpl.min.js",
-        depends=[jquery])
-discount = Resource(lib_autonomie, "js/discount.js",
-        depends=[backbone_marionnette])
-address = Resource(lib_autonomie, "js/address.js")
-date = Resource(lib_autonomie, "js/date.js")
-task = Resource(lib_autonomie, "js/task.js", depends=[date, jquery_tmpl,
-                            address, discount, math, duplicate, dom])
+                  depends=[jquery])
+
+
+_date = Resource(lib_autonomie, "js/date.js")
+_dom = Resource(lib_autonomie, "js/dom.js", depends=[jquery])
+_math = Resource(lib_autonomie, "js/math.js")
+tools = Group([_dom, _math, _date])
+
+
+duplicate = Resource(lib_autonomie, "js/duplicate.js", depends=[jquery])
+discount = Resource(lib_autonomie, "js/discount.js", depends=[backbone])
+address = Resource(lib_autonomie, "js/address.js", depends=[jquery])
+task = Resource(lib_autonomie, "js/task.js",
+        depends=[tools, jquery_tmpl, address, discount, duplicate, backbone,
+            templates])
 
 # Main javascript requirements
 main_js = Group([main,

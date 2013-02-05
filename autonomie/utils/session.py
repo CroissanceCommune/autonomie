c465ddd8123729ad6e7a48351f76ed7d25c34804
G.Tjebbes <g.t@majerti.fr>
2013-02-05 10:28:55 +0100

    Fix security issue on authentification

--- autonomie/utils/session.py
+++ autonomie/utils/session.py
@@ -26,24 +26,25 @@ def get_session_factory(settings):
         Wrap the beaker session factory to add longtimeout support
     """
     factory = session_factory_from_settings(settings)
-    class AutonomieSessionObject(factory):
-        """
-            Our pyramid session object
-        """
-        _longtimeout = factory._options.pop("longtimeout")
-        def __init__(self, request):
-            options = self._options.copy()
-            if "remember_me" in request.cookies.keys():
-                options['timeout'] = self._longtimeout
-            SessionObject.__init__(self, request.environ, **options)
-            def session_callback(request, response):
-                exception = getattr(request, 'exception', None)
-                if (exception is None or self._cookie_on_exception
-                    and self.accessed()):
-                    self.persist()
-                    headers = self.__dict__['_headers']
-                    if headers['set_cookie'] and headers['cookie_out']:
-                        response.headerlist.append(
-                            ('Set-Cookie', headers['cookie_out']))
-            request.add_response_callback(session_callback)
-    return AutonomieSessionObject
+    return factory
+#    class AutonomieSessionObject(factory):
+#        """
+#            Our pyramid session object
+#        """
+#        _longtimeout = factory._options.pop("longtimeout")
+#        def __init__(self, request):
+#            options = self._options.copy()
+#            if "remember_me" in request.cookies.keys():
+#                options['timeout'] = self._longtimeout
+#            SessionObject.__init__(self, request.environ, **options)
+#            def session_callback(request, response):
+#                exception = getattr(request, 'exception', None)
+#                if (exception is None or self._cookie_on_exception
+#                    and self.accessed()):
+#                    self.persist()
+#                    headers = self.__dict__['_headers']
+#                    if headers['set_cookie'] and headers['cookie_out']:
+#                        response.headerlist.append(
+#                            ('Set-Cookie', headers['cookie_out']))
+#            request.add_response_callback(session_callback)
+#    return AutonomieSessionObject

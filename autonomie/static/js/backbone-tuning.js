513a52ba3bc768d9c7882f1e259d99263e194b4e
G.Tjebbes <g.t@majerti.fr>
2013-03-11 11:21:07 +0100

    Remove the old modal region (didn't work fine)
    It's now replaced By the Popup region

--- autonomie/static/js/backbone-tuning.js
+++ autonomie/static/js/backbone-tuning.js
@@ -12,23 +12,3 @@
 Backbone.Marionette.Renderer.render = function(template_obj, data){
   return template_obj.render(data);
 };
-var ModalRegion = Backbone.Marionette.Region.extend({
-  el: "modal",
-    constructor: function(){
-      Backbone.Marionette.Region.prototype.constructor.apply(this, arguments);
-      this.on("show", this.showModal, this);
-      setPopUp(this.el, "Title");
-    },
-    getEl: function(selector){
-      var $el = $("#" + selector);
-      $el.on("hidden", this.close);
-      return $el;
-    },
-    showModal: function(view){
-      view.on("close", this.hideModal, this);
-      this.$el.dialog('open');
-    },
-    hideModal: function(){
-      this.$el.dialog('close');
-    }
-});

c8c13c1935f49afb902b358b12e249390fb81b5b
G.Tjebbes <g.t@majerti.fr>
2013-04-30 16:35:43 +0200

    Fix #800 : Update the excel export
    We now add a new worksheet to store the km expenses board and we
    modified the main expensesheet output to fit user needs

--- autonomie/utils/export.py
+++ autonomie/utils/export.py
@@ -19,9 +19,13 @@
 import openpyxl
 from openpyxl.style import Color, Fill
 import cStringIO as StringIO
+from string import ascii_uppercase
 
 from autonomie.utils.ascii import force_ascii
 from autonomie.utils.math_utils import integer_to_amount
+from autonomie.models.treasury import ExpenseType
+from autonomie.models.treasury import ExpenseKmType
+from autonomie.models.treasury import ExpenseTelType
 
 
 LETTERS = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',)
@@ -38,26 +42,68 @@ class ExcelExpense(object):
     """
         Wrapper for excel export of an expense object
     """
-    expense_columns = [('Date', 'date', None, 'A', 'A'),
-            ('Code analytique', 'code', None, 'B', 'C'),
-            ('Description', 'description', None, 'D', 'G'),
-            ('HT', 'ht', integer_to_amount, 'H', 'H'),
-            ('TVA', 'tva', integer_to_amount, 'I', 'I'),
-            ('Total', 'total', integer_to_amount, 'J', 'J', '0.00')]
-
-    expensekm_columns = [('Date', 'date', None, 'A', 'A'),
-            ('Code analytique', 'code', None, 'B', 'C'),
-            ('Prestation', 'description', None, 'D', 'F'),
-            ('Départ', 'start', None, 'G', 'G'),
-            ('Arrivée', 'end', None, 'H', 'H'),
-            ('Kms', 'km', integer_to_amount, 'I', 'I'),
-            ('Indemnités', 'total', integer_to_amount, 'J', 'J', '0.00')]
+    expensekm_columns = [
+    {'label':u'Date', 'key':'date', 'letter': 'A'},
+    {'label':u'Type de véhicule', 'key':'vehicle', 'letter':'B',
+        'last_letter':'C'},
+    {'label':u'Lieu de départ', 'key':'start', 'letter':'D',
+        'last_letter':'E'},
+    {'label':u"Lieu d'arrivée", 'key':'end', 'letter':'F',
+        'last_letter':'G'},
+    {'label':u'Description/Mission', 'key':'description', 'letter':'H',
+        'last_letter':'J'},
+    {'label':'Nombre de kms', 'key':'km', 'formatter':integer_to_amount,
+        'letter':'K'},
+    {'label':'Indemnités', 'key':'total', 'formatter':integer_to_amount,
+        'letter':'L', 'number_format':'0.00'}]
 
     def __init__(self, expensesheet):
         self.book = openpyxl.workbook.Workbook()
-        self.worksheet = self.book.worksheets[0]
         self.model = expensesheet
-        self.index = 1
+        self.internal_columns = []
+        self.activity_columns = []
+        self.build_columns(self.internal_columns)
+        self.build_columns(self.activity_columns, internal=False)
+        self.index = 2
+
+    def build_columns(self, columns, internal=True):
+        """
+            Retrieve all columns and define a global column attribute
+        """
+        columns.append({'label':'Date', 'key':'date'})
+        columns.append({'label':'Description', 'key':'description',
+                                                                'width':3})
+        # Telephonic fees are only available as internal expenses
+        if internal:
+            teltype = ExpenseTelType.query().first()
+            if teltype:
+                columns.append({'label':"Téléphonie", 'code':teltype.code})
+        kmtype = ExpenseKmType.query().first()
+        if kmtype:
+            columns.append({'label':"Frais de déplacement",
+                'code':kmtype.code})
+        for type_ in ExpenseType.query().filter(ExpenseType.type=="expense"):
+            # Here's a hack to allow to group km fee types and displacement fees
+            if type_.code != kmtype.code:
+                columns.append({'label':type_.label, 'code':type_.code})
+        columns.append({'label':'Tva', 'key':'tva',
+            'formatter':integer_to_amount})
+        columns.append({'label':'Total', 'key':'total',
+            'formatter':integer_to_amount, 'number_format':'0.00'})
+
+        # We set the appropriate letter to each column
+        index = 0
+        for col in columns:
+            letter = ascii_uppercase[index]
+            width = col.get('width')
+            if width:
+                last_letter = ascii_uppercase[index+width]
+                index += width + 1
+            else:
+                last_letter = letter
+                index += 1
+            col['letter'] = letter
+            col['last_letter'] = last_letter
 
     def get_merged_cells(self, start, end):
         """
@@ -74,7 +120,7 @@ class ExcelExpense(object):
 
     def write_code(self):
         """
-            write the company code in the heade
+            write the company code in the header
         """
         code = self.model.company.code_compta
         if not code:
@@ -109,54 +155,96 @@ class ExcelExpense(object):
         cell.value = period
         self.index += 2
 
-    def write_table(self, columns, lines):
+    def get_column_cell(self, column):
         """
-            write a table with headers and content
-            :param columns: list of (label, model attribute, formatter) where
-                formatter could be None
-            :params lines: list of models to be written
+            Return the cell corresponding to a given column
+        """
+        letter = column['letter']
+        last_letter = column.get('last_letter', letter)
+        return self.get_merged_cells(letter, last_letter)
+
+    def write_table_header(self, columns):
+        """
+            Write the table's header and its subheader
         """
         for column in columns:
-            cell = self.get_merged_cells(column[3], column[4])
+            cell = self.get_column_cell(column)
             cell.style.fill.fill_type = Fill.FILL_SOLID
             cell.style.fill.start_color.index = Color.header
             cell.style.font.bold = True
-            cell.value = column[0]
+            cell.value = column['label']
         self.index += 1
+        for column in columns:
+            cell = self.get_column_cell(column)
+            cell.style.font.bold = True
+            cell.value = column.get('code', '')
+        self.index += 1
+
+    def get_cell_val(self, line, column):
+        """
+            return the value for a given cell for the current line
+        """
+        if column.has_key('key'):
+            val = getattr(line, column['key'], '')
+            formatter = column.get('formatter')
+            if val and formatter:
+                val = formatter(val)
+        elif column['code'] == line.type_object.code:
+            if hasattr(line, 'ht'):
+                val = integer_to_amount(line.ht)
+            else:
+                val = integer_to_amount(line.total)
+        else:
+            val = ""
+        return val
+
+    def write_table(self, columns, lines):
+        """
+            write a table with headers and content
+            :param columns: list of dict
+            :params lines: list of models to be written
+        """
+        self.write_table_header(columns)
         for line in lines:
-            for column, letter in zip(columns, LETTERS):
-                cell = self.get_merged_cells(column[3], column[4])
-                val = getattr(line, column[1])
-                if column[2] is not None:
-                    val = column[2](val)
-                cell.value = val
-                if len(column) == 6:
-                    cell.style.number_format.format_code = column[5]
+            for column in columns:
+                cell = self.get_column_cell(column)
+                cell.value = self.get_cell_val(line, column)
+                if column.has_key('number_format'):
+                    cell.style.number_format.format_code = \
+                            column['number_format']
             self.index += 1
 
-        cell = self.get_merged_cells('A', 'I')
-        cell.value = 'Total'
-        cell.style.font.bold = True
-        self.set_color(cell, Color.footer)
-        cell = self.get_merged_cells('J', 'J')
-        cell.style.font.bold = True
-        cell.value = integer_to_amount(sum([line.total for line in lines]))
-        cell.style.number_format.format_code = '0.00'
-        self.set_color(cell, Color.footer)
+        for column in columns:
+            cell = self.get_column_cell(column)
+            cell.style.font.bold = True
+            self.set_color(cell, Color.footer)
+            cell.style.number_format.format_code = '0.00'
+            if column.has_key('code'):
+                val = sum([line.ht for line in lines \
+                                     if line.type_object.code==column['code']])
+                cell.value = integer_to_amount(val)
+            elif column.get('key') == 'description':
+                cell.value = "Totaux"
+            elif column.get('key') == 'tva':
+                cell.value = integer_to_amount(
+                        sum([getattr(line, 'tva', 0) for line in lines]))
+                cell.style.number_format.format_code = '0.00'
+            elif column.get('key') == 'total':
+                cell.value = integer_to_amount(
+                        sum([line.total for line in lines]))
+                cell.style.number_format.format_code = '0.00'
         self.index += 4
 
-    def write_expense_table(self, category):
+    def write_expense_table(self, category, columns):
         """
             write expenses tables for the given category
         """
         lines = [line for line in self.model.lines
                             if line.category == category]
-        columns = self.expense_columns
-        self.write_table(columns, lines)
         kmlines = [lin for lin in self.model.kmlines
                             if lin.category == category]
-        columns = self.expensekm_columns
-        self.write_table(columns, kmlines)
+        lines.extend(kmlines)
+        self.write_table(columns, lines)
         self.index += 2
 
     def write_full_line(self, txt, start="A", end="J"):
@@ -176,7 +264,7 @@ class ExcelExpense(object):
 PAR MOIS)"
         cell = self.write_full_line(txt)
         cell.style.font.color.index = Color.Crimson
-        self.write_expense_table('1')
+        self.write_expense_table('1', self.internal_columns)
 
     def write_activity_expenses(self):
         """
@@ -187,7 +275,7 @@ PAR MOIS)"
 CLIENTS"
         cell = self.write_full_line(txt)
         cell.style.font.color.index = Color.Crimson
-        self.write_expense_table('2')
+        self.write_expense_table('2', self.activity_columns)
 
     def write_total(self):
         """
@@ -219,13 +307,36 @@ CLIENTS"
                 start_column=4,
                 end_column=6)
 
+    def write_km_book(self):
+        """
+            Write the km book associated to this expenses
+        """
+        self.index = 3
+        user = self.model.user
+        title = u"Tableau de bord kilométrique de {0} {1}".\
+                format(user.lastname, user.firstname)
+        cell = self.write_full_line(title)
+        cell.style.font.bold = True
+        cell.style.font.size = 24
+        # index has already been increased
+        row_dim = self.worksheet.row_dimensions.get(self.index -1 )
+        row_dim.height = 30
+        self.index += 2
+
+        self.write_table(self.expensekm_columns, self.model.kmlines)
+
     def render(self):
         """
             Return the current excel export as a String buffer (StringIO)
         """
+        self.worksheet = self.book.worksheets[0]
+        self.worksheet.title = "NDF"
         cell = self.write_full_line(u"Feuille de notes de frais")
         cell.style.font.bold = True
         cell.style.font.size = 24
+        # index has already been increased
+        row_dim = self.worksheet.row_dimensions.get(self.index -1 )
+        row_dim.height = 30
         self.index += 2
 
         self.write_code()
@@ -236,7 +347,16 @@ CLIENTS"
         self.write_total()
         self.write_accord()
 
-        for let in LETTERS:
+        for let in ascii_uppercase:
+            col_dim = self.worksheet.column_dimensions.get(let)
+            if col_dim:
+                col_dim.width = 13
+
+        self.worksheet = self.book.create_sheet()
+        self.worksheet.title = u"Journal de bord"
+        self.write_km_book()
+
+        for let in ascii_uppercase:
             col_dim = self.worksheet.column_dimensions.get(let)
             if col_dim:
                 col_dim.width = 13

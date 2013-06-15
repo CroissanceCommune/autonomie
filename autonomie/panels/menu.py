a555c21f7294e41061361b638b29d58e84a0bad4
G.Tjebbes <g.t@majerti.fr>
2013-06-15 17:22:02 +0200

    Move the menu construction to a panel
    Previously the menu was set as a request attribute and was rendered in
    the mako base template. All this was done using hand made solutions,
    that leaked serialisation.
    
    Now the menus are still added in the base mako template, but the
    construction and parameter passing is done more cleanly (in my POV)
    thanks to pyramid_layout's panels.

---
+++ autonomie/panels/menu.py
@@ -0,0 +1,288 @@
+# -*- coding: utf-8 -*-
+# * File Name : menu.py
+#
+# * Copyright (C) 2013 Gaston TJEBBES <g.t@majerti.fr>
+# * Company : Majerti ( http://www.majerti.fr )
+#
+#   This software is distributed under GPLV3
+#   License: http://www.gnu.org/licenses/gpl-3.0.txt
+#
+# * Creation Date : 11-06-2013
+# * Last Modified :
+#
+# * Project :
+#
+"""
+    Panels for the top main menus
+
+    A common user has his company menu with clients, projects ...
+    A manager or an admin has an admin menu and eventually a usermenu if he's
+    consulting a companyr's account
+
+    each user has his own menu with preferences, logout, holidays declaration
+"""
+import logging
+
+from webhelpers.html import tags
+from webhelpers.html import HTML
+from pyramid.security import has_permission
+
+from autonomie.models.company import Company
+
+
+log = logging.getLogger(__name__)
+
+
+class Item(dict):
+    """
+        A single menu item
+
+        {'label': the label to display,
+         'icon': the icon,
+         'href': the link}
+    """
+    __type__ = "item"
+
+
+class HtmlItem(dict):
+    """
+        A static html item that's used to carry html generated code
+        {'html': the html code}
+    """
+    __type__ = 'static'
+
+
+class Menu(dict):
+    """
+        A menu object
+    """
+    def __init__(self, css=None, **elements):
+        dict.__init__(self, **elements)
+        self.css = css
+        self.items = []
+
+    def add_item(self, label, icon="", href=""):
+        """
+            Usefull shortcut to add an item to the menu
+        """
+        self.add(Item(label=label, icon=icon, href=href))
+
+    def add(self, element):
+        """
+            Add an element to the menu
+        """
+        self.items.append(element)
+
+    def insert(self, element):
+        """
+            Insert an element at the begining of the menu
+        """
+        self.items.insert(0, element)
+
+    def __nonzero__(self):
+        return self.items != []
+
+
+class DropDown(Menu):
+    """
+        A dropdown menu
+    """
+    __type__ = "dropdown"
+
+
+def get_cid(request):
+    """
+        Extract the current company id from the request, if there is one
+    """
+    cid = None
+    if len(request.user.companies) == 1 and request.user.is_contractor():
+        cid = request.user.companies[0].id
+    # The requests context provide a get_company_id utility that allows to
+    # retrieve the concerned company
+    elif hasattr(request, "context") and hasattr(request.context,
+                                                "get_company_id"):
+        cid = request.context.get_company_id()
+    return cid
+
+
+def get_companies(request):
+    """
+        Retrieve the companies the current user has access to
+    """
+    companies = []
+    if request.user.is_admin() or request.user.is_manager():
+        companies = Company.query().all()
+    else:
+        companies = request.user.companies
+    return companies
+
+
+def get_company(request, cid):
+    """
+        Retrieve the current company object
+    """
+    if not hasattr(request, "_company"):
+        company = Company.get(cid)
+        request._company = company
+    return request._company
+
+
+def get_company_menu(request, cid, css=None):
+    """
+        Build the usermenu
+    """
+    menu = Menu(css=css)
+    href = request.route_path("company_clients", id=cid)
+    menu.add_item(u"Clients", icon="icon-user", href=href)
+
+    href = request.route_path("company_projects", id=cid)
+    menu.add_item(u"Projets", icon="icon-folder-open", href=href)
+
+    gestion = DropDown(label=u"Gestion")
+
+    href = request.route_path("company_invoices", id=cid)
+    gestion.add_item(u"Factures", icon="icon-list-alt", href=href)
+
+    href = request.route_path("treasury", id=cid)
+    gestion.add_item(u"Trésorerie", icon="icon-info-sign", href=href)
+
+    href = request.route_path("incomestatement", id=cid)
+    gestion.add_item(u"Compte de résultat", icon="icon-info-sign",
+            href=href)
+
+    href = request.route_path("salarysheet", id=cid)
+    gestion.add_item(u"Bulletin de salaire", icon="icon-info-sign",
+            href=href)
+
+    href = request.route_path("commercial_handling", id=cid)
+    gestion.add_item(u"Gestion commerciale", icon="icon-list-alt",
+            href=href)
+    menu.add(gestion)
+
+    params = DropDown(label=u"Paramètres")
+
+    href = request.route_path("company", id=cid)
+    params.add_item(u"Paramètres", icon="icon-cog", href=href)
+
+    href = request.route_path("company_expenses", id=cid)
+    params.add_item(u"Notes de frais", icon="icon-cog", href=href)
+
+    menu.add(params)
+    return menu
+
+
+def get_admin_menus(request):
+    """
+        Build the admin menu
+    """
+    menu = Menu()
+    href = request.route_path("invoices")
+    menu.add_item(u"Factures", icon="icon-list-alt", href=href)
+    href = request.route_path("holidays")
+    menu.add_item(u"Congés", icon="icon-plane", href=href)
+
+    if has_permission("admin", request.context, request):
+        href = request.route_path("admin_index")
+        menu.add_item(u"Configuration", icon="icon-cog", href=href)
+    return menu
+
+
+def company_choice(request, companies, cid):
+    """
+        Add the company choose menu
+    """
+    if request.context.__name__ == 'company':
+        options = ((request.current_route_path(id=company.id),
+                company.name) for company in companies)
+        default = request.current_route_path(id=cid)
+    else:
+        options = ((request.route_path("company", id=company.id),
+                company.name) for company in companies)
+        default = request.route_path("company", id=cid)
+    html_attrs = {'class': 'floatted company-search',
+                    'id': "company-select-menu"}
+    html_code = HTML.li(
+            tags.select("companies", default, options, **html_attrs))
+    return HtmlItem(html=html_code)
+
+def get_usermenu(request):
+    """
+        Return the user menu (My account, holidays ...)
+    """
+    menu = Menu()
+    href = request.route_path('account')
+    menu.add_item(u"Mon compte", icon='ui-icon ui-icon-gear', href=href)
+
+    href = request.route_path('user_holidays', id=request.user.id)
+    menu.add_item(u"Mes congés", icon="icon-plane", href=href)
+
+    href = request.route_path("logout")
+    menu.add_item(u"Déconnexion", icon="ui-icon ui-icon-close", href=href)
+    return menu
+
+
+def menu_panel(context, request):
+    """
+        Top menu panel
+    """
+    # If we've no user in the current request, we don't return anything
+    if not getattr(request, 'user'):
+        return {}
+
+    # If we don't have view perm, go on, there is nothing to do
+    if not has_permission("view", request.context, request):
+        return {}
+
+    cid = get_cid(request)
+    if request.user.is_admin() or request.user.is_manager():
+        menu = get_admin_menus(request)
+    elif cid:
+        menu = get_company_menu(request, cid)
+        companies = get_companies(request)
+        # If there is more than 1 company accessible for the current user, we
+        # provide a usefull dropdown menu
+        if len(companies) > 1:
+            menu.insert(company_choice(request, companies, cid))
+    else:
+        return {}
+
+    href = request.route_path("users")
+    menu.add_item(u"Annuaire", icon="icon-book", href=href)
+    ret_dict = {'menu': menu, "usermenu": get_usermenu(request)}
+    return ret_dict
+
+
+def submenu_panel(context, request):
+    """
+        Submenu panel
+    """
+    # If we've no user in the current request, we don't return anything
+    if not getattr(request, 'user'):
+        return {}
+    # If we don't have view perm, go on, there is nothing to do
+    if not has_permission("view", request.context, request):
+        return {}
+
+    # There are no submenus for non admins
+    if not request.user.is_admin() and not request.user.is_manager():
+        return {}
+
+    cid = get_cid(request)
+    if not cid:
+        return {}
+    submenu = get_company_menu(request, cid, "nav-pills")
+    if submenu:
+        companies = get_companies(request)
+        if len(companies) > 1:
+            submenu.insert(company_choice(request, companies, cid))
+
+    return {"submenu": submenu}
+
+
+def includeme(config):
+    """
+        Pyramid's inclusion mechanism
+    """
+    config.add_panel(menu_panel, 'menu', renderer='panels/menu.mako')
+    config.add_panel(submenu_panel, 'submenu', renderer='panels/menu.mako')
+
--- autonomie/views/subscribers.py
+++ autonomie/views/subscribers.py
@@ -18,19 +18,12 @@
     Add a translation stuff to the templating context
 """
 import logging
-from webhelpers.html import tags
-from webhelpers.html import HTML
 
 from pyramid.events import BeforeRender
 from pyramid.events import NewRequest
 from pyramid.threadlocal import get_current_request
 
 from autonomie.i18n import translate
-from autonomie.models.company import Company
-from autonomie.utils.widgets import Menu
-from autonomie.utils.widgets import MainMenuItem
-from autonomie.utils.widgets import MenuDropDown
-from autonomie.utils.widgets import StaticWidget
 from autonomie.utils.widgets import ActionMenu
 from autonomie.resources import main_js
 from autonomie.views.render_api import api
@@ -38,164 +31,11 @@ from autonomie.views.render_api import api
 log = logging.getLogger(__name__)
 
 
-def get_cid(request):
-    """
-        Return the current cid from the request
-    """
-    cid = None
-    if len(request.user.companies) == 1 and request.user.is_contractor():
-        cid = request.user.companies[0].id
-    elif hasattr(request, "context") and hasattr(request.context,
-                                                "get_company_id"):
-        cid = request.context.get_company_id()
-    return cid
-
-
-def get_companies(request):
-    """
-        Return available companies from the request object
-    """
-    companies = []
-    if request.user.is_admin() or request.user.is_manager():
-        companies = Company.query().all()
-    else:
-        companies = request.user.companies
-    return companies
-
-
-def get_company(request, cid):
-    """
-        Return the current company object
-    """
-    if not hasattr(request, "_company"):
-        company = Company.get(cid)
-        request._company = company
-    return request._company
-
-
-def get_user_menu(cid, css=None):
-    """
-        Return the menu for a common user
-    """
-    menu = None
-    if cid:
-        menu = Menu("base/mainmenu.mako", css=css)
-        menu.add(MainMenuItem(u"Clients", "view",
-            icon="icon-white icon-user",
-            path="company_clients",
-            id=cid))
-        menu.add(MainMenuItem(u"Projets", "view",
-            icon="icon-white icon-folder-open",
-            path="company_projects",
-            id=cid))
-        gestion = MenuDropDown(u"Gestion", "view")
-        gestion.add(MainMenuItem(u"Factures", "view",
-            icon="icon-list-alt",
-            path="company_invoices",
-            id=cid))
-#        gestion.add(MainMenuItem(u"Trésorerie", "view",
-#            icon="icon-info-sign",
-#            path="treasury",
-#            id=cid))
-#        gestion.add(MainMenuItem(u"Compte de résultat", "view",
-#            icon="icon-info-sign",
-#            path="incomestatement",
-#            id=cid))
-#        gestion.add(MainMenuItem(u"Bulletin de salaire",
-#            icon="icon-info-sign",
-#            path="salarysheet",
-#            id=cid))
-        gestion.add(MainMenuItem(u"Gestion commerciale",
-            icon="icon-list-alt",
-            path="commercial_handling",
-            id=cid))
-        menu.add(gestion)
-        params = MenuDropDown(u"Paramètres", "view")
-        params.add(MainMenuItem(u"Paramètres", "view",
-            icon="icon-cog",
-                                path="company", id=cid))
-        params.add(MainMenuItem(u"Notes de frais", "view",
-            icon="icon-cog",
-            path="company_expenses", id=cid))
-        menu.add(params)
-    return menu
-
-
-def get_admin_menus(cid):
-    """
-        Return the menu for admin or managers
-    """
-    menu = Menu("base/mainmenu.mako")
-    menu.add(MainMenuItem(u"Factures", "manage", path="invoices",
-        icon="icon-white icon-list-alt"))
-    menu.add(MainMenuItem(u"Congés", "manage", path="holidays",
-        icon="icon-white icon-plane"
-        ))
-    menu.add(MainMenuItem(u"Configuration", "admin", path="admin_index",
-        icon="icon-white icon-cog"))
-    submenu = get_user_menu(cid, "nav-pills")
-    return menu, submenu
-
-
-def company_menu(request, companies, cid):
-    """
-        Add the company choose menu
-    """
-    menu = HTML.li(HTML.h2(get_company(request, cid).name))
-    menu = MainMenuItem(
-        get_company(request, cid).name, "view",
-        path="company", id=cid, _query=dict(action="index")
-    )
-    if len(companies) > 1:
-        if request.context.__name__ == 'company':
-            options = ((request.current_route_path(id=company.id),
-                    company.name) for company in companies)
-            default = request.current_route_path(id=cid)
-        else:
-            options = ((request.route_path("company", id=company.id),
-                    company.name) for company in companies)
-            default = request.route_path("company", id=cid)
-        html_attrs = {'class': 'floatted company-search',
-                      'id': "company-select-menu"}
-        menu = HTML.li(
-                tags.select("companies", default, options, **html_attrs))
-        menu = StaticWidget(menu, "view")
-    return menu
-
-
-def add_menu(event):
-    """
-        Add the menu to the returned datas (before rendering)
-        if cid is not None
-    """
-    request = event['req']
-    menu = None
-    submenu = None
-    if request and hasattr(request, 'user') and request.user:
-        cid = get_cid(request)
-        if request.user.is_admin() or request.user.is_manager():
-            menu, submenu = get_admin_menus(cid)
-        elif cid:
-            menu = get_user_menu(cid)
-            companies = get_companies(request)
-            if len(companies) > 1:
-                menu.insert(company_menu(request, companies, cid))
-
-        if menu:
-            menu.add(MainMenuItem(u"Annuaire", "view",
-                        icon="icon-white icon-book", path="users"))
-            event.update({'menu': menu})
-        if submenu:
-            companies = get_companies(request)
-            submenu.insert(company_menu(request, companies, cid))
-            event.update({'submenu': submenu})
-
-
 def add_translation(event):
     """
         Add a translation func to the templating context
     """
-    request = event['req']
+    request = event.get('req')
     if not request:
         request = get_current_request()
     event['_'] = request.translate
@@ -251,7 +91,7 @@ def includeme(config):
     """
         Bind the subscribers to the pyramid events
     """
-    for before in (add_menu, add_translation, add_api, add_main_js):
+    for before in (add_translation, add_api, add_main_js):
         config.add_subscriber(before, BeforeRender)
 
     for new_req in (log_request, add_request_attributes):

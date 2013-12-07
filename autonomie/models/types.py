670886cfb08869b346f8bde704f38d664c6a40ad
G.Tjebbes <g.t@majerti.fr>
2013-12-07 15:03:09 +0100

    Add a custom column type storing a dict as json
    Here we implement a typical custom sqlalchemy column type.
    We also bind a mutable on to it that handles the detection of
    modifications (and force sqlalchemy to fire sql queries).

--- autonomie/models/types.py
+++ autonomie/models/types.py
@@ -27,10 +27,15 @@
 """
 import time
 import datetime
+import simplejson as json
 
-from sqlalchemy.types import TypeDecorator
-from sqlalchemy.types import Integer as Integer_type
-from sqlalchemy.types import String as String_type
+from sqlalchemy.ext.mutable import Mutable
+from sqlalchemy.types import (
+    TypeDecorator,
+    Integer as Integer_type,
+    String as String_type,
+    Text as Text_type,
+    )
 
 from autonomie.models.utils import format_to_taskdate
 from autonomie.models.utils import format_from_taskdate
@@ -128,3 +133,65 @@ class CustomInteger(TypeDecorator):
         if isinstance(value, long):
             value = int(value)
         return value
+
+
+class JsonEncodedDict(TypeDecorator):
+    """
+    Stores a dict as a json string in the database
+    """
+    impl = Text_type
+
+    def process_bind_param(self, value, dialect):
+        """
+            Process params when setting the value
+        """
+        if value is not None:
+            value = json.dumps(value)
+        return value
+
+    def process_result_value(self, value, dialect):
+        """
+            Processing the value when getting the value
+        """
+        if value is not None:
+            value = json.loads(value)
+        return value
+
+
+class MutableDict(Mutable, dict):
+    """
+        Allows sqlalchemy to check if a data has changed in our dict
+        If not used, the dbsession will never detect modifications
+
+        Note : only associating a value to one key will work (no subdict
+            handling)
+    """
+    @classmethod
+    def coerce(cls, key, value):
+        "Convert plain dictionaries to MutableDict."
+
+        if not isinstance(value, MutableDict):
+            if isinstance(value, dict):
+                return MutableDict(value)
+
+            # this call will raise ValueError
+            return Mutable.coerce(key, value)
+        else:
+            return value
+
+    def __setitem__(self, key, value):
+        "Detect dictionary set events and emit change events."
+
+        dict.__setitem__(self, key, value)
+        self.changed()
+
+    def __delitem__(self, key):
+        "Detect dictionary del events and emit change events."
+
+        dict.__delitem__(self, key)
+        self.changed()
+
+# Here we always associate our MutableDict to our JsonEncodedDict column type
+# If a column is of type JsonEncodedDict, its value will be casted as a
+# mutabledict that will signify modifications on setitem and delitem
+MutableDict.associate_with(JsonEncodedDict)

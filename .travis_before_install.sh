1baa19653d5fbc598de977a9c913f1dc3d4c31af
Feth AREZKI <feth@tuttu.info>
2013-12-10 17:30:22 +0100

    travis/mysql: try with root user

--- .travis_before_install.sh
+++ .travis_before_install.sh
@@ -4,5 +4,5 @@ set -x
 
 sudo apt-get --reinstall install -qq language-pack-en language-pack-fr
 
-mysql -e 'create database egw;'
-mysql -e 'grant all privileges on egw.* to egw@localhost identified by "egw";'
+echo "USE mysql;\nUPDATE user SET password=PASSWORD('egw') WHERE user='egw';\nFLUSH PRIVILEGES;\n" | mysql -u root
+echo "create database egw;\ngrant all privileges on egw.* to egw@localhost identified by 'egw';\nFLUSH PRIVILEGES;\n" | mysql -u root

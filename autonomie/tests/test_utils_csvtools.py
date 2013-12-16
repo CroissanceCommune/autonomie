f8a625ccea5bc51eabe87802a28ddb1bf9d38282
G.Tjebbes <g.t@majerti.fr>
2013-12-16 08:19:11 +0100

    Fix tests : Ref #51

--- autonomie/tests/test_utils_csvtools.py
+++ autonomie/tests/test_utils_csvtools.py
@@ -34,21 +34,17 @@ from autonomie.models import DBBASE
 
 class DummyModel(DBBASE):
     __tablename__ = 'test'
-    id = Column('id', Integer, primary_key=True)
+    id = Column('id', Integer, primary_key=True,
+            info={'options':{'csv_exclude':True}})
     col1 = Column(String(8))
     col2 = Column(String(8), info={'label':"Column 2"})
 
 
 class TestCsvTools(unittest.TestCase):
 
-    def test_collect_labels(self):
-        labels = csvtools.collect_labels(DummyModel)
-        expected_labels = ["id", "col1", "Column 2"]
-        self.assertTrue(set(labels).difference(set(expected_labels)) == set([]))
-
-    def test_collect_keys(self):
-        keys = csvtools.collect_keys(DummyModel)
-        expected_keys = ['id', 'col1', 'col2']
+    def test_collect_headers(self):
+        keys = csvtools.collect_headers(DummyModel)
+        expected_keys = [('col1', 'col1'), ('col2', 'Column 2')]
         self.assertTrue(set(keys).difference(set(expected_keys)) == set([]))
 
     def test_should_be_exported(self):

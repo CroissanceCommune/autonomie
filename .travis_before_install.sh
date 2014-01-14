a06e795e7b6ee8045d085c9975b9abc41c76efa4
G.Tjebbes <g.t@majerti.fr>
2014-01-14 16:16:32 +0100

    Using system package doesn't work, trying symlinking

--- .travis_before_install.sh
+++ .travis_before_install.sh
@@ -3,9 +3,12 @@
 set -x
 
 sudo apt-get update -qq
-sudo apt-get install -qq build-essential libjpeg-dev libfreetype6 libfreetype6-dev zlib1g-dev python-imaging
 sudo apt-get --reinstall install -qq language-pack-en language-pack-fr
 export TZ="Europe/Paris"
+sudo apt-get install -qq libjpeg8 libjpeg8-dev libfreetype6 libfreetype6-dev zlib1g-dev tree
+sudo ln -s /usr/lib/`uname -i`-linux-gnu/libjpeg.so.8 ~/virtualenv/python2.7/lib/
+sudo ln -s /usr/lib/`uname -i`-linux-gnu/libfreetype.so ~/virtualenv/python2.7/lib/
+sudo ln -s /usr/lib/`uname -i`-linux-gnu/libz.so ~/virtualenv/python2.7/lib/
 
 echo "USE mysql;\nUPDATE user SET password=PASSWORD('autonomie') WHERE user='autonomie';\nFLUSH PRIVILEGES;\n" | mysql -u root
 echo "create database autonomie;\ngrant all privileges on autonomie.* to autonomie@localhost identified by 'autonomie';\nFLUSH PRIVILEGES;\n" | mysql -u root

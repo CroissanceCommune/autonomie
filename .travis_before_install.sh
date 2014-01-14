7e1a10d6765e52bb209804384810d2e059b4f6df
G.Tjebbes <g.t@majerti.fr>
2014-01-14 15:42:06 +0100

    Use separate requirements file for travis
    PIL needs png support for handling png files.
    Since we use this functionality in our tests, and since travis doesn't
    seem to install PIL correctly, we try to use the python-imaging deb
    package.

--- .travis_before_install.sh
+++ .travis_before_install.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+sudo apt-get update -qq
+sudo apt-get install -qq build-essential libjpeg-dev libfreetype6 libfreetype6-dev zlib1g-dev python-imaging
 sudo apt-get --reinstall install -qq language-pack-en language-pack-fr
 
 echo "USE mysql;\nUPDATE user SET password=PASSWORD('autonomie') WHERE user='autonomie';\nFLUSH PRIVILEGES;\n" | mysql -u root

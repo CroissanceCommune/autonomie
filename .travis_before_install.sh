fce5f38afae6a77479b7060aa65742c96a360e5f
G.Tjebbes <g.t@majerti.fr>
2014-01-14 15:52:04 +0100

    Forcing french locales for tests
    since we use hardcoded timestamps (and their associated datetime object)
    in our tests, we need to have the appropriate timezone set.

--- .travis_before_install.sh
+++ .travis_before_install.sh
@@ -5,6 +5,7 @@ set -x
 sudo apt-get update -qq
 sudo apt-get install -qq build-essential libjpeg-dev libfreetype6 libfreetype6-dev zlib1g-dev python-imaging
 sudo apt-get --reinstall install -qq language-pack-en language-pack-fr
+export TZ="Europe/Paris"
 
 echo "USE mysql;\nUPDATE user SET password=PASSWORD('autonomie') WHERE user='autonomie';\nFLUSH PRIVILEGES;\n" | mysql -u root
 echo "create database autonomie;\ngrant all privileges on autonomie.* to autonomie@localhost identified by 'autonomie';\nFLUSH PRIVILEGES;\n" | mysql -u root

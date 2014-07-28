a72a00b998dc66519f2ca1f9aeb0a7d5250e8c77
G.Tjebbes <g.t@majerti.fr>
2014-07-29 00:23:52 +0200

    More generic list view base class
    To be able to use the same search functionnalities from within a
    listview and a csv/xls export view

--- autonomie/views/__init__.py
+++ autonomie/views/__init__.py
@@ -38,6 +38,7 @@ from pyramid.url import current_route_url
 
 from autonomie.views.forms.lists import ITEMS_PER_PAGE_OPTIONS
 from autonomie.export.csvtools import SqlaToCsvWriter
+from autonomie.export.excel import SqlaXlsWriter
 from autonomie.export.utils import write_file_to_request
 
 
@@ -65,50 +66,53 @@ class BaseView(object):
         self.session = self.request.session
 
 
-class BaseListView(BaseView):
+class BaseListClass(BaseView):
     """
-        A base list view used to provide an easy way to list elements
+    Base class for list related views (list view and exports)
 
         * It launches a query to retrieve records
         * Validates GET params regarding the given schema
         * filter the query with the provided filter_* methods
-        * Launches complementary methods to populate request vars like popup
-          or actionmenu
 
-        @param add_template_vars: list of attributes (or properties)
-                                  that will be automatically added
-                                  to the response dict
+    @param schema: Schema used to validate the GET params provided in the
+                    url, the schema should inherit from
+                    autonomie.views.forms.lists.BaseListsSchema to preserve
+                    most of the processed automation
+    @param sort_columns: dict of {'sort_column_key':'sort_column'...}.
+        Allows to generate the validator for the sort availabilities and
+        to automatically add a order_by clause to the query. sort_column
+        may be equal to Table.attribute if join clauses are present in the
+        main query.
+    @default_sort: the default sort_column_key to be used
+    @default_direction: the default sort direction (one of ['asc', 'desc'])
 
-        @param schema: Schema used to validate the GET params provided in the
-                        url, the schema should inherit from
-                        autonomie.views.forms.lists.BaseListsSchema to preserve
-                        most of the processed automation
-        @param sort_columns: dict of {'sort_column_key':'sort_column'...}.
-            Allows to generate the validator for the sort availabilities and
-            to automatically add a order_by clause to the query. sort_column
-            may be equal to Table.attribute if join clauses are present in the
-            main query.
-        @default_sort: the default sort_column_key to be used
-        @default_direction: the default sort direction (one of ['asc', 'desc'])
-
-        A subclass shoud provide at least a schema and a query method
+    A subclass shoud provide at least a schema and a query method
     """
-    add_template_vars = ('title',)
     schema = None
     default_sort = 'name'
     sort_columns = {'name':'name'}
     default_direction = 'asc'
 
+    def _get_bind_params(self):
+        """
+        return the params passed to the form schema's bind method
+        if subclass override this method, it should call the super
+        one's too
+        """
+        return dict(request=self.request,
+                    default_sort=self.default_sort,
+                    default_direction=self.default_direction,
+                    sort_columns=self.sort_columns)
+
     def query(self):
         """
-            The main query, should be overrided by a subclass
+        The main query, should be overrided by a subclass
         """
         pass
 
     def _get_filters(self):
         """
-            Return the list of the filter_... methods attached to the current
-            object
+        collect the filter_... methods attached to the current object
         """
         for method_name, method in inspect.getmembers(self, inspect.ismethod):
             if method_name.startswith('filter_'):
@@ -116,7 +120,7 @@ class BaseListView(BaseView):
 
     def _filter(self, query, appstruct):
         """
-            filter the query with the configured filters
+        filter the query with the configured filters
         """
         for method in self._get_filters():
             query = method(query, appstruct)
@@ -124,9 +128,9 @@ class BaseListView(BaseView):
 
     def _sort(self, query, appstruct):
         """
-            Sort the results regarding the default values and
-            the sort_columns dict, maybe overriden to provide a custom sort
-            method
+        Sort the results regarding the default values and
+        the sort_columns dict, maybe overriden to provide a custom sort
+        method
         """
         sort_column_key = appstruct['sort']
         sort_column = self.sort_columns[sort_column_key]
@@ -137,6 +141,58 @@ class BaseListView(BaseView):
         else:
             func = desc
         return query.order_by(func(sort_column))
+
+    def _collect_appstruct(self):
+        """
+        collect the filter options from the current url
+
+        Use the schema to validate the GET params of the current url and returns
+        the formated datas
+        """
+        schema = None
+        appstruct = {}
+        if self.schema is not None:
+            schema = self.schema.bind(**self._get_bind_params())
+            try:
+                appstruct = schema.deserialize(self.request.GET)
+            except colander.Invalid as e:
+                # If values are not valid, we want the default ones to be provided
+                # see the schema definition
+                log.error("CURRENT SEARCH VALUES ARE NOT VALID")
+                log.error(e)
+                appstruct = schema.deserialize({})
+        return schema, appstruct
+
+    def __call__(self):
+        schema, appstruct = self._collect_appstruct()
+
+        query = self.query()
+        query = self._filter(query, appstruct)
+        query = self._sort(query, appstruct)
+
+        return self._build_return_value(schema, appstruct, query)
+
+    def _build_return_value(self, schema, appstruct, query):
+        """
+        To be implemented : datas returned by the view
+        """
+        return {}
+
+
+class BaseListView(BaseListClass):
+    """
+    A base list view used to provide an easy way to build list views
+    Uses the BaseListClass and add the templating datas :
+
+        * Provide a pagination object
+        * Provide a search form based on the given schema
+        * Launches complementary methods to populate request vars like popup
+        or actionmenu
+
+    @param add_template_vars: list of attributes (or properties)
+                                that will be automatically added
+    """
+    add_template_vars = ('title',)
 
     def _paginate(self, query, appstruct):
         """
@@ -150,41 +206,6 @@ class BaseListView(BaseView):
                              current_page,
                              url=page_url,
                              items_per_page=items_per_page)
-
-    def _get_bind_params(self):
-        """
-            return the params passed to the form schema's bind method
-            if subclass override this method, it should call the super
-            one's too
-        """
-        return dict(request=self.request,
-                    default_sort=self.default_sort,
-                    default_direction=self.default_direction,
-                    sort_columns=self.sort_columns)
-
-    def __call__(self):
-        """
-            This method is a used in pyramid to make a class a view
-        """
-        query = self.query()
-        schema = self.schema.bind(**self._get_bind_params())
-        try:
-            appstruct = schema.deserialize(self.request.GET)
-        except colander.Invalid as e:
-            # If values are not valid, we want the default ones to be provided
-            # see the schema definition
-            log.error("CURRENT SEARCH VALUES ARE NOT VALID")
-            log.error(e)
-            appstruct = schema.deserialize({})
-
-        query = self._filter(query, appstruct)
-        query = self._sort(query, appstruct)
-        records = self._paginate(query, appstruct)
-        result = dict(records=records)
-        result['form'] = self.get_form(schema, appstruct)
-        result.update(self.more_template_vars())
-        self.populate_actionmenu(appstruct)
-        return result
 
     def get_form(self, schema, appstruct):
         # counter is used to avoid field name conflicts
@@ -211,32 +232,68 @@ class BaseListView(BaseView):
         """
         pass
 
+    def _build_return_value(self, schema, appstruct, query):
+        """
+        Return the datas expected by the template
+        """
+        records = self._paginate(query, appstruct)
+        result = dict(records=records)
+        result['form'] = self.get_form(schema, appstruct)
+        result.update(self.more_template_vars())
+        self.populate_actionmenu(appstruct)
+        return result
 
-class BaseCsvView(BaseView):
+
+class BaseCsvView(BaseListClass):
     """
         Base Csv view
 
-        Launches the appropriate query and store the whole stuff in a csv file
+        A list view that returns a streamed file
+
+        A subclass should implement :
+
+            * a query method
+            * filename property
+
+        To be able to handle the rows that are streamed:
+
+            * a _stream_rows method
+
+        If this view should support the GET params filtering method (export
+        associated to a list view), a subclass should provide:
+
+            * a schema attr
+            * filter_ methods
     """
     model = None
-    csvwriter = SqlaToCsvWriter
+    writer = SqlaToCsvWriter
 
     @property
     def filename(self):
         """
-            To be implemented by the subclasse
+        To be implemented by the subclass
         """
         pass
 
-    def query(self):
+    def _stream_rows(self, query):
         """
-            To be implemented by the subclasse
+        Return a generator with the rows we expect in our output,
+        default is to return the sql ones
         """
-        pass
+        for item in query.all():
+            yield item
 
-    def __call__(self):
-        writer = self.csvwriter(self.model)
-        for item in self.query():
-            writer.add_row(item.appstruct())
+    def _build_return_value(self, schema, appstruct, query):
+        """
+        Return the streamed file object
+        """
+        writer = self.writer(self.model)
+        for item in self._stream_rows(query):
+            writer.add_row(item)
+
         write_file_to_request(self.request, self.filename, writer.render())
         return self.request.response
+
+
+class BaseXlsView(BaseCsvView):
+    writer = SqlaXlsWriter

9e6a00e95052c1c85f126a02cf43611b41bff179
G.Tjebbes <g.t@majerti.fr>
2014-09-01 17:55:38 +0200

    Remove print statement

--- autonomie/deform_extend.py
+++ autonomie/deform_extend.py
@@ -608,7 +608,6 @@ class CustomDateTimeInputWidget(CustomDateInputWidget):
                        ('separator', ' '))
 
     def serialize(self, field, cstruct, readonly=False):
-        print("Cstruct : %s" % cstruct)
         if cstruct in (colander.null, None):
             cstruct = ''
         if cstruct:

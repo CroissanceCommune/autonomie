4e1014e68c6721c3350cf0556fada179661f655a
G.Tjebbes <g.t@majerti.fr>
2014-09-02 19:37:40 +0200

    Enhance forms.files to allow download of templates

--- autonomie/forms/files.py
+++ autonomie/forms/files.py
@@ -36,18 +36,26 @@ from autonomie.views.render_api import human_readable_filesize
 from autonomie import forms
 
 
+TEMPLATES_PATH = "autonomie:deform_templates/"
+
+
 class SessionDBFileUploadTempStore(SessionFileUploadTempStore):
     def preview_url(self, uid):
-        if self.request.context.type_ == 'file':
-            return self.request.route_path('file', id=self.request.context.id)
+        doctype = getattr(self.request.context, 'type_', '')
+        if doctype:
+            return self.request.route_path(doctype, id=self.request.context.id)
         else:
             return None
+
 
 @colander.deferred
 def deferred_upload_widget(node, kw):
     request = kw['request']
     tmpstore = SessionDBFileUploadTempStore(request)
-    return deform.widget.FileUploadWidget(tmpstore)
+    return deform.widget.FileUploadWidget(
+        tmpstore,
+        template=TEMPLATES_PATH + "fileupload.mako"
+    )
 
 
 @colander.deferred
@@ -93,25 +101,36 @@ def filesize_validator(node, value):
 
 class FileUploadSchema(colander.Schema):
     parent_id = colander.SchemaNode(
-            colander.Integer(),
-            widget=deform.widget.HiddenWidget(),
-            )
+        colander.Integer(),
+        widget=deform.widget.HiddenWidget(),
+        missing=colander.drop,
+    )
 
     come_from = forms.come_from_node()
 
     filetype = colander.SchemaNode(
-            colander.String(),
-            widget=deferred_filetype_select,
-            missing="",
-            title=u"Type de fichier",
-            )
+        colander.String(),
+        widget=deferred_filetype_select,
+        missing="",
+        title=u"Type de fichier",
+    )
 
     description = colander.SchemaNode(colander.String())
 
     upload = colander.SchemaNode(
-            deform.FileData(),
-            widget=deferred_upload_widget,
-            title=u"Choix du fichier",
-            description=file_description(),
-            validator=filesize_validator,
-            )
+        deform.FileData(),
+        widget=deferred_upload_widget,
+        title=u"Choix du fichier",
+        description=file_description(),
+        validator=filesize_validator,
+    )
+
+
+def get_template_upload_schema():
+    """
+    Return the form schema for template upload
+    """
+    schema = FileUploadSchema().clone()
+    schema['upload'].description += u" Le fichier doit être au format ODT"
+    del schema['filetype']
+    return schema

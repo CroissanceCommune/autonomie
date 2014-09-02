5ecf6d54e537077c2f8d27b49298f25a59cbdc3e
G.Tjebbes <g.t@majerti.fr>
2014-09-02 23:54:58 +0200

    Add a docgeneration view

--- autonomie/views/user.py
+++ autonomie/views/user.py
@@ -37,6 +37,7 @@ from pyramid.httpexceptions import HTTPFound
 from pyramid.security import has_permission
 from pyramid.decorator import reify
 
+from autonomie.models import files
 from autonomie.models.user import (
     User,
     UserDatas,
@@ -51,6 +52,8 @@ from autonomie.utils.widgets import (
     PopUp,
     StaticWidget,
 )
+from autonomie.export.utils import write_file_to_request
+from autonomie.export import py3o
 from autonomie.deform_extend import (
     AccordionFormWidget,
     TableFormWidget,
@@ -406,7 +409,7 @@ mot de passe : {1}".format(login, password, url)
 
 
 class UserDatasEdit(UserDatasAdd):
-    add_template_vars = ('doctypes_form', 'account_form',)
+    add_template_vars = ('doctypes_form', 'account_form', 'doctemplates', )
 
     @property
     def title(self):
@@ -487,6 +490,48 @@ class UserDatasEdit(UserDatasAdd):
         form.widget = TableFormWidget()
         form.set_appstruct(appstruct)
         return form
+
+    @property
+    def doctemplates(self):
+        templates = files.Template.query()
+        templates = templates.filter(files.Template.active==True)
+        return templates.all()
+
+
+def py3o_view(context, request):
+    """
+    py3o view :
+
+        compile the template provided as argument using the current view
+        context for templating
+    """
+    doctemplate_id = request.GET.get('template_id')
+
+    if doctemplate_id:
+        template = files.Template.get(doctemplate_id)
+        if template:
+            try:
+                output = py3o.compile_template(context, template.data_obj)
+                write_file_to_request(
+                    request,
+                    template.name,
+                    output,
+                )
+                return request.response
+            except:
+                request.session.flash(
+                    u"Erreur à la compilation du modèle, merci de contacter \
+votre administrateur"
+                )
+        request.session.flash(
+            u"Impossible de mettre la main sur ce modèle",
+            "error"
+        )
+    request.session.flash(
+        u"Les données fournies en paramètres sont invalides",
+        "error"
+    )
+    return HTTPFound(request.route_path(context.type_, id=context.id))
 
 
 def userdata_doctype_view(userdata_model, request):
@@ -1006,3 +1051,9 @@ def includeme(config):
         permission="manage",
     )
 
+    config.add_view(
+        py3o_view,
+        route_name="userdata",
+        request_param="action=py3o",
+        permission="manage",
+    )

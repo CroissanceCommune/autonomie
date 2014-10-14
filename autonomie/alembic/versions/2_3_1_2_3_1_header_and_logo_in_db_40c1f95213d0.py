476bc081c0aa44890cc6b2771990d39ac753c21a
G.Tjebbes <g.t@majerti.fr>
2014-10-14 19:37:14 +0200

    Add migration of company files to the database

--- autonomie/alembic/versions/2_3_1_2_3_1_header_and_logo_in_db_40c1f95213d0.py
+++ autonomie/alembic/versions/2_3_1_2_3_1_header_and_logo_in_db_40c1f95213d0.py
@@ -10,16 +10,70 @@ Create Date: 2014-10-13 10:45:40.840370
 revision = '40c1f95213d0'
 down_revision = '42c3d2634645'
 
+import os
+import mimetypes
 from alembic import op
 import sqlalchemy as sa
+import logging
+logger = logging.getLogger("alembic.migrate_company_header")
+
+
+BASEFILEPATH = "/var/intranet_files/"
+
+
+def load_file_struct(filepath, filename):
+    res = dict()
+    if os.path.isfile(filepath):
+        data = open(filepath, "rb").read()
+        res['data'] = data
+        res['mimetype'] = mimetypes.guess_type(filepath)[0] or 'text/plain'
+        res['size'] = len(data)
+        res['name'] = filename
+    else:
+        logger.warn("Unknown file %s" % filepath)
+    return res
 
 
 def upgrade():
+    from autonomie.models.company import Company
+    from autonomie.models.files import File
+    from autonomie.models import DBSESSION
+    from alembic.context import get_bind
     for i in ('header_id', 'logo_id',):
         col = sa.Column(i, sa.Integer, sa.ForeignKey('file.id'))
         op.add_column('company', col)
 
+    query = "select id, header, logo from company;"
+    conn = get_bind()
+    result = conn.execute(query)
+
+    session = DBSESSION()
+
+    for id_, header, logo in result:
+        company = Company.get(id_)
+        basepath = "%scompany/%s" % (BASEFILEPATH, id_,)
+
+        if header:
+            header_path = "%s/header/%s" % (basepath, header)
+            file_datas = load_file_struct(header_path, header)
+            if file_datas:
+                company.header = file_datas
+                session.add(company.header_file)
+                session.flush()
+
+        if logo:
+            logo_path = "%s/logo/%s" % (basepath, logo)
+            file_datas = load_file_struct(logo_path, logo)
+            if file_datas:
+                company.logo = file_datas
+                company = session.merge(company)
+                session.flush()
+
 
 def downgrade():
-    op.drop_column('company', sa.Column('logo_id'))
-    op.drop_column('company', sa.Column('header_id'))
+    for query in ("alter table company DROP FOREIGN KEY company_ibfk_1",
+                  "alter table company DROP FOREIGN KEY company_ibfk_2",):
+        op.execute(query)
+    op.drop_column('company', 'logo_id')
+    op.drop_column('company', 'header_id')
+

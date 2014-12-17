dbdc31317a2a76c55913340733e5e2bbf4f55b00
G.Tjebbes <g.t@majerti.fr>
2014-12-17 18:57:13 +0100

    Add a custom exception

--- autonomie/exception.py
+++ autonomie/exception.py
@@ -60,3 +60,10 @@ class InstanceNotFound(Exception):
     Raised when no instance could be found
     """
     pass
+
+
+class MultipleInstanceFound(Exception):
+    """
+    Raised when no instance could be found
+    """
+    pass

6637e280a41fc71c437069ae14fb68558af75966
G.Tjebbes <g.t@majerti.fr>
2014-12-19 10:36:29 +0100

    Add a job model

--- autonomie/models/__init__.py
+++ autonomie/models/__init__.py
@@ -40,6 +40,7 @@ import tva
 import user
 import task
 import workshop
+import job
 __author__ = "Arezki Feth, Miotte Julien, Pettier Gabriel and Tjebbes Gaston"
 __copyright__ = "Copyright 2012-2013, Croissance Commune"
 __license__ = "GPL"

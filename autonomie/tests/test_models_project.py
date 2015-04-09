525ccd5f4dc351f51cdf41fb2cdb54fec94c6ebf
G.Tjebbes <g.t@majerti.fr>
2015-04-09 18:49:47 +0200

    Remove deprecated tests

--- autonomie/tests/test_models_project.py
+++ autonomie/tests/test_models_project.py
@@ -25,40 +25,7 @@ from pytest import fixture
 from mock import MagicMock
 from autonomie.models.project import Project
 
-PROJECT = dict(name=u'project1',
-               code=u"PRO1",
-               company_id=1,
-                )
-
-EST_LIST = ["Devis 1", "Devis deoko"]
-INV_LIST = ['Facture 1', "Facture d'acompte 2", "Facture 5"]
-CINV_LIST = ["Avoir 1", "Avoir 4"]
-
-@fixture()
-def project():
-    p = Project(**PROJECT)
-    for i in EST_LIST:
-        p.tasks.append(MagicMock(number=i, type_='estimation'))
-    for i in INV_LIST:
-        p.tasks.append(MagicMock(number=i, type_='invoice'))
-    for i in CINV_LIST:
-        p.tasks.append(MagicMock(number=i, type_='cancelinvoice'))
-    return p
-
-
 def test_get_number():
     test_str = "Devis 5"
     assert Project.get_number(test_str, "Devis ") == 5
     assert Project.get_number(test_str, "Devistoto") == 0
-
-def test_get_next_estimation_number(project):
-    assert project.get_next_estimation_number() == 3
-
-def test_get_next_invoice_number(project):
-    assert project.get_next_invoice_number() == 6
-    project.tasks = []
-    assert project.get_next_invoice_number() == 1
-
-def test_get_next_cancelinvoice_number(project):
-    print(project.tasks)
-    assert project.get_next_cancelinvoice_number() == 5

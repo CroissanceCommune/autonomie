c6f819a07118a03e5c190d6b307356666620fe8e
G.Tjebbes <g.t@majerti.fr>
2015-04-30 13:27:07 +0200

    Statistiques tools

---
+++ autonomie/statistics/__init__.py
@@ -0,0 +1,328 @@
+# -*-coding:utf-8-*-
+"""
+Main classes for statistics handling
+Maybe it will be merged with the models further on
+
+
+1- On récupère la feuille de stats depuis la base
+2- On récupère le dictionnaire d'inspection pour le modèle cible
+3- Pour chaque critère, on va peupler le critère avec des les données renvoyées
+par l'inspection
+
+
+On doit pouvoir générer des query :
+    1-
+    db().query(distinct(models.user.UserDatas.id)).filter(models.user.UserDatas.coordonnees_lastname.startswith('tje')).count()
+
+    2- Des relations M2o
+    Les objets remotes existent déjà, c'est forcément par l'id qu'on match
+    db().query(distinct(models.user.UserDatas.id)).filter(models.user.UserDatas.parcours_status_id.in_((1,2,3))).count()
+
+    3- Des relations o2M
+    On doit passer par les attributs des objets remotes pour filtrer
+    filtre sur un attribut cible :
+    db().query(distinct(models.user.UserDatas.id)).outerjoin(models.user.UserDatas.parcours_date_diagnostic).filter(models.user.DateDiagnosticDatas.date<datetime.datetime.now()).count()
+    filtre sur deux attributs cibles :
+    db().query(distinct(models.user.UserDatas.id), models.user.UserDatas).outerjoin(models.user.UserDatas.statut_external_activity).filter(models.user.ExternalActivityDatas.brut_salary>100).filter(models.user.ExternalActivityDatas.hours<8).count()
+"""
+from sqlalchemy import (
+    not_,
+    distinct,
+    and_,
+    or_
+)
+
+from autonomie.models.base import DBSESSION
+
+
+class StatSheet(object):
+    """
+    Statistics sheet
+
+    Compound of :
+        a list of entries
+
+    Rendering:
+
+            A sheet should be rendered as a csv file, the headers :
+                ('label', 'count')
+            each row matches an entry
+
+    :param obj model: The model we're building stats on (UserDatas)
+    :param obj sheet: The StatSheet instance we're mapping
+    :param obj inspector: The model's sqlalchemy inspector used to retrieve
+    database related informations
+    """
+    entries = []
+
+    def __init__(self, model, sheet, inspector):
+        self.model = model
+        self.sheet = sheet
+        self.inspector = inspector
+
+        self.entries = []
+        for entry in self.sheet.entries:
+            self.entries.append(StatEntry(self.model, entry, self.inspector))
+
+    def headers(self):
+        return u"Libellé", u"Nombre"
+
+    def rows(self):
+        """
+        Return the rows of our sheet output
+        """
+        result = []
+        for entry in self.entries:
+            result.append(entry.render_row())
+
+        return result
+
+
+class StatEntry(object):
+    """
+    Statistic entry
+
+    Compound of :
+        a label
+        a description
+        a list of criteria
+    """
+    model = None
+    related_joins = None
+    criteria = []
+
+    def __init__(self, model, entry_model, inspector):
+        self.model = model
+        self.entry = entry_model
+        self.stat_model = inspector
+
+        self.criteria = []
+        for criterion in self.entry.criteria:
+            self.criteria.append(
+                get_stat_criterion(self.model, criterion, inspector)
+            )
+
+    def query(self):
+        """
+        Returns the main query used to find objects
+
+        E.G:
+
+            query = DBSESSION().query(distinct(UserDatas.id), UserDatas)
+            query = query.filter(UserDatas.name.startswith('test'))
+            query = query.outerjoin(UserDatas.conseiller)
+            query = query.filter(User.lastname=='A manager')
+        """
+        query = DBSESSION().query(distinct(self.model.id), self.model)
+
+        # Pour chaque critère sur lesquels on va ajouter des filtres, on a
+        # besoin d'être sûr que la classe concernée est bien requêtée, il faut
+        # donc ajouter des outerjoins pour chaque classe liée.
+
+        # NOTE: on ne gère pas les alias (les joins sur deux tables identiques
+        # pour deux relations différentes)
+        already_added = []
+        for criterion in self.criteria:
+            # On génère le filtre
+            filter_ = criterion.gen_filter()
+
+            # si il n'y a pas de filtres ...
+            if filter_ is not None:
+                # On récupère l'objet lié
+                join_class = criterion.get_join_class()
+                # Si il y a un outerjoin à faire (le critère se fait sur un
+                # attribut de l'objet lié)
+                if join_class is not None:
+                    # on ne va pas outerjoiné deux fois la classe pour le même
+                    # critère
+                    if criterion.key not in already_added:
+                        query = query.outerjoin(join_class)
+                        already_added.append(criterion.key)
+
+                query = query.filter(filter_)
+        return query
+
+    def render_row(self):
+        """
+        Returns the datas expected for statistics rendering
+        """
+        return self.entry.title, self.query().count()
+
+
+def get_stat_criterion(model, criterion_model, inspector):
+    """
+    Return the appropriate criterion class
+    """
+    if criterion_model.type == 'string':
+        factory = StrStatCriterion
+    elif criterion_model.type == 'number':
+        factory = NumericStatCriterion
+    elif criterion_model.type == 'opt_rel':
+        factory = OptRelStatCriterion
+    elif criterion_model.type == 'date':
+        factory = DateStatCriterion
+    return factory(model, criterion_model, inspector)
+
+
+class StatCriterion(object):
+    """
+    Statistic criterion
+
+    Compound of :
+        a model we query on
+        a field name
+        a condition key
+        conditions values
+        a type
+    """
+    model = None
+    key = None
+    search1 = None
+    search2 = None
+    searches = []
+    type = 'str'
+
+    def __init__(self, model, criterion_model, inspector):
+        self.model = model
+        self.criterion_model = criterion_model
+        self.search1 = criterion_model.search1
+        self.search2 = criterion_model.search2
+        self.searches = criterion_model.searches
+
+        self.key = criterion_model.key
+        self.method = criterion_model.method
+
+        self.sqla_datas = inspector.get_datas(self.key)
+        self.column = self.sqla_datas['column']
+
+    def get_join_class(self):
+        """
+        Return a related class if this criterion used one
+        """
+        return self.sqla_datas.get('join_class')
+
+    def gen_filter(self):
+        result = None
+        if self.key:
+            filter_method = getattr(self, "filter_%s" % self.method, None)
+            if filter_method is not None:
+                result = filter_method(self.column)
+        return result
+
+    # FIXME : maybe one of the none values ('', 0 ...)
+    def filter_nll(self, attr):
+        """ null """
+        return attr == None
+
+    def filter_nnll(self, attr):
+        """ not null """
+        return attr != None
+
+    def filter_eq(self, attr):
+        """ equal """
+        if self.search1:
+            return attr == self.search1
+
+    def filter_neq(self, attr):
+        """ not equal """
+        if self.search1:
+            return attr != self.search1
+
+
+class StrStatCriterion(StatCriterion):
+    """
+    Statistic criterion related to strings
+    """
+    def filter_has(self, attr):
+        """ contains """
+        if self.search1:
+            like_str = u"%" + self.search1 + u"%"
+            return attr.like(like_str)
+
+    def filter_sw(self, attr):
+        """ startswith """
+        if self.search1:
+            like_str = self.search1 + u"%"
+            return attr.like(like_str)
+
+    def filter_ew(self, attr):
+        """ endswith """
+        if self.search1:
+            like_str = self.search1 + u"%"
+            return attr.like(like_str)
+
+    def filter_nhas(self, attr):
+        """ not contains """
+        f = self.filter_has(attr)
+        if f is not None:
+            return not_(f)
+
+
+class OptRelStatCriterion(StatCriterion):
+    """
+    Statistic criterion related to related options
+    """
+    searches = []
+
+    def filter_ioo(self, attr):
+        """ is one of """
+        if self.searches:
+            return attr.in_(self.searches)
+
+    def filter_nioo(self, attr):
+        """ is not one of """
+        if self.searches:
+            return not_(attr.in_(self.searches))
+
+
+class NumericStatCriterion(StatCriterion):
+    """
+    Statistic criterion for filtering numeric datas
+    """
+    def filter_lte(self, attr):
+        if self.search1:
+            return attr <= self.search1
+        return
+
+    def filter_gte(self, attr):
+        if self.search1:
+            return attr >= self.search1
+        return
+
+    def filter_lt(self, attr):
+        if self.search1:
+            return attr < self.search1
+        return
+
+    def filter_gt(self, attr):
+        if self.search1:
+            return attr > self.search1
+        return
+
+    def bw(self, attr):
+        if self.search1 and self.search2:
+            return and_(attr > self.search1, attr < self.search2)
+
+    def nbw(self, attr):
+        if self.search1 and self.search2:
+            return or_(attr <= self.search1, attr >= self.search2)
+
+
+class DateStatCriterion(StatCriterion):
+    """
+    Statistic criterion related to Dates
+    """
+    def filter_dr(self, attr):
+        # TODO
+        pass
+
+#   80 DATE_OPTIONS = (
+#   81     ('', '- Tous -', ),
+#   82     ('dr', u"Dans l'intervalle", ),
+#   83     ('this_year', u"Depuis le début de l'année", ),
+#   84     ('this_month', u"Ce mois-ci", ),
+#   85     ("previous_year", u"L'année dernière", ),
+#   86     ("previous_month", u"Le mois dernier", ),
+#   87     ("nll", u"N'est pas renseigné(e)", ),
+#   88     ("nnll", u"Est renseigné(e)", ),
+#   89 )
---
+++ autonomie/statistics/inspect.py
@@ -0,0 +1,188 @@
+# -*- coding: utf-8 -*-
+# * Copyright (C) 2012-2014 Croissance Commune
+# * Authors:
+#       * Arezki Feth <f.a@majerti.fr>;
+#       * Miotte Julien <j.m@majerti.fr>;
+#       * TJEBBES Gaston <g.t@majerti.fr>
+#
+# This file is part of Autonomie : Progiciel de gestion de CAE.
+#
+#    Autonomie is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Autonomie is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Autonomie.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+from collections import OrderedDict
+from sqlalchemy.orm import (
+    RelationshipProperty,
+)
+from sqla_inspect.base import BaseSqlaInspector
+
+
+logger = logging.getLogger(__name__)
+
+
+class Column(dict):
+    """
+    The column object wrapping the model's attribute
+    """
+    def __json__(self, request):
+        return dict(
+            label=self.get('label'),
+            name=self.get('name'),
+            type=self.get('type', 'string'),
+        )
+
+
+class StatisticInspector(BaseSqlaInspector):
+    """
+    A sqla inspector made for statistics
+
+    model
+
+        The model we want to inspect
+
+    excludes
+
+        The name of the attributes we want to exclude from inspection
+
+    exclude_relationships
+
+        Should we exclude relationships (usefull for limiting recursive
+        inspection)
+    """
+    config_key = 'export'
+
+    def __init__(self, model, excludes=(), exclude_relationships=False):
+        BaseSqlaInspector.__init__(self, model)
+        self.model = model
+        self.excludes = excludes
+        self.exclude_relationships = exclude_relationships
+        self.columns = self._collect_columns()
+
+    def _collect_columns(self):
+        """
+        Collect the columns names, titles, ...
+
+        Single attribute :
+            name: for column identification
+            label : for UI
+            prop : The property (sqla object from which handle all datas
+            column : The associated sqlalchemy.Column object
+
+        OneToMany relationship :
+            related_class: The related class
+            related_key: The related key we may act on
+
+        ManyToOne relationship :
+            for each field of the destination class:
+                related_class: The related_class
+                name: for column identification
+                label : for UI
+                prop : The property (sqla object from which handle all datas
+                column : The associated sqlalchemy.Column object
+
+
+        """
+        result = OrderedDict()
+        todrop = []
+        for prop in self.get_sorted_columns():
+            if prop.key in self.excludes:
+                continue
+            info_dict = self.get_info_field(prop)
+            colanderalchemy_infos = info_dict.get('colanderalchemy', {})
+
+            import_dict = info_dict.get(self.config_key, {})
+
+            ui_label = colanderalchemy_infos.get('title', prop.key)
+            datas = Column({
+                'name': prop.key,
+                'label': ui_label,
+                'prop': prop,
+                'column': prop.class_attribute,
+            })
+            datas.update(import_dict)
+
+            if isinstance(prop, RelationshipProperty):
+
+                if prop.uselist:
+                    # A one to many relationship
+
+                    # 1 get the result of the mapper's inspection
+                    # 2 append a prefix to the names and labels
+                    # 3 merge the resulting dict with the current one
+                    if self.exclude_relationships:
+                        # On zappe les relations o2M pour éviter les boucles
+                        continue
+                    res = StatisticInspector(
+                        prop.mapper,
+                        self.excludes,
+                        True
+                    ).columns
+
+                    keys = res.keys()
+                    for key in keys:
+                        # We modifiy the keys and the labels (we append the
+                        # related before to identify the relationship)
+                        # <rel_name>-<remote attribute>
+                        new_key = "%s-%s" % (prop.key, key)
+                        res[new_key] = res.pop(key)
+                        res[new_key]['label'] = "%s %s" % (
+                            datas['label'],
+                            res[new_key]['label'],
+                        )
+                        res[new_key]['rel_type'] = 'onetomany'
+                        # On a besoin de la classe pour les outerjoin et
+                        res[new_key]['join_class'] = prop.class_attribute
+                        result.update(res)
+
+                else:
+                    # On doit avoir directement les ids des options (objets
+                    # distants) disponibles
+                    # (sera fait lors de la génération du schéma)
+
+                    # On utilisera la colonne avec l'id
+                    todrop.append("%s_id" % prop.key)
+                    datas['rel_type'] = 'manytoone'
+                    # On a besoin de la classe liée pour la génération du form
+                    # (récupérer les options disponibles)
+                    datas['related_class'] = prop.class_attribute
+                    result[datas['name']] = datas
+            else:
+                result[datas['name']] = datas
+
+        for id_key in todrop:
+            if id_key in result:
+                ui_label = result[id_key].get('label')
+                rel_key = id_key[:-3]
+                if rel_key in result:
+                    result[rel_key]['label'] = ui_label
+                result[rel_key]['column'] = result[id_key]['column']
+                result.pop(id_key)
+        return result
+
+    def get_datas(self, key):
+        """
+        Return the inspected datas for the key
+
+        :param str key: A column name
+        """
+        return self.columns.get(key)
+
+    def get_json_columns(self):
+        """
+        Return the json representation of the dict of columns
+        """
+        result = OrderedDict()
+        for key, value in self.columns.items():
+            result[key] = value.__json__(None)
+        return result

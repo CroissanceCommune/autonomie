fd0e8e5b3c5ef13b1e7f45f707bfff58e58a0e7a
G.Tjebbes <g.t@majerti.fr>
2015-04-30 22:48:00 +0200

    Migrate Holiday js

--- autonomie/static/js/holiday.js
+++ autonomie/static/js/holiday.js
@@ -25,210 +25,6 @@
 
 var AppOptions = {};
 
-var HolidayModel = Backbone.Model.extend({
-  /*
-   *
-   * A model representing a holiday
-   */
-  initialize: function(options){
-    // Here we provide default values for the alt fields (alt fields are used
-    // by the datepicker widget to provide a display field and a stored one
-    // that will be sent to the server)
-    options = options || {};
-
-    if ( _.isUndefined( options['alt_start_date'] ) &&
-         ! _.isUndefined( options['start_date'] ) ){
-      this.set('alt_start_date', formatPaymentDate(options['start_date']));
-    }
-    if ( _.isUndefined( options['alt_end_date'] ) &&
-       ! _.isUndefined( options['end_date'] ) ){
-      this.set('alt_end_date', formatPaymentDate(options['end_date']));
-    }
-  },
-  validation:{
-    start_date: {
-      required: true,
-      pattern:/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/,
-      msg:"est requise"
-    },
-    end_date: {
-      required: true,
-      pattern:/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/,
-      msg:"est requise"
-
-    }
-  }
-});
-
-
-var HolidaysCollection = Backbone.Collection.extend({
-  /*
-   *  A collection of holidays sorted by start date
-   */
-  model: HolidayModel,
-  comparator: function(model){
-    return model.get('start_date');
-  }
-});
-
-
-var HolidayView = BaseTableLineView.extend({
-  /*
-   * A single line in the table
-   */
-  template: "holiday",
-  tagName: "tr",
-  events: {
-    'click a.remove':'_remove',
-    "click a.edit" : "_edit"
-  },
-  initialize: function(){
-    /*
-     * View constructor
-     */
-    // bind the model change to the view rendering
-    this.listenTo(this.model, 'change', this.render, this);
-  },
-  _remove: function(){
-    /*
-     *  Delete the line
-     */
-    var confirmed = confirm("Êtes vous certain de vouloir supprimer cet élément ?");
-    if (confirmed){
-      var _model = this.model;
-      this.highlight(
-      {callback: function(){_model.destroy(
-        {success: function(model, response) {
-          displayServerSuccess("L'élément a bien été supprimé");
-        }}
-      );}});
-    }
-  },
-  _edit: function(){
-    /*
-     * Redirect to the edit page
-     */
-    var route = "edit/" + this.model.cid;
-    AutonomieApp.router.navigate(route, {trigger: true});
-  }
-});
-
-var HolidayList = Backbone.Marionette.CompositeView.extend({
-  /*
-   * Holidays table
-   */
-  template: "holidayList",
-  itemViewContainer: "tbody",
-  itemView: HolidayView,
-  events: {
-    "click a.add": "_add"
-  },
-  appendHtml: function(collectionView, itemView, index){
-    // Launched when an item is added to the collectionview
-    // Here we provide a sorted output
-    // See :
-    // https://github.com/marionettejs/backbone.marionette/wiki/Adding-support-for-sorted-collections
-    // for more informations
-    var childrenContainer = collectionView.itemViewContainer ? collectionView.$(collectionView.itemViewContainer) : collectionView.$el;
-    var children = childrenContainer.children();
-    if (children.size() <= index) {
-      childrenContainer.append(itemView.el);
-    } else {
-      childrenContainer.children().eq(index).before(itemView.el);
-    }
-  },
-  _add: function(){
-    /*
-     *  Redirect our one page app to the holiday add page
-     */
-    AutonomieApp.router.navigate("add", {trigger: true});
-  }
-});
-
-
-var HolidayForm = BaseFormView.extend({
-  /*
-   *  Holiday add form view
-   */
-  template: "holidayForm",
-  ui:{
-    start_date: "#holidayForm input[name=alt_start_date]",
-    end_date: "#holidayForm input[name=alt_end_date]",
-    form: "#holidayForm"
-  },
-  onShow: function(){
-    /*
-     * Launched when the form is added to the dom
-     * Make some js calls
-     */
-    this.setDatePicker("holidayForm", this.ui.start_date, "start_date");
-    this.setDatePicker("holidayForm", this.ui.end_date, "end_date");
-    this.ui.start_date.focus();
-  }
-});
-
-
-AutonomieApp.Controller = {
-  /*
-   * Application controller
-   * Provides methods that are called regarding the router's configuration
-   */
-  holidays: null,
-  addform: null,
-  editform: null,
-  initialized: false,
-  index: function() {
-    this.ensurePopupClosed();
-    this.initialize();
-  },
-  ensurePopupClosed: function(){
-    /*
-     *  ensure the popup is closed (is necessary when we come from other views)
-     */
-    AutonomieApp.formContainer.close();
-  },
-  initialize: function(){
-    if (!this.initialized){
-      this.holidays = new HolidayList({collection: AutonomieApp.holidays});
-      AutonomieApp.holidayRegion.show(this.holidays);
-      this.initialized = true;
-    }
-  },
-  add: function(){
-    this.initialize();
-    var model = new HolidayModel();
-    holidayForm = new HolidayForm({
-      title: "Ajouter",
-      destCollection: AutonomieApp.holidays,
-      model:model
-      });
-    AutonomieApp.formContainer.show(holidayForm);
-  },
-  edit: function(id){
-    this.initialize();
-    var model = AutonomieApp.holidays.get(id);
-    holidayForm = new HolidayForm({
-      title:"Éditer",
-      model:model
-      });
-    AutonomieApp.formContainer.show(holidayForm);
-  }
-};
-
-
-AutonomieApp.Router = Backbone.Marionette.AppRouter.extend({
-  /*
-   * Application's routes configuration
-   */
-  appRoutes: {
-    "": "index",
-    "index": "index",
-    "add": "add",
-    "edit/:id": "edit"
-  }
-});
-
-
 var popup = Popup.extend({
   /*
    * Popup object that extends the default popup providing its element
@@ -244,32 +40,240 @@ AutonomieApp.addRegions({
   formContainer: popup
 });
 
+AutonomieApp.module("Holiday", function(Holiday, AutonomieApp,  Backbone, Marionette, $, _){
+  var HolidayModel = Backbone.Model.extend({
+    /*
+     *
+     * A model representing a holiday
+     */
+    initialize: function(options){
+      // Here we provide default values for the alt fields (alt fields are used
+      // by the datepicker widget to provide a display field and a stored one
+      // that will be sent to the server)
+      options = options || {};
 
-AutonomieApp.addInitializer(function(options){
-  /*
-   *  Application initialization
-   *  options : data provided by the server on setup ajax call
-   *
-   *  options should provide : a holidays objects list and a user_id param
-   */
-  if (AppOptions['loadurl'] !== undefined){
-    $.ajax({
-      url: AppOptions['loadurl'],
-      dataType: 'json',
-      async: false,
-      mimeType: "textPlain",
-      data: {},
-      cache: false,
-      success: function(options) {
-        AutonomieApp.holidays = new HolidaysCollection(options['holidays']);
-        AutonomieApp.holidays.url = "/user/" + options['user_id'] + "/holidays";
-        AutonomieApp.router = new AutonomieApp.Router({controller: AutonomieApp.Controller});
-      },
-      error: function(){
-        alert("Une erreur a été rencontrée, contactez votre administrateur.");
+      if ( _.isUndefined( options['alt_start_date'] ) &&
+           ! _.isUndefined( options['start_date'] ) ){
+        this.set('alt_start_date', formatPaymentDate(options['start_date']));
       }
-    });
-  }else{
-    alert("Une erreur a été rencontrée, contactez votre administrateur.");
-  }
+      if ( _.isUndefined( options['alt_end_date'] ) &&
+         ! _.isUndefined( options['end_date'] ) ){
+        this.set('alt_end_date', formatPaymentDate(options['end_date']));
+      }
+    },
+    validation:{
+      start_date: {
+        required: true,
+        pattern:/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/,
+        msg:"est requise"
+      },
+      end_date: [{
+        required: true,
+        pattern:/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/,
+        msg:"est requise"
+      }, {fn:'check_end_date'}
+      ]},
+      check_end_date: function(value, attr, computedState){
+        var start_date = computedState['start_date'];
+        if (!_.isUndefined(start_date)){
+          if (start_date > value){
+            return "La date de début doit précéder celle de fin";
+          }
+        }
+      }
+  });
+
+  var HolidaysCollection = Backbone.Collection.extend({
+    /*
+     *  A collection of holidays sorted by start date
+     */
+    model: HolidayModel,
+    comparator: function(model){
+      return model.get('start_date');
+    }
+  });
+  var HolidayView = BaseTableLineView.extend({
+    /*
+     * A single line in the table
+     */
+    template: "holiday",
+    tagName: "tr",
+    events: {
+      'click a.remove':'_remove',
+      "click a.edit" : "_edit"
+    },
+    initialize: function(){
+      /*
+       * View constructor
+       */
+      // bind the model change to the view rendering
+      this.listenTo(this.model, 'change', this.render, this);
+    },
+    _remove: function(){
+      /*
+       *  Delete the line
+       */
+      var confirmed = confirm("Êtes vous certain de vouloir supprimer cet élément ?");
+      if (confirmed){
+        var _model = this.model;
+        this.highlight(
+        {callback: function(){_model.destroy(
+          {success: function(model, response) {
+            displayServerSuccess("L'élément a bien été supprimé");
+          }}
+        );}});
+      }
+    },
+    _edit: function(){
+      /*
+       * Redirect to the edit page
+       */
+      var route = "edit/" + this.model.cid;
+      Holiday.router.navigate(route, {trigger: true});
+    }
+  });
+  var NoHolidatView = Backbone.Marionette.ItemView.extend({
+    template: "empty"
+  });
+  var HolidayList = Backbone.Marionette.CompositeView.extend({
+    /*
+     * Holidays table
+     */
+    template: "holidayList",
+    childViewContainer: "tbody",
+    childView: HolidayView,
+    emptyView: NoHolidatView,
+    events: {
+      "click a.add": "_add"
+    },
+    appendHtml: function(collectionView, childView, index){
+      // Launched when an item is added to the collectionview
+      // Here we provide a sorted output
+      // See :
+      // https://github.com/marionettejs/backbone.marionette/wiki/Adding-support-for-sorted-collections
+      // for more informations
+      var childrenContainer = collectionView.childViewContainer ? collectionView.$(collectionView.childViewContainer) : collectionView.$el;
+      var children = childrenContainer.children();
+      if (children.size() <= index) {
+        childrenContainer.append(childView.el);
+      } else {
+        childrenContainer.children().eq(index).before(childView.el);
+      }
+    },
+    _add: function(){
+      /*
+       *  Redirect our one page app to the holiday add page
+       */
+      Holiday.router.navigate("add", {trigger: true});
+    }
+  });
+
+
+  var HolidayForm = BaseFormView.extend({
+    /*
+     *  Holiday add form view
+     */
+    template: "holidayForm",
+    ui:{
+      start_date: "#holidayForm input[name=alt_start_date]",
+      end_date: "#holidayForm input[name=alt_end_date]",
+      form: "#holidayForm"
+    },
+    onShow: function(){
+      /*
+       * Launched when the form is added to the dom
+       * Make some js calls
+       */
+      this.setDatePicker("holidayForm", this.ui.start_date, "start_date");
+      this.setDatePicker("holidayForm", this.ui.end_date, "end_date");
+      this.ui.start_date.focus();
+    }
+  });
+  var controller = {
+    /*
+     * Application controller
+     * Provides methods that are called regarding the router's configuration
+     */
+    holidays: null,
+    addform: null,
+    editform: null,
+    initialized: false,
+    index: function() {
+      this.ensurePopupClosed();
+      this.initialize();
+    },
+    ensurePopupClosed: function(){
+      /*
+       *  ensure the popup is closed (is necessary when we come from other views)
+       */
+      AutonomieApp.formContainer.closeModal();
+    },
+    initialize: function(){
+      if (!this.initialized){
+        this.holidays = new HolidayList({collection: Holiday.holidays});
+        AutonomieApp.holidayRegion.show(this.holidays);
+        this.initialized = true;
+      }
+    },
+    add: function(){
+      this.initialize();
+      var model = new HolidayModel();
+      holidayForm = new HolidayForm({
+        title: "Ajouter",
+        destCollection: Holiday.holidays,
+        model:model
+        });
+      AutonomieApp.formContainer.show(holidayForm);
+    },
+    edit: function(id){
+      this.initialize();
+      var model = Holiday.holidays.get(id);
+      holidayForm = new HolidayForm({
+        title:"Éditer",
+        model:model
+        });
+      AutonomieApp.formContainer.show(holidayForm);
+    }
+  };
+
+  var router = Backbone.Marionette.AppRouter.extend({
+    /*
+     * Application's routes configuration
+     */
+    controller: controller,
+    appRoutes: {
+      "index": "index",
+      "add": "add",
+      "edit/:id": "edit"
+    }
+  });
+  Holiday.addInitializer(function(options){
+    /*
+     *  Application initialization
+     *  options : data provided by the server on setup ajax call
+     *
+     *  options should provide : a holidays objects list and a user_id param
+     */
+    Holiday.router = new router();
+    if (AppOptions['loadurl'] !== undefined){
+      $.ajax({
+        url: AppOptions['loadurl'],
+        dataType: 'json',
+        async: false,
+        mimeType: "textPlain",
+        data: {},
+        cache: false,
+        success: function(options) {
+          Holiday.holidays = new HolidaysCollection(options['holidays']);
+          Holiday.holidays.url = "/user/" + options['user_id'] + "/holidays";
+          Holiday.router.controller.index();
+        },
+        error: function(){
+          alert("Une erreur a été rencontrée, contactez votre administrateur.");
+        }
+      });
+    }else{
+      alert("Une erreur a été rencontrée, contactez votre administrateur.");
+    }
+  });
 });
--- autonomie/static/js/templates/holiday.js
+++ autonomie/static/js/templates/holiday.js
@@ -1,10 +1,13 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['empty.mustache'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  return "<td colspan='3'>Aucun congés n'a été saisi</td>\n";
+  },"useData":true});
 templates['holidayForm.mustache'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
   return "<form id='holidayForm' class='form' action='#' onsubmit='return false;'>\n<div class=\"form-group\">\n<label class=\"control-label\" for='alt_start_date'>Début</label>\n<div class='controls'>\n    <input name=\"alt_start_date\" class=\"input-small\" type=\"text\" autocomplete=\"off\">\n    <input name=\"start_date\" type=\"hidden\">\n</div>\n</div>\n<div class=\"form-group\">\n<label class=\"control-label\" for='alt_end_date'>Fin</label>\n<div class='controls'>\n    <input name=\"alt_end_date\" class=\"input-small\" type=\"text\" autocomplete=\"off\">\n    <input name=\"end_date\" type=\"hidden\">\n</div>\n</div>\n\n<div class=\"form-actions\">\n<button type=\"submit\" class=\"btn btn-primary\" name='submit'>Valider</button>\n<button type=\"reset\" class=\"btn btn-default\" name=\"cancel\">Annuler</button>\n</div>\n</form>\n";
   },"useData":true});
 templates['holidayList.mustache'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  return "<div>\n    <table class=\"opa table table-bordered table-condensed\">\n        <caption>\n        Vos congés\n            <div class=\"inline-element\">\n                <a class='btn btn-info add' title=\"Déclarer un congés\"><i class='icon icon-plus-sign'></i>&nbsp;Ajouter</a>\n            </div>\n        </caption>\n        <thead>\n            <th>Date de début</th>\n            <th>Date de fin</th>\n            <th>Actions</th>\n        </thead>\n        <tbody>\n        </tbody>\n    </table>\n</div>\n";
+  return "<div>\n    <table class=\"opa table table-bordered table-condensed\">\n        <caption>\n        Vos congés\n            <div class=\"inline-element\">\n                <a class='btn btn-info add' title=\"Déclarer un congés\"><i class='glyphicon glyphicon-plus-sign'></i>&nbsp;Ajouter</a>\n            </div>\n        </caption>\n        <thead>\n            <th>Date de début</th>\n            <th>Date de fin</th>\n            <th>Actions</th>\n        </thead>\n        <tbody>\n        </tbody>\n    </table>\n</div>\n";
   },"useData":true});
 templates['holiday.mustache'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
   var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
@@ -12,6 +15,6 @@ templates['holiday.mustache'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main
     + escapeExpression(((helper = (helper = helpers.alt_start_date || (depth0 != null ? depth0.alt_start_date : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"alt_start_date","hash":{},"data":data}) : helper)))
     + "</td>\n<td>"
     + escapeExpression(((helper = (helper = helpers.alt_end_date || (depth0 != null ? depth0.alt_end_date : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"alt_end_date","hash":{},"data":data}) : helper)))
-    + "</td>\n<td><a class='btn btn-default edit'><i class='icon icon-pencil'></i>&nbsp;Éditer</a><a class='btn btn-default remove'><i class='icon icon-remove-sign'></i>&nbsp;Supprimer</a></td>\n";
+    + "</td>\n<td><a class='btn btn-default edit'><i class='glyphicon glyphicon-pencil'></i>&nbsp;Éditer</a><a class='btn btn-default remove'><i class='glyphicon glyphicon-remove-sign'></i>&nbsp;Supprimer</a></td>\n";
 },"useData":true});
 })();

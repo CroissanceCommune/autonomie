c2304bb58a7c83e8fe5b2ba6006f0c6e04d0bc99
G.Tjebbes <g.t@majerti.fr>
2015-10-13 14:23:03 +0200

    Migrate competence related models

---
+++ autonomie/alembic/versions/3_1_1_fix_competences_bdb7dd32c2c.py
@@ -0,0 +1,34 @@
+"""3.1.1 : fix competences
+
+Revision ID: bdb7dd32c2c
+Revises: 59f05bb3051d
+Create Date: 2015-10-13 09:44:07.621413
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'bdb7dd32c2c'
+down_revision = '59f05bb3051d'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    from autonomie.models import competence
+    from autonomie.models.base import DBSESSION
+
+    for comp in competence.CompetenceOption.query():
+        req = comp.requirement
+        for deadline in competence.CompetenceDeadline.query():
+            comp.requirements.append(
+                competence.CompetenceRequirement(
+                    deadline_id=deadline.id,
+                    requirement=req,
+                )
+            )
+        DBSESSION().merge(comp)
+
+
+def downgrade():
+    pass

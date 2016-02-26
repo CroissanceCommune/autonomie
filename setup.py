0870985ec97ff686b2c47474b320bcb7a4cf3a67
G.Tjebbes <g.t@majerti.fr>
2016-02-26 10:05:06 +0100

    use find_packages

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from setuptools import setup
+from setuptools import find_packages
 
 entry_points = {
     "paste.app_factory": ["main=autonomie:main",],
@@ -15,11 +16,11 @@ entry_points = {
 setup(
     setup_requires=['d2to1'],
     d2to1=True,
-
+    packages=find_packages(),
+    zip_safe=False,
     # This ensures that the MANIFEST.in is read, but it will become implicit
     # in distutils2.
     include_package_data=True,
-
     # This is setuptools specific, will not work with distutils2.
     entry_points=entry_points,
     test_suite="autonomie.tests"

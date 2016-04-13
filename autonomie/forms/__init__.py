0804b15b8aee3f5747c8ba84b9abcd333f87e24e
G.Tjebbes <g.t@majerti.fr>
2016-04-13 09:16:39 +0200

    Add default preparer to textarea nodes

--- autonomie/forms/__init__.py
+++ autonomie/forms/__init__.py
@@ -117,6 +117,58 @@ def come_from_node(**kw):
     )
 
 
+def strip_whitespace(value):
+    """
+    Strip whitespace and tabs at the beginning/end of a string
+    """
+    if hasattr(value, 'strip'):
+        value = value.strip(' \t')
+    return value
+
+
+tags_to_check = (('<p>', '</p>'), ('<div>', '</div>'),)
+
+
+def remove_tag(text, tag):
+    return text[0:-1*len(tag)].strip()
+
+
+def strip_linebreaks(value):
+    """
+    Strip linebreaks
+    """
+    # we don't use rstrip since it's used for character stripping
+    # (not chain)
+    if hasattr(value, 'strip'):
+        value = value.strip('\n\r')
+        for tag in '<br />', '<br>', '<br/>':
+            if value.endswith(tag):
+                print("value : %s" % value)
+                value = remove_tag(value, tag)
+                return strip_linebreaks(value)
+
+    return value
+
+
+def strip_void_lines(value):
+    """
+    RStrip value ending with void html tags
+    """
+    if hasattr(value, 'strip'):
+        for tag, close_tag in tags_to_check:
+            if value.endswith(close_tag):
+                prec_value = remove_tag(value, close_tag)
+                prec_value = strip_whitespace(prec_value)
+                prec_value = strip_linebreaks(prec_value)
+                if prec_value.endswith(tag):
+                    value = remove_tag(prec_value, tag)
+                    value = strip_whitespace(value)
+                    value = strip_linebreaks(value)
+                    return strip_void_lines(value)
+
+    return value
+
+
 def textarea_node(**kw):
     """
     Return a node for storing Text objects
@@ -168,6 +220,11 @@ def textarea_node(**kw):
     else:
         widget_options.setdefault("rows", 4)
         wid = deform.widget.TextAreaWidget(**widget_options)
+
+    kw.setdefault(
+        'preparer',
+        (strip_whitespace, strip_linebreaks, strip_void_lines)
+    )
     return colander.SchemaNode(
         colander.String(),
         widget=wid,
---
+++ autonomie/tests/test_forms.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# * Copyright (C) 2012-2016 Croissance Commune
+# * Authors:
+#       * TJEBBES Gaston <g.t@majerti.fr>
+#       * Arezki Feth <f.a@majerti.fr>;
+#       * Miotte Julien <j.m@majerti.fr>;
+#
+# This file is part of Autonomie : Progiciel de gestion de CAE.
+#
+#    Autonomie is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Autonomie is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Autonomie.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Test for the forms package
+"""
+import colander
+from autonomie.forms import (
+    strip_whitespace,
+    strip_linebreaks,
+    strip_void_lines,
+)
+
+
+def test_strip_whitespace():
+    value = "toto"
+    assert strip_whitespace(value) == value
+    value = "     toto\t   "
+    assert strip_whitespace(value) == 'toto'
+    value = "   to    toto \t toto"
+    assert strip_whitespace(value) == "to    toto \t toto"
+    assert strip_whitespace(None) is None
+    assert strip_whitespace(colander.null) == colander.null
+
+
+def test_strip_linebreaks():
+    value = "\n toto \n <br /><br><br/>"
+    assert strip_linebreaks(value) == "toto"
+    assert strip_linebreaks(None) is None
+    assert strip_linebreaks(colander.null) == colander.null
+
+
+def test_strip_void_lines():
+    value = "<div></div><p>toto</p><p> </p>"
+    assert strip_void_lines(value) == "<div></div><p>toto</p>"

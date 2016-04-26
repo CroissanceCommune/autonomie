0ce0be8328a28d9b2cda8bce9f3fdd89f52d745b
G.Tjebbes <g.t@majerti.fr>
2016-04-26 18:36:56 +0200

    Fix payment '3'

---
+++ autonomie/alembic/versions/3_2_1_fix_payment_3_18504ec02955.py
@@ -0,0 +1,35 @@
+"""3.2.1 : fix payment 3
+
+Revision ID: 18504ec02955
+Revises: 658e0f23ee2
+Create Date: 2016-04-26 18:35:37.775710
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '18504ec02955'
+down_revision = '658e0f23ee2'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    from sqlalchemy import Date, cast
+    from datetime import date
+    from autonomie.models.task.invoice import Payment
+    from autonomie.models.base import DBSESSION as db
+
+    for payment in db().query(Payment).filter(cast(Payment.created_at, Date) == date.today()):
+        try:
+            payment.remittance_amount = int(payment.remittance_amount) / 1000.0
+            db().merge(payment)
+        except:
+            print(u"Erreur payment : %s (%s)" % payment.id, payment.amount)
+
+    from zope.sqlalchemy import mark_changed
+    mark_changed(db())
+
+
+def downgrade():
+    pass

e110bd8a5209dc8fe1fb13de9b6d803cc0bbf539
G.Tjebbes <g.t@majerti.fr>
2016-09-08 18:29:58 +0200

    Ref #398: Migration des données

--- autonomie/alembic/versions/3_3_0_fonctionnalites_9_2b6ac7b172d3.py
+++ autonomie/alembic/versions/3_3_0_fonctionnalites_9_2b6ac7b172d3.py
@@ -14,6 +14,74 @@ from alembic import op
 import sqlalchemy as sa
 
 
+def add_company_id(session, logger):
+    logger.warn("Adding company_id to Task")
+    from alembic.context import get_bind
+    conn = get_bind()
+
+    query = "select t.id, p.company_id from task t, project p where t.project_id = p.id"
+
+    for id, company_id in conn.execute(query):
+        req = "update task set company_id={0} where id={1}".format(company_id, id)
+        session.execute(req)
+
+
+def add_company_index(session, logger):
+    logger.warn("Adding company_index to Task")
+    from autonomie.models.company import Company
+    from autonomie.models.task import Task
+    for datas in session.query(Company.id):
+        query = Task.query()
+        query = query.options(
+            sa.orm.load_only('id', 'company_id', "company_index", 'type_')
+        )
+        query = query.filter(Task.company_id == datas[0])
+        for type_ in ('estimation', 'invoice', 'cancelinvoice'):
+            index = 1
+            typequery = query.filter(Task.type_ == type_)
+            for task in typequery:
+                task.company_index = index
+                index += 1
+                session.merge(task)
+
+
+def add_internal_number(session, logger):
+    logger.warn("Adding internal_number to Task")
+    NUMBER_TMPLS = {
+        'estimation': u"{s.project.code}_{s.customer.code}_D{s.project_index}\
+        _{s.date:%m%y}",
+        'invoice': u"{s.project.code}_{s.customer.code}_F{s.project_index}\
+        _{s.date:%m%y}",
+        'cancelinvoice': u"{s.project.code}_{s.customer.code}_A{s.project_index}\
+        _{s.date:%m%y}"
+    }
+    from autonomie.models.task import Task
+    from autonomie.models.customer import Customer
+    from autonomie.models.project import Project
+    from autonomie.models.project import Phase
+
+    from sqlalchemy.orm import joinedload
+    from sqlalchemy.orm import load_only
+
+    query = Task.query().options(
+        load_only("project_index", "company_index", "date", "phase_id", 'type_')
+    )
+
+    query = query.filter(
+        Task.type_.in_(['invoice', 'estimation', 'cancelinvoice'])
+    )
+    query = query.options(joinedload(Task.customer).load_only(Customer.code))
+    query = query.options(joinedload(Task.project).load_only(Project.code))
+
+    for task in query:
+        tmpl = NUMBER_TMPLS[task.type_]
+        if Phase.get(task.phase_id) is None:
+            session.delete(task)
+        else:
+            task.internal_number = tmpl.format(s=task).upper()
+            session.merge(task)
+
+
 def upgrade():
     import logging
     logger = logging.getLogger('alembic.here')
@@ -33,7 +101,18 @@ def upgrade():
             sa.ForeignKey('employee_quality_option.id'),
         )
     )
-
+    op.add_column(
+        "task",
+        sa.Column(
+            "internal_number",
+            sa.String(40),
+        )
+    )
+    op.add_column(
+        "task",
+        sa.Column("company_index", sa.Integer)
+    )
+    op.execute("alter table task CHANGE sequence_number project_index int(11)")
     op.add_column(
         "task",
         sa.Column(
@@ -42,53 +121,16 @@ def upgrade():
             sa.ForeignKey('company.id'),
         )
     )
-    op.add_column(
-        "task",
-        sa.Column(
-            "internal_number",
-            sa.String(40),
-        )
-    )
-    logger.warn("######################## Company stuff")
     from autonomie.models.base import (
         DBSESSION,
     )
-    from alembic.context import get_bind
-    conn = get_bind()
-
     session = DBSESSION()
-    query = "select t.id, p.company_id from task t, project p where t.project_id = p.id"
 
-    for id, company_id in conn.execute(query):
-        req = "update task set company_id={0} where id={1}".format(company_id, id)
-        session.execute(req)
+    add_company_id(session, logger)
+    add_company_index(session, logger)
+    add_internal_number(session, logger)
 
-    logger.warn("######################## Task stuff")
-    from autonomie.models.task import Task
-    from autonomie.models.customer import Customer
-    from autonomie.models.project import Project
-    from autonomie.models.project import Phase
-
-    from sqlalchemy.orm import joinedload
-    from sqlalchemy.orm import load_only
-
-    query = Task.query().options(
-        load_only("sequence_number", "date", "phase_id")
-    )
-    query = query.filter(
-        Task.type_.in_(['invoice', 'estimation', 'cancelinvoice'])
-    )
-    query = query.options(joinedload(Task.customer).load_only(Customer.code))
-    query = query.options(joinedload(Task.project).load_only(Project.code))
-
-    for task in query:
-        if Phase.get(task.phase_id) is None:
-            session.delete(task)
-        else:
-            task.internal_number = task._number_tmpl.format(s=task)
-            session.merge(task)
-
-    logger.warn("######################## Contract stuff")
+    logger.warn("Adding Contract Histories")
     from autonomie.models.user import UserDatas, ContractHistory
     for id_, last_avenant in UserDatas.query('id', 'parcours_last_avenant'):
         if last_avenant:

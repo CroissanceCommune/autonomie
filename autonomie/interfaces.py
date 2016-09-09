30a496e9457a3fb1f00a9f245b19e912d478a4d9
G.Tjebbes <g.t@majerti.fr>
2016-09-09 18:43:43 +0200

    Add interfaces for some services

---
+++ autonomie/interfaces.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+# * Copyright (C) 2012-2016 Croissance Commune
+# * Authors:
+#       * TJEBBES Gaston <g.t@majerti.fr>
+#       * Arezki Feth <f.a@majerti.fr>;
+#       * Miotte Julien <j.m@majerti.fr>;
+#
+# This file is part of Autonomie : Progiciel de gestion de CAE.
+#
+#    Autonomie is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Autonomie is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Autonomie.  If not, see <http://www.gnu.org/licenses/>.
+
+from zope.interface import Interface
+
+
+class ITreasuryMainInvoice(Interface):
+    """
+    Service producing the main export lines from a given invoice
+    """
+    def set_invoice(self):
+        """
+        Set the current invoice
+        """
+        pass
+
+    def yield_entries(self):
+        """
+        yield lines that need to be outputed
+        """
+        pass
+
+
+class ITreasuryInvoiceProducer(Interface):
+    """
+    Interface for the class that will produce treasury invoice export lines
+    """
+    def get_book_entries(self, invoicelist):
+        pass
+
+
+class ITreasuryInvoiceWriter(Interface):
+    """
+    Interface for the module handling the generation of the tabular export file
+    """
+    def set_datas(self, lines):
+        """
+        Set the tabular datas that will be written in the output file
+        """
+        pass

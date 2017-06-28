acdfa9426557b9d5d7f043d17e4a38fdf2bd7078
G.Tjebbes <g.t@majerti.fr>
2017-06-28 12:43:03 +0200

    Add js webpack base stuff + update Makefile

--- Makefile
+++ Makefile
@@ -1,18 +1,28 @@
+JS_SOURCES_DIR=js_sources/
 JS_TEMPLATE_MAIN_FILE=autonomie/static/js/template.js
 JS_TEMPLATE_DEST=autonomie/static/js/templates
-JS_TEMPLATES_SOURCE=handlebars
+JS_TEMPLATES_SOURCE=$(JS_SOURCES_DIR)src/handlebars
 DIRECTORIES=$(shell cd $(JS_TEMPLATES_SOURCE) && find *  -maxdepth 1 -type d)
 
 SASSC=$(shell which sassc)
 CSS_SOURCES=css_sources/main.scss
 CSS_DEST=autonomie/static/css/main.css
 
+# Used to build templates used in inline javascript stuff (not webpacked)
 js:
 	handlebars $(JS_TEMPLATES_SOURCE)/*.mustache  -f $(JS_TEMPLATE_MAIN_FILE)
 	for dir in $(DIRECTORIES);do \
 		handlebars $(JS_TEMPLATES_SOURCE)/$$dir/*.mustache -f $(JS_TEMPLATE_DEST)/$$dir.js; \
 	done
 
+# build js with webpack
+prodjs:
+	cd $(JS_SOURCES_DIR) && npm run prod
+
+devjs:
+	cd $(JS_SOURCES_DIR) && npm run dev
+
+# build css with libsassc
 css:
 	$(SASSC) $(CSS_SOURCES) $(CSS_DEST)
 	
---
+++ js_sources/webpack.config.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+const merge = require('webpack-merge');
+
+const APP_DIR = path.resolve(__dirname, 'src');
+const BUILD_DIR = path.resolve(__dirname, "..", "autonomie/static/js/build/");
+var PROD = (process.env.NODE_ENV === 'production');
+
+const config = {
+  entry: {
+    task: path.join(APP_DIR, 'task', 'task.js')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.mustache$/,
+        loader: "handlebars-loader"
+      }
+    ]
+  },
+  output: {
+    path: BUILD_DIR,
+    filename: PROD ? '[name].min.js': '[name].js'
+  },
+  plugins: PROD? [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      _: 'underscore'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false
+        }
+    })
+  ]: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      _: 'underscore'
+    })
+  ],
+  resolve: {
+    root: path.join(__dirname, './src')
+  },
+  resolveLoader: {
+    root: path.join(__dirname, './node_modules')
+  }
+};
+
+module.exports = config;

0054b574fc5ea89547ae116d01a3b72d656ec352
G.Tjebbes <g.t@majerti.fr>
2017-07-03 11:18:37 +0200

    Add Controller and Router

---
+++ js_sources/src/task/components/Controller.js
@@ -0,0 +1,24 @@
+/*
+ * File Name : Controller.js
+ *
+ * Copyright (C) 2012 Gaston TJEBBES g.t@majerti.fr
+ * Company : Majerti ( http://www.majerti.fr )
+ *
+ * This software is distributed under GPLV3
+ * License: http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ */
+import Mn from 'backbone.marionette';
+import MainView from '../views/MainView.js';
+import App from './App.js';
+
+const Controller = Mn.Object.extend({
+    initialize: function(datas){
+        this.mainView = new MainView({"datas": datas});
+        App.showView(this.mainView);
+    },
+    test: function(){
+        console.log('test Route is working');
+    }
+});
+export default Controller
---
+++ js_sources/src/task/components/Router.js
@@ -0,0 +1,19 @@
+/*
+ * File Name : router.js
+ *
+ * Copyright (C) 2012 Gaston TJEBBES g.t@majerti.fr
+ * Company : Majerti ( http://www.majerti.fr )
+ *
+ * This software is distributed under GPLV3
+ * License: http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ */
+
+import Mn from 'backbone.marionette';
+
+const Router = Mn.AppRouter.extend({
+  appRoutes: {
+    'test': 'test'
+  }
+});
+export default Router;

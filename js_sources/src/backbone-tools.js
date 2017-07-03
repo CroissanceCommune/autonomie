b6176a8898dad86b48a62535b5fd55e5cb9ed392
G.Tjebbes <g.t@majerti.fr>
2017-07-03 11:14:23 +0200

    Base library for backbone-validation tools

---
+++ js_sources/src/backbone-tools.js
@@ -0,0 +1,73 @@
+/*
+ * * Copyright (C) 2012-2017 Croissance Commune
+ * * Authors:
+ *       * Arezki Feth <f.a@majerti.fr>;
+ *       * Miotte Julien <j.m@majerti.fr>;
+ *       * TJEBBES Gaston <g.t@majerti.fr>
+ *
+ * This file is part of Autonomie : Progiciel de gestion de CAE.
+ *
+ *    Autonomie is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    Autonomie is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with Autonomie.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const showError = function(control, error){
+  /*"""
+   * shows error 'message' to the group group in a twitter bootstrap
+   * friendly manner
+   */
+  var group = control.parents(".form-group");
+  group.addClass("has-error");
+  if (group.find(".help-block").length === 0){
+    group.append(
+    "<span class=\"help-block error-message\"></span>");
+  }
+  var target = group.find(".help-block");
+  return target.text(error);
+}
+const hideFormError = function(form){
+  /*"""
+   * Remove bootstrap style errors from the whole form
+   */
+    form.find(".alert").remove();
+    var groups = form.find(".form-group");
+    groups.removeClass("has-error");
+    groups.find(".error-message").remove();
+    return form;
+}
+const hideFieldError = function(control){
+  /*"""
+   */
+   var group = control.parents(".form-group");
+   group.removeClass("has-error");
+   group.find(".error-message").remove();
+   return control;
+}
+const BootstrapOnValidForm = function(view, attr, selector){
+    var control, group;
+    control = view.$('[' + selector + '=' + attr + ']');
+    hideFieldError(control);
+}
+const BootstrapOnInvalidForm = function(view, attr, error, selector) {
+    var control, group, position, target;
+    control = view.$('[' + selector + '=' + attr + ']');
+    showError(control, error);
+}
+const setUpBbValidationCallbacks = function(bb_module){
+    _.extend(bb_module, {
+        valid: BootstrapOnValidForm,
+        invalid: BootstrapOnInvalidForm
+    });
+}
+
+export default setUpBbValidationCallbacks;

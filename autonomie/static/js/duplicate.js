eb031dabb2c04cce4efbc56c80d98bf4c2ed0458
G.Tjebbes <g.t@majerti.fr>
2017-07-04 18:08:15 +0200

    indent

--- autonomie/static/js/duplicate.js
+++ autonomie/static/js/duplicate.js
@@ -27,120 +27,121 @@
 var ALREADY_LOADED = new Object();
 
 function fetch_customer(customer_id){
-  return $.ajax({
-         type: 'GET',
-         url:"/customers/" + customer_id,
-         dataType: 'json',
-         success: function(data) {
-           ALREADY_LOADED[customer_id] = data;
-         },
-         async: false
-  });
+    return $.ajax({
+        type: 'GET',
+        url:"/customers/" + customer_id,
+        dataType: 'json',
+        success: function(data) {
+            ALREADY_LOADED[customer_id] = data;
+        },
+        async: false
+    });
 }
 
 function get_customer(customer_id){
-  /*
-   * Fetch the customer object through the json api
-   */
-  if (customer_id in ALREADY_LOADED){
-    return ALREADY_LOADED[customer_id];
-  }else{
-    fetch_customer(customer_id);
-    return ALREADY_LOADED[customer_id];
-  }
+    /*
+     * Fetch the customer object through the json api
+     */
+    if (customer_id in ALREADY_LOADED){
+        return ALREADY_LOADED[customer_id];
+    }else{
+        fetch_customer(customer_id);
+        return ALREADY_LOADED[customer_id];
+    }
 }
 function get_form(){
-  return $('#duplicate_form');
+    return $('#duplicate_form');
 }
 function get_select_customer(){
-  return $(get_form().find('select')[0]);
+    return $(get_form().find('select')[0]);
 }
 function get_select_project(){
-  return $(get_form().find('select')[1]);
+    return $(get_form().find('select')[1]);
 }
 function get_select_phase(){
-  return $(get_form().find('select')[2]);
+    return $(get_form().find('select')[2]);
 }
 
 function update_project_select(projects){
-  /*
-   * Update the project select
-   */
-  var options = "";
-  if (projects){
-    for (var i = 0; i < projects.length; i++) {
-      var project = projects[i];
-      options += "<option value='" + project.id;
-      if (i === 0){
-        options += "' selected='selected'>";
-      }else{
-        options += "'>";
-      }
-      options += project.name + '(' + project.code + ')' + "</option>";
+    /*
+     * Update the project select
+     */
+    var options = "";
+    if (projects){
+        for (var i = 0; i < projects.length; i++) {
+            var project = projects[i];
+            options += "<option value='" + project.id;
+            if (i === 0){
+                options += "' selected='selected'>";
+            }else{
+                options += "'>";
+            }
+            options += project.name + '(' + project.code + ')' + "</option>";
+        }
     }
-  }
-  get_select_project().html(options);
-  get_select_project().change();
+    get_select_project().html(options);
+    get_select_project().change();
 }
 function update_phase_select(phases){
-  /*
-   * Update the phase select
-   */
-  var options = "";
-  if (phases){
-    for (var i = 0; i < phases.length; i++) {
-      var phase = phases[i];
-      options += "<option value='" + phase.id + "'>"  +
+    /*
+     * Update the phase select
+     */
+    var options = "";
+    if (phases){
+        for (var i = 0; i < phases.length; i++) {
+            var phase = phases[i];
+            options += "<option value='" + phase.id + "'>"  +
                 phase.name +
                 "</option>";
+        }
     }
-  }
-  get_select_phase().html(options);
+    get_select_phase().html(options);
 }
 function getCurrentCustomer(){
-  var customer_id = get_select_customer().children('option:selected').val();
-  return get_customer(customer_id);
+    var customer_id = get_select_customer().children('option:selected').val();
+    return get_customer(customer_id);
 }
 function getCurrentProject(){
-  var project_id = get_select_project().children('option:selected').val();
-  var customer = getCurrentCustomer();
-  var ret_data = {};
-  for (var i=0; i < customer.projects.length; i++){
-    var project = customer.projects[i];
-    if (project.id == project_id){
-      ret_data = project;
-      break;
+    var project_id = get_select_project().children('option:selected').val();
+    var customer = getCurrentCustomer();
+    var ret_data = {};
+    for (var i=0; i < customer.projects.length; i++){
+        var project = customer.projects[i];
+        if (project.id == project_id){
+            ret_data = project;
+            break;
+        }
     }
-  }
-  return ret_data;
+    return ret_data;
 }
 $(function(){
-  /*
-   * Add onchange behaviour at page load
-   */
-  get_select_customer().change(
-    function(){
-      var customer_obj = getCurrentCustomer();
-      if (customer_obj.projects){
-        update_project_select(customer_obj.projects);
-      }else{
-        update_project_select([]);
-      }
-    }
-    );
-  get_select_project().change(
-    function(){
-      var project_obj = getCurrentProject();
-
-      if (project_obj !== {}){
-        if (project_obj.phases){
-            update_phase_select(project_obj.phases);
-        }else{
-          update_phase_select([]);
+    /*
+     * Add onchange behaviour at page load
+     */
+    get_select_customer().change(
+        function(){
+            console.log("Changed");
+            var customer_obj = getCurrentCustomer();
+            if (customer_obj.projects){
+                update_project_select(customer_obj.projects);
+            }else{
+                update_project_select([]);
+            }
         }
-      }else{
-        window.alert("Le client et le projet ne correspondent pas.");
-      }
-    }
-  );
+    );
+    get_select_project().change(
+        function(){
+            var project_obj = getCurrentProject();
+
+            if (project_obj !== {}){
+                if (project_obj.phases){
+                    update_phase_select(project_obj.phases);
+                }else{
+                    update_phase_select([]);
+                }
+            }else{
+                window.alert("Le client et le projet ne correspondent pas.");
+            }
+        }
+    );
 });

a376ebb055b9a33c14bd239a49281bbe887d92e7
G.Tjebbes <g.t@majerti.fr>
2017-07-11 13:18:29 +0200

    Add behaviour for bootstrap modal support

---
+++ js_sources/src/task/behaviors/ModalBehavior.js
@@ -0,0 +1,44 @@
+/*
+ * File Name : ModalBehavior.js
+ *
+ * Copyright (C) 2017 Gaston TJEBBES g.t@majerti.fr
+ * Company : Majerti ( http://www.majerti.fr )
+ *
+ * This software is distributed under GPLV3
+ * License: http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ */
+import Mn from 'backbone.marionette';
+
+
+const ModalBehavior = Mn.Behavior.extend({
+  defaults: {
+    modalClasses: '',
+    modalOptions: null
+  },
+
+  ui: {
+    close: '.close-modal'
+  },
+
+  events: {
+    'hidden.bs.modal': 'triggerFinish',
+  },
+
+  triggers: {
+    'click @ui.close': 'close:modal'
+  },
+  onRender: function() {
+    this.view.$el.addClass('modal ' + this.getOption('modalClasses'));
+  },
+  onAttach: function() {
+    this.view.$el.modal(this.getOption('modalOptions') || {});
+  },
+  onCloseModal: function() {
+    this.view.$el.modal('hide');
+  },
+  triggerFinish: function() {
+    this.view.triggerMethod('destroy:modal');
+  }
+});
+export default ModalBehavior;

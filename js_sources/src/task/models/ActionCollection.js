6b24e6aeba2b76f887db1e6bdcacf709065a64fb
G.Tjebbes <g.t@majerti.fr>
2017-07-11 13:18:51 +0200

    Add model and collection for Actions

---
+++ js_sources/src/task/models/ActionCollection.js
@@ -0,0 +1,18 @@
+/*
+ * File Name : ActionCollection.js
+ *
+ * Copyright (C) 2017 Gaston TJEBBES g.t@majerti.fr
+ * Company : Majerti ( http://www.majerti.fr )
+ *
+ * This software is distributed under GPLV3
+ * License: http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ */
+import Bb from 'backbone';
+import ActionModel from './ActionModel.js';
+
+
+const ActionCollection = Bb.Collection.extend({
+    model: ActionModel
+});
+export default ActionCollection;
---
+++ js_sources/src/task/models/ActionModel.js
@@ -0,0 +1,15 @@
+/*
+ * File Name : ActionModel.js
+ *
+ * Copyright (C) 2017 Gaston TJEBBES g.t@majerti.fr
+ * Company : Majerti ( http://www.majerti.fr )
+ *
+ * This software is distributed under GPLV3
+ * License: http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ */
+import Bb from 'backbone';
+
+
+const ActionModel = Bb.Model.extend({});
+export default ActionModel;

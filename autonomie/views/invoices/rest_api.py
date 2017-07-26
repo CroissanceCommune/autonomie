92353b45af205e6b4d468234fc3ebf646714e688
G.Tjebbes <g.t@majerti.fr>
2017-07-26 15:17:51 +0200

    Add missing module

---
+++ autonomie/views/invoices/rest_api.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# * Authors:
+#       * TJEBBES Gaston <g.t@majerti.fr>
+#       * Arezki Feth <f.a@majerti.fr>;
+#       * Miotte Julien <j.m@majerti.fr>;
+"""
+Rest views for invoices and cancelinvoices
+"""
+
+
+def add_routes(config):
+    pass
+
+
+def add_views(config):
+    pass
+
+
+def includeme(config):
+    add_routes(config)
+    add_views(config)

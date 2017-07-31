6a4253108d64fc9a9c03ec92e4f48221bbae4336
G.Tjebbes <g.t@majerti.fr>
2017-07-31 22:32:47 +0200

    Add a base OrderableCollection

---
+++ js_sources/src/task/models/OrderableCollection.js
@@ -0,0 +1,69 @@
+/*
+ * File Name : OrderableCollection.js
+ *
+ * Copyright (C) 2017 Gaston TJEBBES g.t@majerti.fr
+ * Company : Majerti ( http://www.majerti.fr )
+ *
+ * This software is distributed under GPLV3
+ * License: http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ */
+import _ from 'underscore';
+import Bb from 'backbone';
+
+const OrderableCollection = Bb.Collection.extend({
+    comparator: 'order',
+    initialize: function(options) {
+        this.on('change:reorder', this.updateModelOrder);
+        this.updateModelOrder(false);
+    },
+    updateModelOrder: function(sync){
+        var sync = sync || true;
+        this.each(function(model, index) {
+            model.set('order', index);
+            if (sync){
+                model.save(
+                    {'order': index},
+                    {patch: true},
+                );
+            }
+        });
+
+    },
+    getMinOrder: function(){
+        if (this.models.length == 0){
+            return 0
+        }
+        let first_model = _.min(
+            this.models,
+            function(model){return model.get('order')}
+        );
+        return first_model.get('order');
+    },
+    getMaxOrder: function(){
+        if (this.models.length == 0){
+            return 0
+        }
+        let last_model = _.max(
+            this.models,
+            function(model){return model.get('order')}
+        );
+        return last_model.get('order');
+    },
+    moveUp: function(model) { // I see move up as the -1
+        var index = this.indexOf(model);
+        if (index > 0) {
+            this.models.splice(index - 1, 0, this.models.splice(index, 1)[0]);
+            this.trigger('change:reorder');
+        }
+    },
+    moveDown: function(model) {
+        // I see move up as the -1
+        var index = this.indexOf(model);
+        if (index < this.models.length) {
+            this.models.splice(index + 1, 0, this.models.splice(index, 1)[0]);
+            this.trigger('change:reorder');
+        }
+    },
+});
+export default OrderableCollection;

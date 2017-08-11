804a6d96b70e2bc884eb04887d9c8cc0d0238fee
G.Tjebbes <g.t@majerti.fr>
2017-08-11 14:42:53 +0200

    Moved some stuff to autonomie_base

--- autonomie/utils/renderer.py
+++ autonomie/utils/renderer.py
@@ -26,24 +26,16 @@
     MultiRenderer tools to allow multiple renderers to be used with deform
 """
 import logging
-import datetime
-import colander
 import deform
 
-from decimal import Decimal
+from autonomie_base.utils.renderers import (
+    configure_export,
+    set_json_renderer,
+)
 from pkg_resources import resource_filename
 
 from deform.template import ZPTRendererFactory
 
-from sqlalchemy import (
-    Boolean,
-    Date,
-    DateTime,
-    Integer,
-    Float,
-)
-
-from pyramid.renderers import JSON
 from pyramid.threadlocal import get_current_request
 
 
@@ -90,85 +82,6 @@ def set_custom_form_renderer(config):
     deform.form.Form.default_renderer = renderer
 
 
-def set_json_renderer(config):
-    """
-        Customize json renderer to allow datetime rendering
-    """
-    json_renderer = JSON()
-
-    def toisoformat(obj, request):
-        return obj.isoformat()
-
-    json_renderer.add_adapter(datetime.datetime, toisoformat)
-    json_renderer.add_adapter(datetime.date, toisoformat)
-    json_renderer.add_adapter(colander._null, lambda _, r: "null")
-
-    def decimal_to_num(obj, request):
-        return float(obj)
-
-    json_renderer.add_adapter(Decimal, decimal_to_num)
-
-    config.add_renderer('json', json_renderer)
-    return config
-
-
-def set_export_formatters():
-    """
-    Globally set export formatters in the sqla_inspect registry
-    """
-    from sqla_inspect.export import FORMATTERS_REGISTRY
-    from autonomie_base.utils.date import (
-        format_date,
-        format_datetime,
-    )
-    from autonomie.utils.strings import (
-        format_quantity,
-    )
-    from autonomie.export.utils import format_boolean
-
-    FORMATTERS_REGISTRY.add_formatter(
-        Date, format_date, 'py3o'
-    )
-    FORMATTERS_REGISTRY.add_formatter(
-        DateTime, format_datetime, 'py3o'
-    )
-    FORMATTERS_REGISTRY.add_formatter(
-        Date, format_date, 'csv'
-    )
-    FORMATTERS_REGISTRY.add_formatter(
-        DateTime, format_date, 'csv'
-    )
-    FORMATTERS_REGISTRY.add_formatter(Boolean, format_boolean)
-    FORMATTERS_REGISTRY.add_formatter(Float, format_quantity)
-    FORMATTERS_REGISTRY.add_formatter(Integer, format_quantity)
-
-
-def set_export_blacklist():
-    """
-    Globally set an export blacklist
-    """
-    from sqla_inspect.export import BLACKLISTED_KEYS
-
-    BLACKLISTED_KEYS.extend([
-        '_acl',
-        'password',
-        'parent_id',
-        'parent',
-        'type_',
-        'children',
-    ])
-
-
-def set_xls_formats():
-    """
-    Globally set the xls formats by datatype
-    """
-    from sqla_inspect.excel import FORMAT_REGISTRY
-
-    FORMAT_REGISTRY.add_item(Date, "dd/mm/yyyy")
-    FORMAT_REGISTRY.add_item(DateTime, "dd/mm/yyyy hh:mm")
-
-
 def customize_renderers(config):
     """
     Customize the different renderers
@@ -179,6 +92,4 @@ def customize_renderers(config):
     # deform
     set_custom_form_renderer(config)
     # Exporters
-    set_export_formatters()
-    set_export_blacklist()
-    set_xls_formats()
+    configure_export()

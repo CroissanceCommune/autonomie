9b621a03bc6d3cc062f3214d7e5a49886a3b5c04
G.Tjebbes <g.t@majerti.fr>
2017-09-06 17:16:34 +0200

    Add migration script for user.vehicle

---
+++ autonomie/alembic/versions/3_3_2_add_user_vehicle_6a8b2495102.py
@@ -0,0 +1,35 @@
+"""3.3.2 : add_user_vehicle
+
+Revision ID: 6a8b2495102
+Revises: 22721b810d30
+Create Date: 2017-09-06 15:36:29.303641
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '6a8b2495102'
+down_revision = '22721b810d30'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def update_database_structure():
+    op.add_column(
+        'accounts',
+        sa.Column('vehicle', sa.String(50), nullable=True)
+    )
+
+def migrate_datas():
+    from autonomie_base.models.base import DBSESSION
+    session = DBSESSION()
+    from alembic.context import get_bind
+    conn = get_bind()
+
+def upgrade():
+    update_database_structure()
+    migrate_datas()
+
+
+def downgrade():
+    op.drop_column('accounts', 'vehicle')

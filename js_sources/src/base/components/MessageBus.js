517f3a0e7a253153a03fc222f8b5ea634513e0b4
G.Tjebbes <g.t@majerti.fr>
2017-09-19 17:19:00 +0200

    Add support for user feedback when executing actions

--- js_sources/src/base/components/MessageBus.js
+++ js_sources/src/base/components/MessageBus.js
@@ -17,12 +17,54 @@ const MessageBusClass = Mn.Object.extend({
     radioEvents: {
         'success': 'logSuccess',
         'error': 'logError',
+        'error:ajax': 'onAjaxError',
+        'success:ajax': 'onAjaxSuccess',
+        'error:backbone': 'onAjaxError',
+        'success:backbone': 'onAjaxSuccess',
     },
     logSuccess(view, message){
         console.log("MessageBus : " + message);
+        this.getChannel().request('notify:success', message);
     },
     logError(view, message){
         console.error("MessageBus : " + message);
+        this.getChannel().request('notify:error', message);
+    },
+    extractJsonMessage(xhr){
+        let json_resp = xhr.responseJSON;
+        let result = null;
+        if (!_.isUndefined(json_resp)){
+            if (!_.isUndefined(json_resp.message)){
+                result = json_resp.message;
+            }else if (!_.isUndefined(json_resp.messages)){
+                result = json_resp.messages.join("<br />");
+            } else if (!_.isUndefined(json_resp.errors)){
+                result = json_resp.errors.join("<br />");
+            } else if (!_.isUndefined(json_resp.error)){
+                result =  json_resp.error;
+            } else if (json_resp.status == 'success'){
+                result = "Opération réussie";
+            } else if (xhr.status == 200){
+                result = "Opération réussie";
+            }
+        } else if (xhr.status == 403){
+            result = "Opération non permise";
+        } else if (xhr.status == 501){
+            result = "Une erreur inconnue est survenue, contactez votre administrateur";
+        }
+        return result
+    },
+    onAjaxError(xhr){
+        let message = this.extractJsonMessage(xhr);
+        if (!_.isNull(message)){
+            this.getChannel().request('notify:error', message);
+        }
+    },
+    onAjaxSuccess(xhr){
+        let message = this.extractJsonMessage(xhr);
+        if (!_.isNull(message)){
+            this.getChannel().request('notify:success', message);
+        }
     }
 });
 const MessageBus = new MessageBusClass();

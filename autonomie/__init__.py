160163ed16015e870927b623829745473e3011be
G.Tjebbes <g.t@majerti.fr>
2017-10-11 14:08:34 +0200

    Update version

--- autonomie/__init__.py
+++ autonomie/__init__.py
@@ -340,4 +340,4 @@ def main(global_config, **settings):
 __author__ = "Arezki Feth, Miotte Julien, Pettier Gabriel and Tjebbes Gaston"
 __copyright__ = "Copyright 2012-2013, Croissance Commune"
 __license__ = "GPL"
-__version__ = "3.0"
+__version__ = "3.4"

20c19134c3009656ad9964bd61da0530c47a3d2a
G.Tjebbes <g.t@majerti.fr>
2017-10-27 18:06:03 +0200

    Check The task add form integrity check

--- autonomie/tests/forms/tasks/test_base.py
+++ autonomie/tests/forms/tasks/test_base.py
@@ -4,8 +4,68 @@
 #       * Arezki Feth <f.a@majerti.fr>;
 #       * Miotte Julien <j.m@majerti.fr>;
 
+import pytest
 
-def test_new_task_schema(project, customer, phase, company):
+
+@pytest.fixture
+def company2(dbsession, user):
+    from autonomie.models.company import Company
+    company = Company(
+        name=u"Company 2",
+        email=u"company2@c.fr",
+    )
+    company.employees = [user]
+    dbsession.add(company)
+    dbsession.flush()
+    user.companies = [company]
+    user = dbsession.merge(user)
+    dbsession.flush()
+    return company
+
+
+@pytest.fixture
+def customer2(dbsession, company2):
+    from autonomie.models.customer import Customer
+    customer = Customer(
+        name=u"customer 2",
+        code=u"CUST2",
+        lastname=u"Lastname2",
+        firstname=u"Firstname2",
+        address=u"1th street",
+        zip_code=u"01234",
+        city=u"City",
+    )
+    customer.company = company2
+    dbsession.add(customer)
+    dbsession.flush()
+    return customer
+
+
+@pytest.fixture
+def project2(dbsession, company2, customer2):
+    from autonomie.models.project import Project
+    project = Project(name=u"Project 2")
+    project.company = company2
+    project.customers = [customer2]
+    dbsession.add(project)
+    dbsession.flush()
+    return project
+
+
+@pytest.fixture
+def phase2(dbsession, project2):
+    from autonomie.models.project import Phase
+    phase = Phase(name=u"Phase")
+    phase.project = project2
+    phase.project_id = project2.id
+    project2.phases.append(phase)
+    dbsession.add(phase)
+    dbsession.flush()
+    return phase
+
+
+def test_new_task_schema(project, customer, phase, company, phase2, project2):
+    import colander
     from pyramid.testing import DummyRequest
     from autonomie.tests.tools import Dummy
     from autonomie.forms.tasks.base import get_new_task_schema
@@ -32,5 +92,20 @@ def test_new_task_schema(project, customer, phase, company):
         'phase_id': phase.id,
         'course': 1,
     }
+    with pytest.raises(colander.Invalid):
+        schema.deserialize({
+            'name': u'Facture',
+            'customer_id': str(customer.id),
+            'project_id': str(project.id),
+            'phase_id': str(phase2.id),
+            'course': str(1)
+        })
 
-
+    with pytest.raises(colander.Invalid):
+        schema.deserialize({
+            'name': u'Facture',
+            'customer_id': str(customer.id),
+            'project_id': str(project2.id),
+            'phase_id': str(phase2.id),
+            'course': str(1)
+        })

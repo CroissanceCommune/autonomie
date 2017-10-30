a984e399e3e5809019deb55cdfa28f3cd0fc769d
G.Tjebbes <g.t@majerti.fr>
2017-10-30 18:45:26 +0100

    Fix stat computation with null value
    NULL values are not handled by the in_ func, here we enhance it by
    adding a or_ clause in case of not_in calls.

--- autonomie/views/user.py
+++ autonomie/views/user.py
@@ -66,6 +66,7 @@ from autonomie.utils.widgets import (
     ViewLink,
 )
 from autonomie.export.utils import write_file_to_request
+from autonomie.utils.renderer import set_close_popup_response
 from sqla_inspect import py3o
 from deform_extensions import (
     AccordionFormWidget,
@@ -946,11 +947,17 @@ class UserDatasXlsView(UserDatasListClass, BaseListView):
         """
         Return the streamed file object
         """
+        all_ids = [elem[0] for elem in query]
+
+        if not all_ids:
+            msg = u"Il n'y a aucun élément à exporter"
+            set_close_popup_response(self.request, msg)
+            return self.request.response
+
         job = FileGenerationJob()
         job.set_owner(self.request.user.login)
         self.request.dbsession.add(job)
         self.request.dbsession.flush()
-        all_ids = [elem[0] for elem in query]
         celery_job = export_to_file.delay(
             job.id,
             'userdatas',

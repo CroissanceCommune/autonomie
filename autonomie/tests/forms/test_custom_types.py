d2f9bc21c757fbb4ebf97b17ff03ab610d3e5acd
G.Tjebbes <g.t@majerti.fr>
2018-01-31 18:06:30 +0100

    Add a usefull CsvTuple tool

--- autonomie/tests/forms/test_custom_types.py
+++ autonomie/tests/forms/test_custom_types.py
@@ -27,17 +27,34 @@ from autonomie.forms.custom_types import AmountType
 from autonomie.forms.custom_types import specialfloat
 from autonomie.forms.custom_types import Integer
 
+
 def test_amount_type():
     a = AmountType()
     assert a.serialize(None, 15000) == "150.0"
     assert a.deserialize(None, u"79.4") == 7940
     assert a.deserialize(None, u"292,65") == 29265
 
+
 def test_specialfloat():
     a = u"495, 4 5€"
-    assert specialfloat( "", a) == 495.45
+    assert specialfloat("", a) == 495.45
+
 
 def test_integer():
     i = Integer()
     assert colander.null == i.serialize(None, None)
     assert "0" == i.serialize(None, 0)
+
+
+def test_csv_set():
+    from autonomie.forms.custom_types import CsvTuple
+    i = CsvTuple()
+
+    assert colander.null == i.serialize(None, None)
+    assert () == i.serialize(None, "")
+
+    tuple_res = ('aa', 'bb', 'cc')
+    str_res = "aa,bb,cc"
+
+    assert i.serialize(None, str_res) == tuple_res
+    assert i.deserialize(None, tuple_res) == str_res

a5b599ceabd6c2fcdca5353da43da5b5b68fdf45
G.Tjebbes <g.t@majerti.fr>
2018-02-27 18:16:02 +0100

    Ref #518 : customer forms module + tests

--- autonomie/forms/customer.py
+++ autonomie/forms/customer.py
@@ -26,9 +26,15 @@
     Customer handling forms schemas
 """
 import colander
+import deform
 from colanderalchemy import SQLAlchemySchemaNode
 
+from autonomie_base.consts import CIVILITE_OPTIONS as ORIG_CIVILITE_OPTIONS
+from autonomie import forms
 from autonomie.forms.lists import BaseListsSchema
+
+# For customers we also want 'Mr et Mme'
+CIVILITE_OPTIONS = ORIG_CIVILITE_OPTIONS + (('mr&mme', u"Monsieur et Madame"),)
 
 
 def get_list_schema():
@@ -79,13 +85,39 @@ def customer_after_bind(node, kw):
         del node['compte_cg']
 
 
+def add_common_widgets(schema):
+    """
+    Add common widgets configuration for the customer forms schema
+
+    :param obj schema: The Customer form schema
+    """
+    schema['civilite'].widget = forms.get_radio(
+        CIVILITE_OPTIONS[1:],
+        inline=True,
+    )
+    schema['civilite'].validator = colander.OneOf(
+        [a[0] for a in CIVILITE_OPTIONS]
+    )
+    schema['address'].widget = deform.widget.TextAreaWidget(
+        cols=25,
+        row=1,
+    )
+    schema['email'].validator = forms.mail_validator()
+    schema['comments'].widget = deform.widget.TextAreaWidget(
+        css_class="col-md-10"
+    )
+    return schema
+
+
 def get_company_customer_schema():
     """
     return the schema for user add/edit regarding the current user's role
     """
     from autonomie.models.customer import Customer
     schema = SQLAlchemySchemaNode(Customer)
+    schema = add_common_widgets(schema)
     schema['name'].missing = colander.required
+    schema.after_bind = customer_after_bind
     return schema
 
 
@@ -96,8 +128,9 @@ def get_individual_customer_schema():
     from autonomie.models.customer import Customer
     excludes = ('name', 'tva_intracomm', 'function',)
     schema = SQLAlchemySchemaNode(Customer, excludes=excludes)
-
+    schema = add_common_widgets(schema)
     schema['firstname'].title = u"Prénom"
     schema['lastname'].title = u'Nom'
     schema['civilite'].missing = colander.required
+    schema.after_bind = customer_after_bind
     return schema

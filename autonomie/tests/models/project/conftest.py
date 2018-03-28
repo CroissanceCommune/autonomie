12ae55cb54c491c3889900298ccef89784aeb9bf
G.Tjebbes <g.t@majerti.fr>
2018-03-28 17:35:34 +0200

    Ref #518 : add missing fixture file

---
+++ autonomie/tests/models/project/conftest.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+# * Authors:
+#       * TJEBBES Gaston <g.t@majerti.fr>
+#       * Arezki Feth <f.a@majerti.fr>;
+#       * Miotte Julien <j.m@majerti.fr>;
+import pytest
+
+
+@pytest.fixture
+def estimation(
+    dbsession,
+    tva,
+    unity,
+    project,
+    customer,
+    company,
+    user,
+    phase,
+):
+    from autonomie.models.task.estimation import Estimation
+    estimation = Estimation(
+        company=company,
+        project=project,
+        customer=customer,
+        phase=phase,
+        user=user,
+    )
+    dbsession.add(estimation)
+    dbsession.flush()
+    return estimation
+
+
+@pytest.fixture
+def invoice(
+    dbsession,
+    tva,
+    unity,
+    project,
+    customer,
+    company,
+    user,
+    phase,
+):
+    from autonomie.models.task.invoice import Invoice
+    invoice = Invoice(
+        company=company,
+        project=project,
+        customer=customer,
+        phase=phase,
+        user=user,
+    )
+    dbsession.add(invoice)
+    dbsession.flush()
+    return invoice
+
+
+@pytest.fixture
+def cancelinvoice(
+    dbsession,
+    tva,
+    unity,
+    project,
+    customer,
+    company,
+    user,
+    phase,
+):
+    from autonomie.models.task.invoice import CancelInvoice
+    cancelinvoice = CancelInvoice(
+        company=company,
+        project=project,
+        customer=customer,
+        phase=phase,
+        user=user,
+    )
+    dbsession.add(cancelinvoice)
+    dbsession.flush()
+    return cancelinvoice

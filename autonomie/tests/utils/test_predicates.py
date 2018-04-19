c38ecb7921e7e6ac98cbb760fbf2de0a6f2993f2
G.Tjebbes <g.t@majerti.fr>
2018-04-19 17:55:17 +0200

    Ref #550 : Add custom view predicates for auth
    1- if_setting_has_value permet de spécifier qu'une vue n'est disponible
    que si une settings est définie dans le fichier .ini
    
    2- api_key_authentication permet de spécifier qu'un vue n'est accessible
    que si la requête contient les informations pour une authentification par
    clé d'api (en spécifiant le nom de la clé):
    
    Une requête sera authentifiée (et la vue donc accessible) si :
    
        - une clé est définie dans le fichier.ini
        - Les headers de la requête contiennent :
            Une entrée timestamp (ou Timestamp) avec un timestamp
            suffisament proche de l'instant où celui-ci sera traité côté
            serveur
    
            Une entrée Authorization (ou authorization) contenant une clé de
            la forme :
                <algorithme> <clé encodée>
    
            Clé encodée correspond à l'encodage md5 de :
            <timestamp>-<clé d'api du fichier .ini>
    
            Ex de headers :
    
                Authorization HMAC-MD5 06dda91136f6ad4688cdf6c8fd991696
                Timestamp 1524146419.441631

---
+++ autonomie/tests/utils/test_predicates.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+# * Authors:
+#       * TJEBBES Gaston <g.t@majerti.fr>
+#       * Arezki Feth <f.a@majerti.fr>;
+#       * Miotte Julien <j.m@majerti.fr>;
+import time
+import pytest
+from hashlib import md5
+from autonomie.tests.tools import Dummy
+
+
+@pytest.fixture
+def settings():
+    return {}
+
+
+@pytest.fixture
+def request(settings):
+    registry = Dummy(settings=settings)
+    req = Dummy(registry=registry, headers={})
+    return req
+
+
+def test_settings_has_value(request, settings):
+    from autonomie.utils.predicates import SettingHasValuePredicate
+
+    predicate = SettingHasValuePredicate(('key', True), None)
+    assert predicate(None, request) is False
+    predicate = SettingHasValuePredicate(('key', False), None)
+    assert predicate(None, request) is True
+
+    settings['key'] = "Test value"
+    predicate = SettingHasValuePredicate(('key', True), None)
+    assert predicate(None, request) is True
+    predicate = SettingHasValuePredicate(('key', False), None)
+    assert predicate(None, request) is False
+
+
+def test_get_timestamp_from_request(request):
+    from autonomie.utils.predicates import get_timestamp_from_request
+
+    with pytest.raises(KeyError):
+        get_timestamp_from_request(request)
+
+    request.headers = {'timestamp': "124545.152"}
+    assert get_timestamp_from_request(request) == "124545.152"
+
+    request.headers = {'Timestamp': "124545.152"}
+    assert get_timestamp_from_request(request) == "124545.152"
+
+
+def test_check_timestamp():
+    from autonomie.utils.predicates import check_timestamp
+    assert check_timestamp(time.time(), tolerance=2)
+    assert not check_timestamp(time.time() - 3, tolerance=2)
+
+
+def test_get_clientsecret_from_request(request):
+    from autonomie.utils.predicates import get_clientsecret_from_request
+
+    with pytest.raises(KeyError):
+        get_clientsecret_from_request(request)
+
+    with pytest.raises(ValueError):
+        request.headers = {'Authorization': "HMAC-OTHER secret"}
+        get_clientsecret_from_request(request)
+
+    with pytest.raises(KeyError):
+        request.headers = {'Authorization': "nospacesecret"}
+        get_clientsecret_from_request(request)
+
+    request.headers = {'Authorization': "HMAC-MD5 secret"}
+    assert get_clientsecret_from_request(request) == "secret"
+
+    request.headers = {'authorization': "HMAC-MD5 secret"}
+    assert get_clientsecret_from_request(request) == "secret"
+
+
+def test_check_secret():
+    from autonomie.utils.predicates import check_secret
+
+    # In [8]: md5('123456-secret').hexdigest()
+    # Out[8]: '06dda91136f6ad4688cdf6c8fd991696'
+    assert check_secret("06dda91136f6ad4688cdf6c8fd991696", 123456, "secret")
+
+
+def test_api_key_authentication(request, settings):
+    from autonomie.utils.predicates import ApiKeyAuthentication
+
+    settings['key'] = 'secret'
+    timestamp = request.headers['timestamp'] = time.time()
+    request.headers['Authorization'] = "HMAC-MD5 " + \
+        md5(u"%s-secret" % timestamp).hexdigest()
+
+    api = ApiKeyAuthentication('key', None)
+    assert api(None, request)
+
+    api = ApiKeyAuthentication('wrongkey', None)
+    assert not api(None, request)

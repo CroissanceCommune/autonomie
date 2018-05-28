b1bab900e4f48b8d1cdcaf6f38b035429cac8095
G.Tjebbes <g.t@majerti.fr>
2018-05-28 16:30:33 +0200

    Modèle Business : hérite désormais de Node

--- autonomie/models/project/business.py
+++ autonomie/models/project/business.py
@@ -18,26 +18,39 @@ from autonomie_base.models.types import (
 )
 from autonomie_base.models.base import (
     default_table_args,
-    DBBASE,
 )
+from autonomie.models.node import Node
 
 
-class Business(DBBASE, PersistentACLMixin):
+class Business(Node):
+    """
+    Permet de :
+
+        * Collecter les fichiers
+
+        * Regrouper devis/factures/avoirs
+
+        * Calculer le CA d'une affaire
+
+        * Générer les factures
+
+        * Récupérer le HT à dépenser
+
+        * Des choses plus complexes en fonction du type de business
+
+    Business.estimations
+    Business.invoices
+    Business.invoices[0].cancelinvoices
+    """
     __tablename__ = "business"
     __table_args__ = default_table_args
+    __mapper_args__ = {'polymorphic_identity': "business"}
+
     id = Column(
         Integer,
+        ForeignKey('node.id'),
         primary_key=True,
         info={'colanderalchemy': {'exclude': True}},
-    )
-
-    name = Column(
-        String(150),
-        info={
-            "colanderalchemy": {
-                "title": u"Nom du sous-projet",
-            }
-        }
     )
 
     closed = Column(
@@ -45,7 +58,7 @@ class Business(DBBASE, PersistentACLMixin):
         default=False,
         info={
             'colanderalchemy': {
-                'title': u"Ce sous-projet est-il fermé ?"
+                'title': u"Cette affaire est-elle terminée ?"
             }
         },
     )
@@ -59,14 +72,35 @@ class Business(DBBASE, PersistentACLMixin):
         info={'colanderalchemy': {'exclude': True}},
     )
 
+    # Relations
     business_type = relationship(
         "BusinessType",
         info={'colanderalchemy': {'exclude': True}},
     )
     project = relationship(
         "Project",
+        primaryjoin="Project.id == Business.project_id",
         info={
             'colanderalchemy': {'exclude': True},
             'export': {'exclude': True}
         },
+    )
+    estimations = relationship(
+        "Estimation",
+        back_populates="businesses",
+        secondary="estimation_business",
+        info={
+            'colanderalchemy': {'exclude': True},
+            'export': {'exclude': True}
+        },
+    )
+    invoices = relationship(
+        "Invoice",
+        back_populates="business",
+        primaryjoin="Invoice.business_id==Business.id",
+    )
+    cancelinvoices = relationship(
+        "CancelInvoice",
+        back_populates="business",
+        primaryjoin="CancelInvoice.business_id==Business.id",
     )

7a6dfa1e0f27fd0edbee94d11715d5a5db01fbbe
G.Tjebbes <g.t@majerti.fr>
2018-06-07 13:06:59 +0200

    Update de sqlalchemy en version 1.1 + ajout d'un outil set_attribute

--- autonomie/models/tools.py
+++ autonomie/models/tools.py
@@ -3,6 +3,7 @@
 #       * TJEBBES Gaston <g.t@majerti.fr>
 #       * Arezki Feth <f.a@majerti.fr>;
 #       * Miotte Julien <j.m@majerti.fr>;
+from sqlalchemy import inspect
 
 
 def get_excluded_colanderalchemy(title):
@@ -11,3 +12,17 @@ def get_excluded_colanderalchemy(title):
     inspection based modules like py3o inspection or ods generation)
     """
     return {'exclude': True, title: title}
+
+
+def set_attribute(instance, key, value, initiator=None):
+    """Set the value of an attribute, firing history events.
+
+    This function is copied from the attributes module but adds the
+    "initiator" argument.
+
+    Mike Bayer's code provided on the sqlalchemy mailling list
+
+    """
+    state = inspect(instance)
+    dict_ = state.dict
+    state.manager[key].impl.set(state, dict_, value, initiator)

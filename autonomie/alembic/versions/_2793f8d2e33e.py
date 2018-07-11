8e2f8e17e0026a62e43375be7d3f1bfae3e5a69c
Jocelyn Delalande <jocelyn@crapouillou.net>
2018-07-11 15:27:28 +0200

    Ajoute le merge alembic manquant

---
+++ autonomie/alembic/versions/_2793f8d2e33e.py
@@ -0,0 +1,32 @@
+""" Revision merge
+
+Revision ID: 2793f8d2e33e
+Revises: ('50caf9dd4cd6', '2a66d798c55d')
+Create Date: 2018-07-11 14:43:11.369728
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '2793f8d2e33e'
+down_revision = ('50caf9dd4cd6', '2a66d798c55d')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def update_database_structure():
+    pass
+
+def migrate_datas():
+    from autonomie_base.models.base import DBSESSION
+    session = DBSESSION()
+    from alembic.context import get_bind
+    conn = get_bind()
+
+def upgrade():
+    update_database_structure()
+    migrate_datas()
+
+
+def downgrade():
+    pass

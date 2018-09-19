571d417428cfa60b52e16dce71e437afdb1c5247
G.Tjebbes <g.t@majerti.fr>
2018-09-19 16:46:37 +0200

    Fix sur le nom de la propriété dbsession de la BaseListClass

--- autonomie/views/__init__.py
+++ autonomie/views/__init__.py
@@ -117,7 +117,7 @@ class BaseListClass(BaseView):
 
     def __init__(self, *args, **kwargs):
         BaseView.__init__(self, *args, **kwargs)
-        self.dbsession = self.request.session
+        self.dbsession = self.request.dbsession
         self.error = None
 
     def _get_bind_params(self):

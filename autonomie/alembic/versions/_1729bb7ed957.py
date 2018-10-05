3324ad226b7349d4100d9d25f0cc56ad03bd29cc
Jocelyn Delalande <jocelyn@crapouillou.net>
2018-10-05 20:06:59 +0200

    Merge alembic
    suite rebase de jd-workshops sur master

---
+++ autonomie/alembic/versions/_1729bb7ed957.py
@@ -0,0 +1,34 @@
+"""Merge alembic
+
+(suite rebase jd-workshops sur master)
+
+Revision ID: 1729bb7ed957
+Revises: ('3885e8260693', '30fe999bc58e')
+Create Date: 2018-11-02 09:33:32.730752
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1729bb7ed957'
+down_revision = ('3885e8260693', '2ed5c14be058')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def update_database_structure():
+    pass
+
+def migrate_datas():
+    from autonomie_base.models.base import DBSESSION
+    session = DBSESSION()
+    from alembic.context import get_bind
+    conn = get_bind()
+
+def upgrade():
+    update_database_structure()
+    migrate_datas()
+
+
+def downgrade():
+    pass

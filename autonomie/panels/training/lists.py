b5377466e8d6acbcfcf63306411fcb0ce031b438
G.Tjebbes <g.t@majerti.fr>
2018-10-09 11:29:31 +0200

    Delete unused files

--- autonomie/panels/training/lists.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# * Authors:
-#       * TJEBBES Gaston <g.t@majerti.fr>
-#       * Arezki Feth <f.a@majerti.fr>;
-#       * Miotte Julien <j.m@majerti.fr>;
-
-
-class TrainingListPanel():
-    pass

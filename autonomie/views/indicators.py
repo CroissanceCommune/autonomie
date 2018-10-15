d5294d080f050e9e0f91d1497200c615b651802d
G.Tjebbes <g.t@majerti.fr>
2018-10-15 14:00:16 +0200

    Ajout d'un event IndicatorChanged

--- autonomie/views/indicators.py
+++ autonomie/views/indicators.py
@@ -3,9 +3,14 @@
 #       * TJEBBES Gaston <g.t@majerti.fr>
 #       * Arezki Feth <f.a@majerti.fr>;
 #       * Miotte Julien <j.m@majerti.fr>;
+import logging
 from pyramid.httpexceptions import HTTPFound
+from autonomie.events.indicators import IndicatorChanged
 
 INDICATOR_ROUTE = "/indicators/{id}"
+
+
+logger = logging.getLogger(__name__)
 
 
 def force_indicator_view(context, request):
@@ -14,9 +19,20 @@ def force_indicator_view(context, request):
     """
     if context.forced:
         context.unforce()
+        logger.debug(
+            u"+ Setting force=False for the indicator {}".format(
+                context.id,
+            )
+        )
     else:
+        logger.debug(
+            u"+ Setting force=True for the indicator {}".format(
+                context.id,
+            )
+        )
         context.force()
     request.dbsession.merge(context)
+    request.registry.notify(IndicatorChanged(request, context))
     return HTTPFound(request.referrer)
 
 
@@ -25,7 +41,11 @@ def validate_file_view(context, request):
     """
     validation_status = request.GET.get('validation_status')
     if validation_status in context.VALIDATION_STATUS:
+        logger.debug(u"+ Setting the status of the indicator {} to {}".format(
+            context.id, validation_status
+        ))
         context.set_validation_status(validation_status)
+        request.registry.notify(IndicatorChanged(request, context))
     else:
         request.session.flash(u"Statut invalide : %s" % validation_status)
     return HTTPFound(request.referrer)

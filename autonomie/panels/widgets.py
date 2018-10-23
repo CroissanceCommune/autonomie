f35df10f81c8c50a07feb59e9d744833d1f36a94
G.Tjebbes <g.t@majerti.fr>
2018-10-23 12:35:59 +0200

    Ajout d'un action_dropdown
    ${request.layout_manager.render_panel('action_dropdown',
        links=links)}
    
        où links est une liste d'instance de autonomie.utils.widgets.Link

--- autonomie/panels/widgets.py
+++ autonomie/panels/widgets.py
@@ -15,16 +15,21 @@ def link_panel(context, request):
     return dict(link=context)
 
 
-def menu_dropdown_panel(context, request, label, links):
+def menu_dropdown_panel(context, request, label, links, icon=None):
     """
     Menu dropdown panel
 
     :param obj context: The current context
     :param obj request: The current pyramid request
     :param str label: the label to use
+    :param str icon: An optionnal icon to add
     :param list buttons: List of autonomie.widgets.Link
     """
-    return dict(label=label, links=links)
+    return dict(label=label, links=links, icon=icon)
+
+
+def action_dropdown_panel(context, request, label=u"Actions", links=()):
+    return menu_dropdown_panel(context, request, label, links)
 
 
 def legend_panel(context, request, legends):
@@ -48,6 +53,11 @@ def includeme(config):
     config.add_panel(
         menu_dropdown_panel,
         "menu_dropdown",
+        renderer="autonomie:templates/panels/widgets/menu_dropdown.pt",
+    )
+    config.add_panel(
+        action_dropdown_panel,
+        "action_dropdown",
         renderer="autonomie:templates/panels/widgets/menu_dropdown.pt",
     )
     config.add_panel(

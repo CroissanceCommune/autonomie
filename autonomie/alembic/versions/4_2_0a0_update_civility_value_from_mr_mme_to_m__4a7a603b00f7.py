a49778e34ed6518a8703815b6e3952c03c8cb514
sbodrero <bodrero.sebastien@gmail.com>
2018-11-12 09:39:05 +0100

    Added alembic migration script

---
+++ autonomie/alembic/versions/4_2_0a0_update_civility_value_from_mr_mme_to_m__4a7a603b00f7.py
@@ -0,0 +1,35 @@
+"""Update civility value from mr&mme to M. et Mme
+
+Revision ID: 4a7a603b00f7
+Revises: 3885e8260693
+Create Date: 2018-11-10 17:00:58.886950
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '4a7a603b00f7'
+down_revision = '3885e8260693'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def update_database_structure():
+    pass
+
+def migrate_datas():
+    from autonomie_base.models.base import DBSESSION
+    session = DBSESSION()
+    from alembic.context import get_bind
+    conn = get_bind()
+    op.execute('update customer set civilite="M. et Mme" where civilite="mr&mme"')
+    from zope.sqlalchemy import mark_changed
+    mark_changed(session)
+
+def upgrade():
+    update_database_structure()
+    migrate_datas()
+
+
+def downgrade():
+    pass
